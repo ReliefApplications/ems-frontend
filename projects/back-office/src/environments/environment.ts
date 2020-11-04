ea2d5e956cbb0a7f8747a5edebcccf296050ace9
Antoine Hurard <antoine.reliefapps@gmail.com>
2020-11-04 11:48:30 +0100

    Install apollo, and set env files

--- projects/back-office/src/environments/environment.ts
+++ projects/back-office/src/environments/environment.ts
@@ -1,16 +1,8 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  API_URL: 'http://localhost:3000',
+  clientId: 'd62083d8-fdc0-4a6a-8618-652380eebdb9',
+  authority: 'https://login.microsoftonline.com/fbacd48d-ccf4-480d-baf0-31048368055f',
+  redirectUrl: 'http://localhost:4200',
+  postLogoutRedirectUri: 'http://localhost:4200/auth'
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
--- projects/front-office/src/environments/environment.prod.ts
+++ projects/front-office/src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  API_URL: 'https://api-ems-ui-poc.test.humanitarian.tech',
+  clientId: '13213ca9-a94b-4524-bc6e-6e9c0c3e6d53',
+  authority: 'https://login.microsoftonline.com/fbacd48d-ccf4-480d-baf0-31048368055f',
+  redirectUrl: 'https://ems-ui-poc.test.humanitarian.tech',
+  postLogoutRedirectUri: 'https://ems-ui-poc.test.humanitarian.tech/auth'
 };
