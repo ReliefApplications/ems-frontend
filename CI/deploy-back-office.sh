7d1c5c8aabe88f427c0748cb7d3aa2208371aefb
Antoine Hurard <antoine.reliefapps@gmail.com>
2020-11-06 17:16:06 +0100

    Test for nom

---
+++ CI/deploy-back-office.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Remove files
+REMOTE_PATH=/var/www/html/ems-ui-poc
+OUT=dist/back-office
+CONNECTION=newton@92.243.16.149
+
+echo -e "Cleaning destination ..."
+CMD="mkdir -p ${REMOTE_PATH} && cd ${REMOTE_PATH} && rm -rf *"
+ssh -oStrictHostKeyChecking=no -o PubkeyAuthentication=yes $CONNECTION "$CMD"
+
+echo -e "Synchronizing files ..."
+scp -o stricthostkeychecking=no -o PubkeyAuthentication=yes -r $OUT/* $CONNECTION:$REMOTE_PATH
+
+echo -e "Deployed !!"
