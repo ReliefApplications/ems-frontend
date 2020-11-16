08e03de7838dbd628cc2dbfee91d56ef564376b0
Pacome Rivier <pacome@reliefapplications.org>
2020-11-16 17:20:42 +0100

    Initialize the form view in a step

--- projects/back-office/src/app/dashboard/pages/application/form/form.component.ts
+++ projects/back-office/src/app/dashboard/pages/application/form/form.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Apollo } from 'apollo-angular';
+import { Form, WhoSnackBarService } from '@who-ems/builder';
+import { GetFormByIdQueryResponse, GET_FORM_BY_ID } from '../../../../graphql/queries';
 
 @Component({
   selector: 'app-form',
@@ -7,9 +11,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
-  constructor() { }
+  // === DATA ===
+  public id: string;
+  public loading = true;
+  public form: Form
+
+  constructor(
+    private apollo: Apollo,
+    private route: ActivatedRoute,
+    private snackBar: WhoSnackBarService) { }
 
   ngOnInit(): void {
+    this.id = this.route.snapshot.params.id;
+    this.apollo.watchQuery<GetFormByIdQueryResponse>({
+      query: GET_FORM_BY_ID,
+      variables: {
+        id: this.id
+      }
+    }).valueChanges.subscribe((res) => {
+      if (res.data.form) {
+        this.form = res.data.form;
+        this.loading = res.loading;
+      } else {
+        this.snackBar.openSnackBar('No access provided to this form.', { error: true });
+      }
+    },
+    (err) => {
+      this.snackBar.openSnackBar(err.message, { error: true });
+    })
   }
-
 }
