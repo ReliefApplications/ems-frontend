d5eda44289c2c1521d04b74ccf5da91ad524a360
Pacome Rivier <pacome@reliefapplications.org>
2020-11-19 10:34:02 +0100

    Fix a routing bug when loading a new workflow

--- projects/back-office/src/app/application/pages/workflow/workflow.component.ts
+++ projects/back-office/src/app/application/pages/workflow/workflow.component.ts
@@ -6,6 +6,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Apollo } from 'apollo-angular';
 import { Workflow, Step, WhoSnackBarService } from '@who-ems/builder';
 import { GetWorkflowByIdQueryResponse, GET_WORKFLOW_BY_ID } from '../../../graphql/queries';
+import { Subscription } from 'rxjs';
 import { AddTabComponent } from './components/add-tab/add-tab.component';
 import {
   EditPageMutationResponse, EDIT_PAGE,
@@ -35,6 +36,9 @@ export class WorkflowComponent implements OnInit {
   public displayStep = false;
   public selectedStep: Step;
 
+  // === ROUTE ===
+  private routeSubscription: Subscription;
+
   constructor(
     private apollo: Apollo,
     private route: ActivatedRoute,
@@ -45,30 +49,32 @@ export class WorkflowComponent implements OnInit {
 
   ngOnInit(): void {
     this.formActive = false;
-    this.id = this.route.snapshot.params.id;
-    this.apollo.watchQuery<GetWorkflowByIdQueryResponse>({
-      query: GET_WORKFLOW_BY_ID,
-      variables: {
-        id: this.id
-      }
-    }).valueChanges.subscribe((res) => {
-      if (res.data.workflow) {
-        this.workflow = res.data.workflow;
-        this.steps = res.data.workflow.steps;
-        this.workflowNameForm = new FormGroup({
-          workflowName: new FormControl(this.workflow.name, Validators.required)
-        });
-        this.loading = res.loading;
-      } else {
-        this.snackBar.openSnackBar('No access provided to this workflow.', { error: true });
-        this.router.navigate(['../../'], { relativeTo: this.route });
-      }
-    },
-      (err) => {
-        this.snackBar.openSnackBar(err.message, { error: true });
-        this.router.navigate(['../../'], { relativeTo: this.route });
-      }
-    );
+    this.routeSubscription = this.route.params.subscribe((params) => {
+      this.id = params.id;
+      this.apollo.watchQuery<GetWorkflowByIdQueryResponse>({
+        query: GET_WORKFLOW_BY_ID,
+        variables: {
+          id: this.id
+        }
+      }).valueChanges.subscribe((res) => {
+        if (res.data.workflow) {
+          this.workflow = res.data.workflow;
+          this.steps = res.data.workflow.steps;
+          this.workflowNameForm = new FormGroup({
+            workflowName: new FormControl(this.workflow.name, Validators.required)
+          });
+          this.loading = res.loading;
+        } else {
+          this.snackBar.openSnackBar('No access provided to this workflow.', { error: true });
+          this.router.navigate(['../../'], { relativeTo: this.route });
+        }
+      },
+        (err) => {
+          this.snackBar.openSnackBar(err.message, { error: true });
+          this.router.navigate(['../../'], { relativeTo: this.route });
+        }
+      );
+    });
   }
 
   toggleFormActive = () => this.formActive = !this.formActive;
@@ -86,20 +92,6 @@ export class WorkflowComponent implements OnInit {
       }
     }).subscribe(res => {
       this.workflow.name = res.data.editPage.name;
-    });
-  }
-
-  /*  Edit the permissions layer.
-  */
-  saveAccess(e: any): void {
-    this.apollo.mutate<EditPageMutationResponse>({
-      mutation: EDIT_PAGE,
-      variables: {
-        id: this.workflow.page.id,
-        permissions: e
-      }
-    }).subscribe(res => {
-      this.workflow.page.permissions = res.data.editPage.permissions;
     });
   }
 
@@ -188,5 +180,9 @@ export class WorkflowComponent implements OnInit {
       this.navigateToSelectedStep();
       this.displayStep = true;
     }
+  }
+
+  ngOnDestroy(): void {
+    this.routeSubscription.unsubscribe();
   }
 }
