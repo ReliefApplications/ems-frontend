03496ecc249b78ffdc0f91671216e25fc4537264
Antoine Hurard <antoine.reliefapps@gmail.com>
2020-11-19 12:44:27 +0100

    Provide quick solution to display the workflow on the front office

--- projects/front-office/src/app/graphql/queries.ts
+++ projects/front-office/src/app/graphql/queries.ts
@@ -1,5 +1,5 @@
 import gql from 'graphql-tag';
-import { Dashboard, Application, Form, User } from '@who-ems/builder';
+import { Dashboard, Application, Form, User, Workflow } from '@who-ems/builder';
 
 // === GET USERS ===
 export const GET_USERS = gql`
@@ -147,4 +147,51 @@ query GetFormById($id: ID!, $filters: JSON, $display: Boolean) {
 export interface GetFormByIdQueryResponse {
   loading: boolean;
   form: Form;
+}
+
+// === GET WORKFLOW BY ID ===
+export const GET_WORKFLOW_BY_ID = gql`
+  query GetWorkflowById($id: ID!){
+    workflow(id: $id){
+      id
+      name
+      createdAt
+      modifiedAt
+      steps {
+        id
+        name
+        type
+        content
+        createdAt
+      }
+      page {
+        id
+        name
+        canUpdate
+        permissions {
+          canSee {
+            id
+            title
+          }
+          canCreate {
+            id
+            title
+          }
+          canUpdate {
+            id
+            title
+          }
+          canDelete {
+            id
+            title
+          }
+        }
+      }
+    }
+  }
+`;
+
+export interface GetWorkflowByIdQueryResponse {
+  loading: boolean;
+  workflow: Workflow;
 }
