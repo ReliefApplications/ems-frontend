30a4c7fe47c3b045006c4c827b4427dc8403ff2f
Adi Subramanian <adi@oortcloud.tech>
2020-11-27 15:09:58 +0100

    Application modal instead of dropdown

--- projects/who-ems/src/lib/components/layout/application-modal/application-modal.component.ts
+++ projects/who-ems/src/lib/components/layout/application-modal/application-modal.component.ts
@@ -11,7 +11,6 @@ export class ApplicationModalComponent implements OnInit {
   constructor(@Inject(MAT_DIALOG_DATA) public data: any) { }
 
   ngOnInit(): void {
-    console.log(this.data)
   }
 
 }
