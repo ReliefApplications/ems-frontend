a492f400c39bcc60784bfc3c1beeec1898692213
Adi Subramanian <adi@oortcloud.tech>
2020-12-02 17:39:28 +0100

    Fix navigation back to resource

--- projects/who-ems/src/lib/components/previous-button/previous-button.component.spec.ts
+++ projects/who-ems/src/lib/components/previous-button/previous-button.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { PreviousButtonComponent } from './previous-button.component';
+import { WhoPreviousButtonComponent } from './previous-button.component';
 
-describe('PreviousButtonComponent', () => {
-  let component: PreviousButtonComponent;
-  let fixture: ComponentFixture<PreviousButtonComponent>;
+describe('WhoPreviousButtonComponent', () => {
+  let component: WhoPreviousButtonComponent;
+  let fixture: ComponentFixture<WhoPreviousButtonComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PreviousButtonComponent ]
+      declarations: [ WhoPreviousButtonComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(PreviousButtonComponent);
+    fixture = TestBed.createComponent(WhoPreviousButtonComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
