53e6e3244097796607559949875facd4fdfac37c
Antoine Hurard <antoine.reliefapps@gmail.com>
2020-12-11 12:54:04 +0100

    Working custom query for charts

--- projects/who-ems/src/lib/components/widgets/chart-settings/chart-settings.component.ts
+++ projects/who-ems/src/lib/components/widgets/chart-settings/chart-settings.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Apollo } from 'apollo-angular';
-import { GetResourcesQueryResponse, GET_RESOURCES, GetFormsQueryResponse, GET_FORMS,
-  GetResourceByIdQueryResponse, GET_RESOURCE_BY_ID, GetFormByIdQueryResponse, GET_FORM_BY_ID } from '../../../graphql/queries';
 import { chartTypes } from './constants';
 
 @Component({
@@ -25,9 +23,7 @@ export class WhoChartSettingsComponent implements OnInit {
   @Output() change: EventEmitter<any> = new EventEmitter();
 
   // === DATA ===
-  public sources = [];
-  public fields = [];
-  public forms: any[] = [];
+  // public fields = [];
   public types = chartTypes;
 
   constructor(
@@ -44,79 +40,14 @@ export class WhoChartSettingsComponent implements OnInit {
         id: this.tile.id,
         title: [(tileSettings && tileSettings.title) ? tileSettings.title : '', Validators.required],
         type: [(tileSettings && tileSettings.type) ? tileSettings.type : '', Validators.required],
-        source: [(tileSettings && tileSettings.source) ? tileSettings.source : '', Validators.required],
+        query: [(tileSettings && tileSettings.query) ? tileSettings.query : '', Validators.required],
         xAxis: [(tileSettings && tileSettings.xAxis) ? tileSettings.xAxis : '', Validators.required],
-        yAxis: [(tileSettings && tileSettings.yAxis) ? tileSettings.yAxis : '', Validators.required],
-        from: [(tileSettings && tileSettings.from) ? tileSettings.from : 'resource', Validators.required]
+        yAxis: [(tileSettings && tileSettings.yAxis) ? tileSettings.yAxis : '', Validators.required]
       }
     );
     this.change.emit(this.tileForm);
     this.tileForm.valueChanges.subscribe(() => {
       this.change.emit(this.tileForm);
     });
-
-    this.getSources({ value: this.tileForm.get('from').value }, true);
-    if (tileSettings.source) {
-      this.getSource({ value: tileSettings.source });
-    }
-  }
-
-  /*  Load the list of resources or forms.
-  */
-  getSources(e: any, init?: boolean): void {
-    if (e.value === 'resource') {
-      this.apollo.query<GetResourcesQueryResponse>({
-        query: GET_RESOURCES
-      }).subscribe(res => {
-        this.sources = res.data.resources.map(source => source = {
-          id: source.id,
-          name: source.name
-        });
-        if (!init) {
-          this.tileForm.get('source').setValue(null);
-          this.tileForm.get('xAxis').setValue(null);
-          this.tileForm.get('yAxis').setValue(null);
-        }
-        this.fields = [];
-      });
-    } else {
-      this.apollo.query<GetFormsQueryResponse>({
-        query: GET_FORMS
-      }).subscribe(res => {
-        this.sources = res.data.forms.map(source => source = { id: source.id, name: source.name });
-        if (!init) {
-          this.tileForm.get('source').setValue(null);
-          this.tileForm.get('xAxis').setValue(null);
-          this.tileForm.get('yAxis').setValue(null);
-        }
-        this.fields = [];
-      });
-    }
-  }
-
-  /*  Load a resource or a form.
-  */
-  getSource(e: any): void {
-    if (this.tileForm.controls.from.value === 'resource') {
-      this.apollo.query<GetResourceByIdQueryResponse>({
-        query: GET_RESOURCE_BY_ID,
-        variables: {
-          id: e.value
-        }
-      }).subscribe(res => {
-        this.fields = res.data.resource.fields;
-        this.forms = res.data.resource.forms;
-      });
-    } else {
-      this.apollo.query<GetFormByIdQueryResponse>({
-        query: GET_FORM_BY_ID,
-        variables: {
-          id: e.value
-        }
-      }).subscribe(res => {
-        this.fields = res.data.form.fields;
-        this.forms = [{ id: res.data.form.id, name: res.data.form.name }];
-      });
-    }
   }
 }
