60ffb866fe7b633f6982b4c789a58c04056c723c
Antoine Hurard <antoine.reliefapps@gmail.com>
2021-01-07 11:41:45 +0100

    Change msal to use common endpoint

--- projects/back-office/src/environments/environment.prod.ts
+++ projects/back-office/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   API_URL: 'https://safe-api.development.humanitarian.tech',
   SUBSCRIPTION_API_URL: 'wss://safe-api.development.humanitarian.tech',
-  clientId: '13213ca9-a94b-4524-bc6e-6e9c0c3e6d53',
+  clientId: 'd62083d8-fdc0-4a6a-8618-652380eebdb9',
   authority: 'https://login.microsoftonline.com/fbacd48d-ccf4-480d-baf0-31048368055f',
   redirectUrl: 'https://safe-backoffice.development.humanitarian.tech',
   postLogoutRedirectUri: 'https://safe-backoffice.development.humanitarian.tech/auth'
