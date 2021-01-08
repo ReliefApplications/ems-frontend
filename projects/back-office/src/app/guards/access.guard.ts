92b9827c0d8c00e51b5236d6fce1480138b0e62e
Antoine Hurard <antoine.reliefapps@gmail.com>
2021-01-08 12:29:15 +0100

    Test for access guard

--- projects/back-office/src/app/guards/access.guard.ts
+++ projects/back-office/src/app/guards/access.guard.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { User, WhoAuthService, WhoSnackBarService } from '@who-ems/builder';
 import { Observable } from 'rxjs';
-import { map, skip, filter } from 'rxjs/operators';
+import { map, skip } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -18,15 +18,18 @@ export class AccessGuard implements CanActivate {
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
       return this.authService.user.pipe(
-        // skip(1), // this is important as first value of behaviorSubject is null
-        filter(x => x !== null),
+        skip(1), // this is important as first value of behaviorSubject is null
         map((user: User) => {
+          console.log(user);
           if (user) {
             if (user.isAdmin) {
               return true;
             }
             this.snackBar.openSnackBar('No access provided to this platform.', { error: true });
             this.authService.logout();
+            this.router.navigate(['/auth']);
+            return false;
+          } else {
             this.router.navigate(['/auth']);
             return false;
           }
