b86b4017140cc5bba29c672508dc2a60cadac8ba
Antoine Hurard <antoine.reliefapps@gmail.com>
2021-01-26 10:51:23 +0100

    Change cmd

--- CI/deploy-back-office.sh
+++ CI/deploy-back-office.sh
@@ -9,7 +9,7 @@ set -e
 
 echo -e "Cleaning destination ..."
 CMD="'""echo '$SSH_PASS' | mkdir -p ${REMOTE_PATH} && cd ${REMOTE_PATH} && echo '$SSH_PASS' | rm -rf *""'"
-ssh -o strictHostKeyChecking=no -o PubkeyAuthentication=yes $CONNECTION "$CMD"
+ssh -oStrictHostKeyChecking=no -o PubkeyAuthentication=yes $CONNECTION "'"$CMD"'"
 
 echo -e "Synchronizing files ..."
 rsync -e "ssh -o StrictHostKeyChecking=no -o PubkeyAuthentication=yes" -avzr --delete $OUT/* $CONNECTION:$REMOTE_PATH
