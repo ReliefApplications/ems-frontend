b90a2d8f4b7ce4c92abebaf2951ff6882b1e88b4
adrianmujica <adrian@reliefapplications.org>
2021-02-04 08:02:13 +0000

    [SAFE-66] Refactor: added module to component to use MatMenu and MatIcon

---
+++ projects/who-ems/src/lib/components/widget-grid/dashboard-menu/dashboard-menu.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatMenuModule,
+    MatIconModule
+  ]
+})
+export class DashboardMenuModule { }
