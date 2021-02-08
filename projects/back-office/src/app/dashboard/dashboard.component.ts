eb7349f72ffb4545b380b82e35debd5b417de375
hugo <hugo.reliefapps@gmail.com>
2021-02-08 09:35:14 +0100

    [SAFE-101] text updated

--- projects/back-office/src/app/dashboard/dashboard.component.ts
+++ projects/back-office/src/app/dashboard/dashboard.component.ts
@@ -29,7 +29,7 @@ export class DashboardComponent implements OnInit {
       ]
     },
     {
-      name: 'Advanced settings',
+      name: 'Advanced assets',
       navItems: [
         {
           name: 'Forms',
