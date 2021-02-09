9bab3807d96fe03365de7f930afe211f6a5e3fbc
Antoine Hurard <antoine.reliefapps@gmail.com>
2021-02-09 09:25:51 +0100

    [SAFE-101] Change all labels of steps / pages addition to be h2

--- projects/back-office/src/app/dashboard/dashboard.component.ts
+++ projects/back-office/src/app/dashboard/dashboard.component.ts
@@ -29,7 +29,7 @@ export class DashboardComponent implements OnInit {
       ]
     },
     {
-      name: 'Advanced assets',
+      name: 'Advanced settings',
       navItems: [
         {
           name: 'Forms',
