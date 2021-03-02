1e471ebffa9c2447f57cb9d21693850f2ce224b7
Pacome Rivier <pacome@reliefapplications.org>
2021-03-02 12:11:50 +0100

    [SAFE-52] Add a canEdit default field in query builder

--- projects/who-ems/src/lib/services/query-builder.service.ts
+++ projects/who-ems/src/lib/services/query-builder.service.ts
@@ -5,7 +5,7 @@ import { GetQueryTypes, GET_QUERY_TYPES } from '../graphql/queries';
 import gql from 'graphql-tag';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
-const DEFAULT_FIELDS = ['id', 'createdAt', 'createdBy'];
+const DEFAULT_FIELDS = ['id', 'createdAt', 'createdBy', 'canEdit'];
 
 const DISABLED_FIELDS = ['createdBy'];
 
