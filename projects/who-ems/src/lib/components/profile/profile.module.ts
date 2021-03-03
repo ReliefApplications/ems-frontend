9946eff277daa25fcb4535aed49abfb8e02d7563
Antoine Hurard <antoine.reliefapps@gmail.com>
2021-03-03 18:01:58 +0200

    [SAFE-178] Start to set the profile page

--- projects/who-ems/src/lib/components/profile/profile.module.ts
+++ projects/who-ems/src/lib/components/profile/profile.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WhoProfileComponent } from './profile.component';
-
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [WhoProfileComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatIconModule
   ],
   exports: [WhoProfileComponent]
 })
