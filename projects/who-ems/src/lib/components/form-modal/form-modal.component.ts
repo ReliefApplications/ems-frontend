381143223945bf5e0f507cbd61d381531b9c8182
adrianmujica <adrian@reliefapplications.org>
2021-03-09 13:05:46 +0000

    [SAFE-204] Feat: now we can send empty data to backend if they are not required.

--- projects/who-ems/src/lib/components/form-modal/form-modal.component.ts
+++ projects/who-ems/src/lib/components/form-modal/form-modal.component.ts
@@ -95,6 +95,17 @@ export class WhoFormModalComponent implements OnInit {
   public completeMySurvey = (survey: any) => {
     const rowsSelected = Array.isArray(this.data.recordId) ? this.data.recordId.length : 1;
 
+    /* we can send to backend empty data if they are not required
+    */
+    const questions = survey.getAllQuestions();
+    const data = survey.data;
+    for (const question of questions) {
+      const key = question.getValueName();
+      if (!survey.data[key] && !question.isRequired) {
+        data[key] = null;
+      }
+    }
+    survey.data = data;
     const dialogRef = this.dialog.open(WhoConfirmModalComponent, {
       data: {
         title: `Update row${rowsSelected > 1 ? 's' : ''}`,
