5509157284a9310617bea113161ce90901136d5d
adrianmujica <adrian@reliefapplications.org>
2021-03-17 10:57:30 +0000

    [SAFE-123] fix: format

--- projects/back-office/src/app/dashboard/pages/resources/resources.module.ts
+++ projects/back-office/src/app/dashboard/pages/resources/resources.module.ts
@@ -13,7 +13,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
-import { FormsModule } from "@angular/forms";
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [ResourcesComponent],
