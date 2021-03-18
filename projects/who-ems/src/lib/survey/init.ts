0c8e69db155c5e34d4db7d683ff630e12d11cbee
Antoine Hurard <antoine.reliefapps@gmail.com>
2021-03-18 15:25:56 +0100

    Fix a bug where tagbox was appearing in new category

--- projects/who-ems/src/lib/survey/init.ts
+++ projects/who-ems/src/lib/survey/init.ts
@@ -1,3 +1,4 @@
+import * as widgets from 'surveyjs-widgets';
 import { init as initCountryComponent } from './components/country';
 import { init as initCountriesComponent } from './components/countries';
 import { init as initResourceComponent } from './components/resource';
@@ -11,6 +12,7 @@ import { init as initCustomWidget } from './widgets/customwidget';
 export function initCustomWidgets(Survey, API_URL): void {
   // initTooltipWidget(Survey);
   // initDateFormatWidget(Survey);
+  widgets.select2tagbox(Survey);
   initCountryComponent(Survey);
   initCountriesComponent(Survey);
   initResourceComponent(Survey, API_URL);
