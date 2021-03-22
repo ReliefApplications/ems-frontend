69dbf3c7ba8a0719e5cd7f57cb03a4b3f7e4ced6
Pacome Rivier <pacome@reliefapplications.org>
2021-03-22 17:50:26 +0100

    [SAFE-97] Support dynamic matrix in grid widget

--- projects/who-ems/src/lib/components/widgets/grid/grid.component.ts
+++ projects/who-ems/src/lib/components/widgets/grid/grid.component.ts
@@ -22,7 +22,8 @@ import { WhoRecordHistoryComponent } from '../../record-history/record-history.c
 import { LayoutService } from '../../../services/layout.service';
 import {
   Component, OnInit, OnChanges, OnDestroy, ViewChild, Input, Output, ComponentFactory, Renderer2,
-  ComponentFactoryResolver, EventEmitter } from '@angular/core';
+  ComponentFactoryResolver, EventEmitter
+} from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { WhoSnackBarService } from '../../../services/snackbar.service';
 import { WhoRecordModalComponent } from '../../record-modal/record-modal.component';
@@ -494,6 +495,23 @@ export class WhoGridComponent implements OnInit, OnChanges, OnDestroy {
             fieldGroup[row.name] = this.formBuilder.group(rowGroup);
           }
           formGroup[field.name] = this.formBuilder.group(fieldGroup);
+        }
+        if (field.meta.type === 'matrixdynamic') {
+          const fieldGroup = [];
+          const fieldValue = dataItem[field.name];
+          for (const rowValue of fieldValue) {
+            const rowGroup = {};
+            for (const column of field.meta.columns) {
+              const columnValue = rowValue ? rowValue[column.name] : null;
+              if (this.multiSelectTypes.includes(column.cellType)) {
+                rowGroup[column.name] = [columnValue];
+              } else {
+                rowGroup[column.name] = columnValue;
+              }
+            }
+            fieldGroup.push(this.formBuilder.group(rowGroup));
+          }
+          formGroup[field.name] = this.formBuilder.array(fieldGroup);
         }
       }
     }
