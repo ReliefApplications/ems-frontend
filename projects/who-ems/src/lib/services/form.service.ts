c1683680702b49f07adf793beab105ad56d9b408
Pacome Rivier <pacome@reliefapplications.org>
2021-04-09 17:32:34 +0200

    [SAFE-251] Add some console.log to help debugging

--- projects/who-ems/src/lib/services/form.service.ts
+++ projects/who-ems/src/lib/services/form.service.ts
@@ -3,7 +3,6 @@ import * as SurveyKo from 'survey-knockout';
 import * as Survey from 'survey-angular';
 import { initCreatorSettings } from '../survey/creator';
 import { initCustomWidgets } from '../survey/init';
-
 @Injectable({
   providedIn: 'root'
 })
@@ -16,7 +15,12 @@ export class FormService {
     // === CREATOR SETTINGS ===
     initCreatorSettings(SurveyKo);
 
-    // === CUSTOM WIDGETS / COMPONENTS ===
+    // === CUSTOM WIDGETS / COMPONENTS FOR SURVEY ===
     initCustomWidgets(Survey, `${environment.API_URL}/graphql`);
+
+    console.log(Survey.ComponentCollection.Instance.items);
+    console.log(Survey.Serializer.getAllClasses());
+    console.log(Survey.CustomWidgetCollection.Instance.widgets);
+    
   }
 }
