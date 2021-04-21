ef03049f36b0148ac8912f1bbd45ec364ea8cc3e
Antoine Hurard <antoine.reliefapps@gmail.com>
2021-04-21 15:28:47 +0300

    fix: configuration azure for web apps

--- projects/back-office/src/environments/environment.azure.ts
+++ projects/back-office/src/environments/environment.azure.ts
@@ -4,6 +4,6 @@ export const environment = {
     SUBSCRIPTION_API_URL: 'wss://safe-app.humanitarian.tech/api',
     clientId: 'a85e101e-e193-4a3f-8911-c6e89bc973e6',
     authority: 'https://login.microsoftonline.com/fbacd48d-ccf4-480d-baf0-31048368055f',
-    redirectUrl: 'https://safe-app.humanitarian.tech/backoffice',
-    postLogoutRedirectUri: 'https://safe-app.humanitarian.tech/backoffice/auth'
+    redirectUrl: 'https://safe-app.humanitarian.tech/backoffice/',
+    postLogoutRedirectUri: 'https://safe-app.humanitarian.tech/backoffice/auth/'
 };
