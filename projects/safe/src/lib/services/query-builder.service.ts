bafe2d32ec965afc538500022818ef9b8895e682
Antoine Hurard <antoine.reliefapps@gmail.com>
2021-04-22 17:59:52 +0300

    [SAFE-215] fix: lint error

--- projects/safe/src/lib/services/query-builder.service.ts
+++ projects/safe/src/lib/services/query-builder.service.ts
@@ -59,7 +59,7 @@ export class QueryBuilderService {
     return filter ? Object.keys(filter).reduce((o, key) => {
       if (filter[key] || filter[key] === false) {
         if (filter[key] === 'today()') {
-          return { ...o, [key]: new Date().toISOString().substring(0,10)};
+          return { ...o, [key]: new Date().toISOString().substring(0, 10)};
         }
         return { ...o, [key]: filter[key] };
       }
