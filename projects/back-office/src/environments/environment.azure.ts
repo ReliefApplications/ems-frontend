f189f59bc76cc132c75361fde33182333e22f1dc
Antoine Hurard <antoine.reliefapps@gmail.com>
2021-05-06 11:07:01 +0300

    correction azure redirect url for backoffice

--- projects/back-office/src/environments/environment.azure.ts
+++ projects/back-office/src/environments/environment.azure.ts
@@ -4,7 +4,7 @@ export const environment = {
     SUBSCRIPTION_API_URL: 'wss://ems-safe-test.who.int/api',
     clientId: '021202ac-d23b-4757-83e3-f6ecde12266b',
     authority: 'https://login.microsoftonline.com/f610c0b7-bd24-4b39-810b-3dc280afb590',
-    redirectUrl: 'https://ems-safe-test.who.int/backoffice',
+    redirectUrl: 'https://ems-safe-test.who.int/backoffice/',
     postLogoutRedirectUri: 'https://ems-safe-test.who.int/backoffice/auth',
     frontOfficeUri: 'https://ems-safe-test.who.int/',
     backOfficeUri: 'https://ems-safe-test.who.int/backoffice/',
