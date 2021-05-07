3bb8cb44ceda5f6353c68727b283759ed65538ae
tristan <tristan.le_goff@edu.devinci.fr>
2021-05-07 16:07:51 +0200

    [SAFE-279] add some ToolTip

--- projects/back-office/src/app/application/application.component.ts
+++ projects/back-office/src/app/application/application.component.ts
@@ -55,6 +55,7 @@ export class ApplicationComponent implements OnInit, OnDestroy {
                 orderable: true,
                 action: {
                   icon: 'delete',
+                  toolTip: 'Delete this display',
                   callback: () => this.onDelete(x)
                 }
               };
