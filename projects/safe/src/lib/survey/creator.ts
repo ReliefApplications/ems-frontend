b1b70c03bb28d5f2124ddb188173136b7a0ff847
tristan <tristan.le_goff@edu.devinci.fr>
2021-06-02 16:39:18 +0200

    [SAFE-343] switch name and value name in form builder

--- projects/safe/src/lib/survey/creator.ts
+++ projects/safe/src/lib/survey/creator.ts
@@ -1,12 +1,12 @@
 /*  Edit general settings of SurveyJS
 */
 export function initCreatorSettings(survey: any): void {
-    survey.Serializer.findProperty('question', 'name').readOnly = true;
-    survey.Serializer.findProperty('question', 'name').dependsOn = 'valueName';
-    survey.Serializer.findProperty('question', 'name').onGetValue = (obj: any) => {
-        return obj.valueName ? obj.valueName : obj.name;
+    survey.Serializer.findProperty('question', 'valueName').readOnly = true;
+    survey.Serializer.findProperty('question', 'valueName').dependsOn = 'name';
+    survey.Serializer.findProperty('question', 'valueName').onGetValue = (obj: any) => {
+        return obj.name ? obj.name : obj.valueName;
     };
-    survey.Serializer.findProperty('question', 'valueName').isRequired = true;
+    survey.Serializer.findProperty('question', 'name').isRequired = true;
     survey.Serializer.findProperty('file', 'storeDataAsText').onGetValue = (obj: any) => {
         return false;
     };
