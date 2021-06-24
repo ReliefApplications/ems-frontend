d6c71ffa79a051056ee392bf940f56eb13c0f91c
MartinGbz <martin.grabarz@outlook.fr>
2021-06-24 17:08:19 +0200

    [SAFE-399] code cleaning

--- projects/back-office/src/app/dashboard/pages/forms/forms.component.ts
+++ projects/back-office/src/app/dashboard/pages/forms/forms.component.ts
@@ -230,21 +230,6 @@ export class FormsComponent implements OnInit, OnDestroy, AfterViewInit {
   }
 
   copyStringToClipboard(str: any): void {
-    // // Create new element
-    // const el = document.createElement('textarea');
-    // // Set value (string to be copied)
-    // el.value = str;
-    // // Set non-editable to avoid focus and move outside of view
-    // el.setAttribute('readonly', '');
-    // el.style = {position: 'absolute', left: '-9999px'};
-    // document.body.appendChild(el);
-    // // Select text inside element
-    // el.select();
-    // // Copy text to clipboard
-    // document.execCommand('copy');
-    // // Remove temporary element
-    // document.body.removeChild(el);
-
     const input = document.body.appendChild(document.createElement('input'));
     input.value = str;
     input.focus();
