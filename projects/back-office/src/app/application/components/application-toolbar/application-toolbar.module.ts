2fd887709066a003f0c9d806a7e35a94df3e7aab
adrianmujica <adrian@reliefapplications.org>
2021-07-06 13:20:11 +0100

    [SAFE-414] change: application-toolbar component button by safe-button

--- projects/back-office/src/app/application/components/application-toolbar/application-toolbar.module.ts
+++ projects/back-office/src/app/application/components/application-toolbar/application-toolbar.module.ts
@@ -3,18 +3,19 @@ import { CommonModule } from '@angular/common';
 import { ApplicationToolbarComponent } from './application-toolbar.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
-import { SafeConfirmModalModule } from '@safe/builder';
+import { SafeConfirmModalModule, SafeButtonModule } from '@safe/builder';
 import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [ApplicationToolbarComponent],
-  imports: [
-    CommonModule,
-    MatIconModule,
-    MatButtonModule,
-    MatDividerModule,
-    SafeConfirmModalModule
-  ],
+    imports: [
+        CommonModule,
+        MatIconModule,
+        MatButtonModule,
+        MatDividerModule,
+        SafeConfirmModalModule,
+        SafeButtonModule
+    ],
   exports: [ApplicationToolbarComponent]
 })
 export class ApplicationToolbarModule { }
