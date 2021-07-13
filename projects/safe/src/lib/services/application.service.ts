f0cdbcbca3a72cea9d3638224af082d170afe98b
hugo <hugo.reliefapps@gmail.com>
2021-07-13 17:40:40 +0200

    [SAFE-375] fix ng lint

--- projects/safe/src/lib/services/application.service.ts
+++ projects/safe/src/lib/services/application.service.ts
@@ -279,7 +279,7 @@ export class SafeApplicationService {
   /* Add a new page to the opened application.
   */
   addPage(value: any): void {
-    console.log("value = ", value);
+    console.log('value = ', value);
     // do a trick to send value.type as a ContentType instead of a String
     const application = this._application.getValue();
     if (application && this.isUnlocked) {
