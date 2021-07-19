12ed74ee88e2bcdd0b59cced4c2ef72d6ff62a3d
hugo <hugo.reliefapps@gmail.com>
2021-07-19 16:07:31 +0200

    remove artifacts

--- projects/safe/src/lib/services/application.service.ts
+++ projects/safe/src/lib/services/application.service.ts
@@ -279,8 +279,6 @@ export class SafeApplicationService {
   /* Add a new page to the opened application.
   */
   addPage(value: any): void {
-    console.log('value = ', value);
-    // do a trick to send value.type as a ContentType instead of a String
     const application = this._application.getValue();
     if (application && this.isUnlocked) {
       this.apollo.mutate<AddPageMutationResponse>({
