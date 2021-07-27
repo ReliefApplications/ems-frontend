a0b30c920556769a4961f832ae1159d11c0ab26e
Antoine Hurard <antoine.reliefapps@gmail.com>
2021-07-27 10:08:58 +0200

    add wv config

---
+++ projects/front-office/src/environments/environment.wv.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: true,
+    API_URL: 'https://api.somrep.humanitarian.tech',
+    SUBSCRIPTION_API_URL: 'wss://api.somrep.humanitarian.tech',
+    clientId: 'a85e101e-e193-4a3f-8911-c6e89bc973e6',
+    authority: 'https://login.microsoftonline.com/common',
+    redirectUrl: 'https://app.somrep.humanitarian.tech',
+    postLogoutRedirectUri: 'https://app.somrep.humanitarian.tech/auth',
+    frontOfficeUri: 'https://app.somrep.humanitarian.tech/',
+    backOfficeUri: 'https://admin.somrep.humanitarian.tech/',
+    module: 'frontoffice'
+};
