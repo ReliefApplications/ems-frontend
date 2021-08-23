76d60f19fe871e9278f3e90742c94cb5e4937345
Antoine Hurard <antoine.reliefapps@gmail.com>
2021-08-23 15:24:48 +0200

    update client id for prod

--- projects/front-office/src/environments/environment.azure.prod.ts
+++ projects/front-office/src/environments/environment.azure.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: true,
     API_URL: 'https://ems-safe.who.int/api',
     SUBSCRIPTION_API_URL: 'wss://ems-safe.who.int/api',
-    clientId: '021202ac-d23b-4757-83e3-f6ecde12266b',
+    clientId: '8e237c86-3d84-4dda-b38d-b92031d77af1',
     authority: 'https://login.microsoftonline.com/f610c0b7-bd24-4b39-810b-3dc280afb590',
     redirectUrl: 'https://ems-safe.who.int',
     postLogoutRedirectUri: 'https://ems-safe.who.int/auth',
