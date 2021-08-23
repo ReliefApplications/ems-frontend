5520688cc428ca9d0305a6d4446496e7c4b1ff63
Antoine Hurard <antoine.reliefapps@gmail.com>
2021-08-23 16:48:47 +0200

    [SAFE-414] fix icon size in safe button

---
+++ projects/safe/src/lib/components/ui/icon/icon.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'safe-icon',
+  templateUrl: './icon.component.html',
+  styleUrls: ['./icon.component.scss']
+})
+export class SafeIconComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ projects/safe/src/lib/components/ui/icon/icon.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SafeIconComponent } from './icon.component';
+
+@NgModule({
+  declarations: [
+    SafeIconComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    SafeIconComponent
+  ]
+})
+export class SafeIconModule { }
