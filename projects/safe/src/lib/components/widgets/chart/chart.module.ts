4777ec081ca23f4ffd736de63fcd0d8bc72982fe
Antoine Hurard <antoine.reliefapps@gmail.com>
2021-09-06 16:51:16 +0200

    [SAFE-481] edit login buttons

--- projects/safe/src/lib/components/widgets/chart/chart.module.ts
+++ projects/safe/src/lib/components/widgets/chart/chart.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SafeChartComponent } from './chart.component';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
 import { SafeButtonModule } from '../../ui/button/button.module';
 import { SafeDonutChartModule } from '../../ui/donut-chart/donut-chart.module';
 import { SafePieChartModule } from '../../ui/pie-chart/pie-chart.module';
