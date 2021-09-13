70efe451740a83015c048673b109e0f08318a36d
Antoine Hurard <antoine.reliefapps@gmail.com>
2021-09-13 11:40:11 +0200

    [SAFE-387] add edition and remove

--- projects/safe/src/lib/components/users/components/invite-users/invite-users.component.ts
+++ projects/safe/src/lib/components/users/components/invite-users/invite-users.component.ts
@@ -1,8 +1,9 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, ViewChild } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { GridDataResult } from '@progress/kendo-angular-grid';
+import { GridComponent, GridDataResult } from '@progress/kendo-angular-grid';
 import { Role } from '../../../../models/user.model';
 import { PositionAttributeCategory } from '../../../../models/position-attribute-category.model';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 interface DialogData {
   roles: Role[];
@@ -18,14 +19,20 @@ interface DialogData {
 export class SafeInviteUsersComponent implements OnInit {
 
   public gridData: GridDataResult = { data: [], total: 0 };
+  public formGroup: FormGroup = new FormGroup({});
+  private editedRowIndex = 0;
+
+  // === TEMPLATE REFERENCE TO KENDO GRID ===
+  @ViewChild(GridComponent)
+  private grid?: GridComponent;
 
   constructor(
+    private formBuilder: FormBuilder,
     public dialogRef: MatDialogRef<SafeInviteUsersComponent>,
     @Inject(MAT_DIALOG_DATA) public data: DialogData
   ) { }
 
   ngOnInit(): void {
-    console.log(this.data);
     this.gridData = {
       data: [{
         email: 'test@gmail.com',
@@ -40,5 +47,39 @@ export class SafeInviteUsersComponent implements OnInit {
    */
   onClose(): void {
     this.dialogRef.close();
+  }
+
+  onAdd(): void { }
+
+  onRemove(index: number): void {
+    this.gridData.data.splice(index, 1);
+  }
+
+  onUpload(): void { }
+
+  onDownload(): void { }
+
+  public cellClickHandler({ isEdited, dataItem, rowIndex }: any): void {
+    this.formGroup = this.createFormGroup(dataItem);
+    this.editedRowIndex = rowIndex;
+    this.grid?.editRow(rowIndex, this.formGroup);
+  }
+
+  public createFormGroup(dataItem: any): FormGroup {
+    const formGroup: any = {
+      email: [dataItem.email, Validators.required],
+      roles: [dataItem.roles, Validators.required]
+    };
+    // if ((this.data.positionAttributeCategories || []).length > 0) {
+    //   formGroup.attributes = 
+    // }
+    return this.formBuilder.group(formGroup);
+  }
+
+  private closeEditor(): void {
+    this.grid?.closeRow(this.editedRowIndex);
+    this.grid?.cancelCell();
+    this.editedRowIndex = 0;
+    this.formGroup = new FormGroup({});
   }
 }
--- projects/safe/src/lib/components/users/components/invite-users/invite-users.module.ts
+++ projects/safe/src/lib/components/users/components/invite-users/invite-users.module.ts
@@ -4,6 +4,9 @@ import { SafeInviteUsersComponent } from './invite-users.component';
 import { GridModule, GroupModule } from '@progress/kendo-angular-grid';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
+import { DropDownsModule } from '@progress/kendo-angular-dropdowns';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ButtonModule, ButtonsModule } from '@progress/kendo-angular-buttons';
 
 @NgModule({
   declarations: [
@@ -11,10 +14,15 @@ import { MatDialogModule } from '@angular/material/dialog';
   ],
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     GridModule,
     GroupModule,
     MatButtonModule,
-    MatDialogModule
+    MatDialogModule,
+    DropDownsModule,
+    ButtonModule,
+    ButtonsModule
   ],
   exports: [SafeInviteUsersComponent]
 })
