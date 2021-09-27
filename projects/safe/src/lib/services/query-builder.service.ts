e3fa3157f54a0032901f09cdd4d7c3a397770873
Antoine Hurard <antoine.reliefapps@gmail.com>
2021-09-27 16:49:26 +0200

    Revert "clear some parts of code"
    This reverts commit b2b10065f72de3979bca10724804a2cf8763b4c8.

--- projects/safe/src/lib/services/query-builder.service.ts
+++ projects/safe/src/lib/services/query-builder.service.ts
@@ -145,7 +145,10 @@ export class QueryBuilderService {
           sortOrder: "${builtQuery.sort?.order || '' }",
           filter: ${this.objToString(this.buildFilter(builtQuery.filter))}
           ) {
-            ${fields}
+            result {
+              ${fields}
+            }
+            count
         }
         }
       `;
