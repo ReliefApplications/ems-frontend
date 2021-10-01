969bd6028e796dcbee5d6e274ef581cb1f9589af
MartinGbz <martin.grabarz@outlook.fr>
2021-10-01 12:55:20 +0200

    quick commit to fix bug after

--- projects/back-office/src/app/application/pages/users/users.component.ts
+++ projects/back-office/src/app/application/pages/users/users.component.ts
@@ -3,6 +3,8 @@ import { MatTableDataSource } from '@angular/material/table';
 import { Application, User, Role, SafeApplicationService, PositionAttributeCategory} from '@safe/builder';
 import { Subscription } from 'rxjs';
 
+const ITEMS_PER_PAGE = 10;
+
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
@@ -13,17 +15,28 @@ export class UsersComponent implements OnInit, OnDestroy {
   // === DATA ===
   public loading = true;
   public users = new MatTableDataSource<User>([]);
+  public cachedUsers: User[] = [];
   public roles: Role[] = [];
   public positionAttributeCategories: PositionAttributeCategory[] = [];
   private applicationSubscription?: Subscription;
+
+  public pageInfo = {
+    pageIndex: 0,
+    pageSize: ITEMS_PER_PAGE,
+    length: 0,
+    endCursor: ''
+  };
 
   constructor(
     public applicationService: SafeApplicationService
   ) { }
 
   ngOnInit(): void {
+    console.log('YSL');
     this.loading = false;
     this.applicationSubscription = this.applicationService.application.subscribe((application: Application | null) => {
+      console.log('application');
+      console.log(application);
       if (application) {
         this.users.data = application.users || [];
         this.roles = application.roles || [];
