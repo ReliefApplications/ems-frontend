30f5fcfd10d3d570bfa636e47a88bf41b81f43da
Antoine Hurard <antoine.reliefapps@gmail.com>
2021-10-01 17:51:49 +0200

    few edits of content type choice

--- projects/safe/src/lib/components/content-choice/content-choice.component.ts
+++ projects/safe/src/lib/components/content-choice/content-choice.component.ts
@@ -1,18 +1,51 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, forwardRef, Input, OnInit, Provider } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { IContentType } from '../../models/page.model';
+
+const CONTROL_VALUE_ACCESSOR: Provider = {
+  provide: NG_VALUE_ACCESSOR,
+  useExisting: forwardRef(() => SafeContentChoiceComponent),
+  multi: true,
+};
 
 @Component({
   selector: 'safe-content-choice',
   templateUrl: './content-choice.component.html',
-  styleUrls: ['./content-choice.component.scss']
+  styleUrls: ['./content-choice.component.scss'],
+  providers: [CONTROL_VALUE_ACCESSOR]
 })
-export class SafeContentChoiceComponent implements OnInit {
+export class SafeContentChoiceComponent implements OnInit, ControlValueAccessor {
 
-  @Input() contentTypes?: any;
-  @Input() form?: any;
-  @Input() assetsPath = '';
+  @Input() contentTypes?: IContentType[];
+
+  selected!: string;
+  disabled = false;
+  private onTouched!: any;
+  private onChanged!: any;
 
   constructor() { }
 
   ngOnInit(): void {}
 
+  public onSelect(value: string): void {
+    this.onTouched();
+    this.selected = value;
+    this.onChanged(value);
+  }
+
+  public writeValue(value: string): void {
+    this.selected = value;
+  }
+
+  public registerOnChange(fn: any): void {
+    this.onChanged = fn;
+  }
+
+  public registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  public setDisabledState(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
 }
--- projects/safe/src/lib/components/content-choice/content-choice.module.ts
+++ projects/safe/src/lib/components/content-choice/content-choice.module.ts
@@ -1,15 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatRadioModule } from '@angular/material/radio';
 import { MatRippleModule } from '@angular/material/core';
-import { MatDialogModule } from '@angular/material/dialog';
 import { SafeContentChoiceComponent } from './content-choice.component';
-
-
 
 @NgModule({
   declarations: [SafeContentChoiceComponent],
@@ -17,12 +11,8 @@ import { SafeContentChoiceComponent } from './content-choice.component';
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
-    MatInputModule,
     MatFormFieldModule,
-    MatSelectModule,
-    MatRadioModule,
-    MatDialogModule,
-    MatRippleModule,
+    MatRippleModule
   ],
   exports: [SafeContentChoiceComponent]
 })
