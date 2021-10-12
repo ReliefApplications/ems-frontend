90a8185d3c9c69af1f93386f5f4a63799ac28724
MartinGbz <martin.grabarz@outlook.fr>
2021-10-12 13:11:05 +0200

    add click event + cursor pointer

--- projects/safe/src/lib/components/ui/new-app-button/new-app-button.component.ts
+++ projects/safe/src/lib/components/ui/new-app-button/new-app-button.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'safe-new-app-button',
   templateUrl: './new-app-button.component.html',
   styleUrls: ['./new-app-button.component.scss']
 })
+
 export class NewAppButtonComponent implements OnInit {
+
+  @Output() onClickEvent: EventEmitter<any> = new EventEmitter();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onClick($event: any): void {
+    console.log('click');
+    this.onClickEvent.emit();
+  }
 }
