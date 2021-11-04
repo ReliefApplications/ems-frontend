17f0f8ee21dadabe3cfc923f4175d13a4e1549d3
Antoine Hurard <antoine.reliefapps@gmail.com>
2021-11-04 12:10:45 +0100

    add aspas env

---
+++ projects/back-office/src/environments/environment.aspas.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  API_URL: 'https://aspas.oortcloud.tech/api',
+  SUBSCRIPTION_API_URL: 'wss://aspas.oortcloud.tech',
+  clientId: '79d8fe89-9ada-46c6-96b5-5bbaa0124a5c',
+  authority: 'https://login.microsoftonline.com/common',
+  redirectUrl: 'https://aspas.oortcloud.tech/admin/',
+  postLogoutRedirectUri: 'https://aspas.oortcloud.tech/admin/auth',
+  frontOfficeUri: 'https://aspas.oortcloud.tech/',
+  backOfficeUri: 'https://aspas.oortcloud.tech/admin/',
+  module: 'frontoffice'
+};
