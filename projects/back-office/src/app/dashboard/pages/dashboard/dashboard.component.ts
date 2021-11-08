b03f6e0e6ac49616fcecc08ef51d9ec516b01501
Antoine Hurard <antoine.reliefapps@gmail.com>
2021-11-08 14:37:55 +0100

    added a few comments #11793

--- projects/back-office/src/app/dashboard/pages/dashboard/dashboard.component.ts
+++ projects/back-office/src/app/dashboard/pages/dashboard/dashboard.component.ts
@@ -104,12 +104,18 @@ export class DashboardComponent implements OnInit, OnDestroy {
     this.autoSaveChanges();
   }
 
-  /*  Edit the settings or display of a widget.
+ /**
+  * Edits the settings or display of a widget.
+  * @param e widget to save.
   */
   onEditTile(e: any): void {
-    const oId = this.tiles.findIndex((v: any) => v.id === e.id);
-    const options = this.tiles[oId]?.settings?.defaultLayout ?
-      {...e.options, defaultLayout: this.tiles[oId].settings.defaultLayout} : e.options;
+    // make sure that we save the default layout.
+    const index = this.tiles.findIndex((v: any) => v.id === e.id);
+    const options = this.tiles[index]?.settings?.defaultLayout ?
+      {
+        ...e.options,
+        defaultLayout: this.tiles[index].settings.defaultLayout
+      } : e.options;
     if (options) {
       switch (e.type) {
         case 'display': {
