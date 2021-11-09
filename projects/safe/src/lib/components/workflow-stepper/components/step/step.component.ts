751d11e2fb34ccc2f94dd8b926c59bc3c6161cf3
Antoine Hurard <antoine.reliefapps@gmail.com>
2021-11-09 11:25:23 +0100

    create safe stepper sub-components #11055

---
+++ projects/safe/src/lib/components/workflow-stepper/components/step/step.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'safe-step',
+  templateUrl: './step.component.html',
+  styleUrls: ['./step.component.scss']
+})
+export class SafeStepComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- projects/safe/src/lib/components/workflow-stepper/workflow-stepper.module.ts
+++ projects/safe/src/lib/components/workflow-stepper/workflow-stepper.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SafeWorkflowStepperComponent } from './workflow-stepper.component';
+import { SafeAddStepComponent } from './components/add-step/add-step.component';
+import { SafeStepComponent } from './components/step/step.component';
 
 @NgModule({
   declarations: [
-    SafeWorkflowStepperComponent
+    SafeWorkflowStepperComponent,
+    SafeAddStepComponent,
+    SafeStepComponent
   ],
   imports: [
     CommonModule
