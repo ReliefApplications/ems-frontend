44f1a2f22e01229c76442607ea7fa6959ecaca56
Antoine Hurard <antoine.reliefapps@gmail.com>
2021-11-29 11:56:01 +0100

    start to implement the changes for the grid

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,7 @@
 import { setCompodocJson } from "@storybook/addon-docs/angular";
 import docJson from "../documentation.json";
 setCompodocJson(docJson);
+import '@angular/localize/init';
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
