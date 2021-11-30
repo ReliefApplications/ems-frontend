00b0382739ebddf202f4aa931fd57bc8d2ae56dd
Antoine Hurard <antoine.reliefapps@gmail.com>
2021-11-30 16:34:12 +0100

    remove useless import in grid widget module

--- projects/safe/src/lib/components/widgets/grid/grid.module.ts
+++ projects/safe/src/lib/components/widgets/grid/grid.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SafeGridWidgetComponent } from './grid.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SafeFormModalModule } from '../../form-modal/form-modal.module';
 import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatTooltipModule } from '@angular/material/tooltip';
 import { SafeChooseRecordModalModule } from '../../choose-record-modal/choose-record-modal.module';
 import { SafeCoreGridModule } from '../../ui/core-grid/core-grid.module';
 
@@ -15,13 +12,9 @@ import { SafeCoreGridModule } from '../../ui/core-grid/core-grid.module';
   ],
   imports: [
     CommonModule,
-    FormsModule,
-    ReactiveFormsModule,
     SafeFormModalModule,
     MatButtonModule,
-    MatIconModule,
     SafeChooseRecordModalModule,
-    MatTooltipModule,
     SafeCoreGridModule
   ],
   exports: [SafeGridWidgetComponent]
