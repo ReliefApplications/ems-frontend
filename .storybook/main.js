45ca132200055fcc348c603f2227bb08ec301ff0
Antoine Hurard <antoine.reliefapps@gmail.com>
2021-12-14 15:22:12 +0100

    update storybook packages #18422

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,14 @@
 module.exports = {
-  "stories": ["../stories/**/*.stories.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)", '../projects/**/*.stories.ts'],
-  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-knobs"],
+  "stories": [
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
+    "../projects/**/*.stories.ts"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-knobs"
+  ],
   core: {
     builder: "webpack5"
   }
