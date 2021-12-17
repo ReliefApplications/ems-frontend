d50180d4eb6bcef6342935500fe3d446368ee090
Antoine Hurard <antoine.reliefapps@gmail.com>
2021-12-17 15:29:56 +0100

    edit env for web elem

---
+++ projects/web-element/src/environments/environment.oort.dev.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://oort-dev.oortcloud.tech/api',
+    subscriptionApiUrl: 'wss://oort-dev.oortcloud.tech/api',
+    clientId: 'a85e101e-e193-4a3f-8911-c6e89bc973e6',
+    authority: 'https://login.microsoftonline.com/common',
+    redirectUrl: 'https://oort-dev.oortcloud.tech/web-element/',
+    postLogoutRedirectUri: 'https://oort-dev.oortcloud.tech/web-element/auth',
+};
