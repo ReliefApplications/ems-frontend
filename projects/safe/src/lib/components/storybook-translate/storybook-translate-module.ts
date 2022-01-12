045bd782dcc058e9d48a289ba43ddbaaf8bb8b8d
Maximo-reliefapps <maximo.reliefapps@gmail.com>
2022-01-12 16:43:29 +0100

    Storybook now shows translations

--- projects/safe/src/lib/components/storybook-translate/storybook-translate-module.ts
+++ projects/safe/src/lib/components/storybook-translate/storybook-translate-module.ts
@@ -1,13 +1,34 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { TranslateModule, TranslateService, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 const CONFIG = undefined;
 
 /**
   A utility module adding I18N support for Storybook stories
  **/
+
+
+  /**
+   * Sets up translator.
+   *
+   * @param http http client
+   * @returns Translator.
+   */
+  export const httpTranslateLoader = (http: HttpClient) => new TranslateHttpLoader(http);
   @NgModule({
-    imports: [TranslateModule.forRoot(CONFIG), TranslateModule],
+    imports: [
+      HttpClientModule,
+      TranslateModule.forRoot(
+      {
+        loader: {
+        provide: TranslateLoader,
+        useFactory: httpTranslateLoader,
+        deps: [HttpClient]
+      }
+    }
+      ), TranslateModule],
   })
   export class StorybookTranslateModule {
     constructor(translateService: TranslateService) {
