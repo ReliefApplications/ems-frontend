c92831191671258874f09162de640b9b6822b11f
Antoine Hurard <antoine.reliefapps@gmail.com>
2022-01-27 17:13:58 +0100

    remove useless import

--- projects/safe/src/lib/components/widgets/grid/grid.module.ts
+++ projects/safe/src/lib/components/widgets/grid/grid.module.ts
@@ -6,7 +6,6 @@ import { MatButtonModule } from '@angular/material/button';
 import { SafeChooseRecordModalModule } from '../../choose-record-modal/choose-record-modal.module';
 import { SafeCoreGridModule } from '../../ui/core-grid/core-grid.module';
 import { LayoutModule } from '@progress/kendo-angular-layout';
-import { SafeBadgeModule } from '../../ui/badge/badge.module';
 
 @NgModule({
   declarations: [SafeGridWidgetComponent],
@@ -17,7 +16,6 @@ import { SafeBadgeModule } from '../../ui/badge/badge.module';
     SafeChooseRecordModalModule,
     SafeCoreGridModule,
     LayoutModule,
-    SafeBadgeModule,
   ],
   exports: [SafeGridWidgetComponent],
 })
