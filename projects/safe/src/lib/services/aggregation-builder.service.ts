c355796ad5ff09157f53fe65020015903ad295aa
Pacôme Rivier <pacome@reliefapplications.org>
2022-02-04 17:29:37 +0100

    feta: move pipeline logic to the back

--- projects/safe/src/lib/services/aggregation-builder.service.ts
+++ projects/safe/src/lib/services/aggregation-builder.service.ts
@@ -29,103 +29,24 @@ export class AggregationBuilderService {
    */
   public buildAggregation(aggregation: any, withMapping = true): any {
     if (aggregation) {
-      const pipeline = this.buildPipeline(aggregation, withMapping);
       const query = gql`
-        query GetCustomAggregation($pipeline: JSON!) {
-          recordsAggregation(pipeline: $pipeline)
+        query GetCustomAggregation($aggregation: JSON!, $withMapping: Boolean) {
+          recordsAggregation(
+            aggregation: $aggregation
+            withMapping: $withMapping
+          )
         }
       `;
       return this.apollo.watchQuery<any>({
         query,
         variables: {
-          pipeline: JSON.parse(pipeline),
+          aggregation,
+          withMapping,
         },
       });
     } else {
       return null;
     }
-  }
-
-  /**
-   * Builds the pipeline string from settings
-   *
-   * @param settings Form value.
-   * @param withMapping Wether if we should inculde the mapping in the pipeline.
-   * @returns Pipeline string.
-   */
-  public buildPipeline(settings: any, withMapping = true): string {
-    const pipeline: any[] = [];
-    if (settings.dataSource) {
-      pipeline.push({
-        $match: {
-          $or: [
-            { resource: { $oid: settings.dataSource } },
-            { form: { $oid: settings.dataSource } },
-          ],
-        },
-      });
-    }
-    if (settings.sourceFields && settings.sourceFields.length) {
-      pipeline.push({
-        $project: (settings.sourceFields as any[]).reduce(
-          (o, field) =>
-            Object.assign(o, {
-              [field]: `$data.${field}`,
-            }),
-          {}
-        ),
-      });
-    }
-    if (settings.pipeline && settings.pipeline.length) {
-      for (const stage of settings.pipeline) {
-        switch (stage.type) {
-          case StageType.FILTER: {
-            // GET FILTERS FROM BACK END FUNCTION ??? PASS IT AS PARAMETER ?
-            // pipeline.push({
-            //   $match: {},
-            // });
-            break;
-          }
-          case StageType.SORT: {
-            pipeline.push({
-              $sort: {
-                [stage.form.field]: stage.form.order === 'asc' ? 1 : -1,
-              },
-            });
-            break;
-          }
-          case StageType.GROUP: {
-            //TO DO
-            break;
-          }
-          case StageType.ADD_FIELDS: {
-            //TO DO
-            break;
-          }
-          case StageType.UNWIND: {
-            //TO DO
-            break;
-          }
-          case StageType.CUSTOM: {
-            //TO DO
-            break;
-          }
-          default: {
-            break;
-          }
-        }
-      }
-    }
-    if (withMapping && settings.mapping) {
-      pipeline.push({
-        $project: {
-          category: `$${settings.mapping.xAxis}`,
-          field: `$${settings.mapping.yAxis}`,
-          id: '$_id',
-        },
-      });
-    }
-    return JSON.stringify(pipeline);
   }
 
   /**
