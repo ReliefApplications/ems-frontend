d4fcac1424adabed23b5e4c601543049e50e5a0b
Antoine Hurard <antoine.reliefapps@gmail.com>
2022-02-08 11:00:39 +0100

    fix: time inputs now have correct value on grid #24068

--- projects/safe/src/lib/components/ui/core-grid/core-grid.component.ts
+++ projects/safe/src/lib/components/ui/core-grid/core-grid.component.ts
@@ -327,14 +327,14 @@ export class SafeCoreGridComponent implements OnInit, OnChanges, OnDestroy {
    */
   private convertDateFields(items: any[]): void {
     const dateFields = this.fields
-      .filter((x) => ['Date', 'DateTime', 'Time'].includes(x.type))
+      .filter((x) => ['Date', 'DateTime'].includes(x.type))
       .map((x) => x.name);
     const timeFields = this.fields
       .filter((x) => ['Time'].includes(x.type))
       .map((x) => x.name);
     items.map((x) => {
       for (const [key, value] of Object.entries(x)) {
-        if (dateFields.includes(key)) {
+        if (dateFields.includes(key) || timeFields.includes(key)) {
           x[key] = x[key] && new Date(x[key]);
           if (timeFields.includes(key)) {
             x[key] =
