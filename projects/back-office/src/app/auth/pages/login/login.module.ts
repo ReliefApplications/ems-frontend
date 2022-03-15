2b1970c9530c01d839ee33d521baaee84f68c5ef
Maximo-reliefapps <maximo.reliefapps@gmail.com>
2022-03-15 16:35:08 +0100

    Login page content removed and changed for a spinner

--- projects/back-office/src/app/auth/pages/login/login.module.ts
+++ projects/back-office/src/app/auth/pages/login/login.module.ts
@@ -2,18 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { SafeButtonModule } from '@safe/builder';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [LoginComponent],
   imports: [
     CommonModule,
-    MatFormFieldModule,
     LoginRoutingModule,
-    MatGridListModule,
-    SafeButtonModule,
+    MatProgressSpinnerModule
   ],
   exports: [LoginComponent],
 })
