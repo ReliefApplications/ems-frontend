bbb4662542ad8f368474a7b0361ef005c3440af6
Maximo-reliefapps <maximo.reliefapps@gmail.com>
2022-03-29 14:46:10 +0200

    Added an export pdf option to scheduler widget

--- projects/safe/src/lib/components/widgets/scheduler/scheduler.component.ts
+++ projects/safe/src/lib/components/widgets/scheduler/scheduler.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { SchedulerEvent } from '@progress/kendo-angular-scheduler';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
+import { SchedulerEvent } from "@progress/kendo-angular-scheduler";
 import { MOCKED_EVENTS } from './MOCK_EVENTS';
+import { SafeDownloadService } from '../../../services/download.service';
 
 /**
  * Scheduler widget using KendoUI.
@@ -13,6 +14,7 @@ import { MOCKED_EVENTS } from './MOCK_EVENTS';
 export class SafeSchedulerComponent implements OnInit {
   // === SCHEDULER ===
   public selectedDate: Date = new Date();
+  @ViewChild("scheduler") public scheduler: any;
 
   // === DATA ===
   public events: SchedulerEvent[] = [];
@@ -22,11 +24,21 @@ export class SafeSchedulerComponent implements OnInit {
   @Input() header = true;
   @Input() settings: any = null;
 
+  constructor(public downloadService: SafeDownloadService) {}
+
   /**
    * Init the widget.
    */
   ngOnInit(): void {
     this.events = MOCKED_EVENTS;
     this.loading = false;
+  }
+
+  /**
+   * Exports the scheduler as a pdf.
+   */
+  public exportPDF() {
+    this.scheduler?.saveAsPDF();
+    return;
   }
 }
--- projects/safe/src/lib/components/widgets/scheduler/scheduler.module.ts
+++ projects/safe/src/lib/components/widgets/scheduler/scheduler.module.ts
@@ -3,13 +3,23 @@ import { CommonModule } from '@angular/common';
 import { SafeSchedulerComponent } from './scheduler.component';
 import { SchedulerModule } from '@progress/kendo-angular-scheduler';
 import { TranslateModule } from '@ngx-translate/core';
+import { PDFModule } from '@progress/kendo-angular-scheduler';
+import { MatMenuModule } from '@angular/material/menu';
+import { ButtonModule } from '@progress/kendo-angular-buttons';
 
 /**
  * Scheduler Widget Module.
  */
 @NgModule({
   declarations: [SafeSchedulerComponent],
-  imports: [CommonModule, SchedulerModule, TranslateModule],
+  imports: [
+    CommonModule,
+    SchedulerModule,
+    TranslateModule,
+    PDFModule,
+    MatMenuModule,
+    ButtonModule
+  ],
   exports: [SafeSchedulerComponent],
 })
 export class SafeSchedulerModule {}
