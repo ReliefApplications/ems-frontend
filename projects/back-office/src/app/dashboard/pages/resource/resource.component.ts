c0e645ab6a873829325e95f6c70ff2948b2d10b1
Antoine Hurard <antoine.reliefapps@gmail.com>
2022-03-30 15:59:57 +0200

    fix some issues with i18n

--- projects/back-office/src/app/dashboard/pages/resource/resource.component.ts
+++ projects/back-office/src/app/dashboard/pages/resource/resource.component.ts
@@ -254,9 +254,12 @@ export class ResourceComponent implements OnInit, OnDestroy {
           title: this.translate.instant('common.deleteObject', {
             name: this.translate.instant('common.record.one'),
           }),
-          content: this.translate.instant('components.record.delete.confirmationMessage', {
-            name: element.name,
-          }),
+          content: this.translate.instant(
+            'components.record.delete.confirmationMessage',
+            {
+              name: element.name,
+            }
+          ),
           confirmText: this.translate.instant('common.delete'),
           cancelText: this.translate.instant('common.cancel'),
         },
