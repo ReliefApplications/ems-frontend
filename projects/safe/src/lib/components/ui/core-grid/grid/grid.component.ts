103782081f69d9ec402eafc5a7253999a3bf660a
Maximo-reliefapps <maximo.reliefapps@gmail.com>
2022-03-31 16:29:36 +0200

    Added number support for colored tiles in grid

--- projects/safe/src/lib/components/ui/core-grid/grid/grid.component.ts
+++ projects/safe/src/lib/components/ui/core-grid/grid/grid.component.ts
@@ -193,6 +193,7 @@ export class SafeGridComponent implements OnInit, AfterViewInit {
     );
   }
 
+  public show = false;
   // === DATA ===
   /**
    * Returns property value in object from path.
@@ -202,6 +203,10 @@ export class SafeGridComponent implements OnInit, AfterViewInit {
    * @returns Value of the property.
    */
   public getPropertyValue(item: any, path: string): any {
+    if (! this.show) {
+      this.show = true;
+      console.log(this.fields);
+    }
     const meta = this.fields.find((x) => x.name === path).meta;
     const value = get(item, path);
     if (meta.choices) {
