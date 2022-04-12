517b43b6a7dbca7ce059b8a1da9628eacbe4066b
Maximo-reliefapps <maximo.reliefapps@gmail.com>
2022-04-12 10:02:40 +0200

    /auth redirects to main page when user is already logged

--- projects/back-office/src/app/auth/pages/login/login.component.ts
+++ projects/back-office/src/app/auth/pages/login/login.component.ts
@@ -1,18 +1,26 @@
-import { Component } from '@angular/core';
-import { OAuthService } from 'angular-oauth2-oidc';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { SafeAuthService } from '@safe/builder';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {
-  constructor(private oauthService: OAuthService) {}
+export class LoginComponent implements OnInit {
+  constructor(private authService: SafeAuthService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.authService.isAuthenticated$.subscribe((res: boolean) => {
+      if (res === true) {
+        this.router.navigate(['/']);
+      }
+    });
+  }
 
   /**
    * Redirects to Azure authentication page.
    */
   onLogin(): void {
-    this.oauthService.initLoginFlow();
   }
 }
