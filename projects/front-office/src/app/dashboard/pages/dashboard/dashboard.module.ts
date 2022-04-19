a7d49d9ed4ed174e6fa02d39c41e9f8ae010889f
Matheus Dias <matheus-relief>
2022-04-19 14:47:23 -0300

    added skeleton loading for dashboard/front-office

--- projects/front-office/src/app/dashboard/pages/dashboard/dashboard.module.ts
+++ projects/front-office/src/app/dashboard/pages/dashboard/dashboard.module.ts
@@ -3,8 +3,7 @@ import { CommonModule } from '@angular/common';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
 import { SafeWidgetGridModule } from '@safe/builder';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-
+import { IndicatorsModule } from '@progress/kendo-angular-indicators';
 /**
  * Dashboard page.
  * Dashboard is one of the available content types of application pages.
@@ -15,7 +14,7 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
     CommonModule,
     DashboardRoutingModule,
     SafeWidgetGridModule,
-    MatProgressSpinnerModule,
+    IndicatorsModule,
   ],
   exports: [DashboardComponent],
 })
