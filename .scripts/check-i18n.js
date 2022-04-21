66393274842deba01ae47cc64cb452ceb251a14d
Matthis-M-ReliefApps <matthis@reliefapplications.org>
2022-04-21 10:01:01 +0200

    check-i18n script now keeps the variables

--- .scripts/check-i18n.js
+++ .scripts/check-i18n.js
@@ -1,8 +1,14 @@
 const enJson = require('../projects/safe/src/i18n/en.json');
 const fs = require('fs');
 
+const I18N_FOLDER_PATH = 'projects/safe/src/i18n/';
+
 /** Default value that will be used as key in test translation file. */
 const DEFAULT_VALUE = '******';
+
+// Regex which matches variables inside translations
+// Be aware that it will only match when the variable name is a single word, which should always be the case
+const REGEX_VALUE_VARIABLE = /({{\w+}})/;
 
 /**
  * Sort a JSON object by its keys. (Array are not available)
@@ -27,14 +33,30 @@ const sortJson = (json) => {
  * Writes in a new JSON default value for each key.
  *
  * @param {*} json json to copy
- * @param {*} defaultValue default value to apply
  * @returns new json
  */
 const setDefaultValue = (json, defaultValue) => {
   const newJson = { ...json };
   for (const key of Object.keys(json)) {
     if (typeof newJson[key] === 'string') {
-      newJson[key] = defaultValue;
+      let newValue = defaultValue;
+      if (REGEX_VALUE_VARIABLE.test(newJson[key])) {
+        // Split the translation value using variables as delimiter, including them in the array
+        // Then, filter out empty values
+        const splittedValue = json[key]
+          .split(REGEX_VALUE_VARIABLE)
+          .filter((x) => x.length > 0);
+
+        // Build the placeholder by replacing all text that is not the variable by the default value, and remove whitespace at start and end
+        newValue = splittedValue
+          .reduce((prev, curr) => {
+            return REGEX_VALUE_VARIABLE.test(curr)
+              ? prev.concat(' ' + curr)
+              : prev.concat(' ' + DEFAULT_VALUE);
+          }, '')
+          .trim();
+      }
+      newJson[key] = newValue;
     } else {
       newJson[key] = setDefaultValue(newJson[key], defaultValue);
     }
@@ -45,7 +67,7 @@ const setDefaultValue = (json, defaultValue) => {
 // Check that translation files are sorted.
 const sortedEnJson = sortJson(enJson);
 fs.writeFile(
-  'projects/safe/src/i18n/en.json',
+  I18N_FOLDER_PATH + 'en.json',
   JSON.stringify(sortedEnJson, null, '\t'),
   (err) => {
     if (err) {
@@ -58,9 +80,8 @@ fs.writeFile(
 
 // Update the i18n test file.
 const testJson = setDefaultValue(enJson, DEFAULT_VALUE);
-
 fs.writeFile(
-  'projects/safe/src/i18n/test.json',
+  I18N_FOLDER_PATH + 'test.json',
   JSON.stringify(testJson, null, '\t'),
   (err) => {
     if (err) {
