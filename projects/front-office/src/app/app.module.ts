62d106600dd4375a9c15ebdac4dc07af379046a4
Matheus Dias <matheus-relief>
2022-04-23 19:36:49 -0300

    added kendoTranslate as provider for front-office

--- projects/front-office/src/app/app.module.ts
+++ projects/front-office/src/app/app.module.ts
@@ -28,6 +28,8 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { OAuthModule, OAuthService } from 'angular-oauth2-oidc';
+import { MessageService } from '@progress/kendo-angular-l10n';
+import { KendoTranslationService } from 'projects/safe/src/public-api';
 import { filter } from 'rxjs/operators';
 
 localStorage.setItem('loaded', 'false');
@@ -181,6 +183,10 @@ const imports: any[] = [];
       useFactory: initializeAuth,
       multi: true,
       deps: [OAuthService],
+    },
+    {
+      provide: MessageService,
+      useClass: KendoTranslationService,
     },
   ],
   bootstrap: [AppComponent],
