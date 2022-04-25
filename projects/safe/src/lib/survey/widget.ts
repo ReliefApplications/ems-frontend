1b3d4057a6163fdaaa2cb13372107da22ce992e9
Alexis Delage <alexisd.reliefapps@gmail.com>
2022-04-25 17:35:52 +0200

    Add translations for fields resource and tooltip

--- projects/safe/src/lib/survey/widget.ts
+++ projects/safe/src/lib/survey/widget.ts
@@ -47,6 +47,7 @@ export const init = (
       survey.Serializer.addProperty('question', {
         name: 'tooltip:text',
         category: 'general',
+        isLocalizable: true,
       });
       survey.Serializer.addProperty('comment', {
         name: 'allowEdition:boolean',
@@ -165,6 +166,11 @@ export const init = (
       }
       // Display of add button for resource question
       if (question.getType() === 'resource') {
+        // support the placeholder field
+        if (question.placeholder) {
+          question.contentQuestion.optionsCaption =
+            question.localizableStrings?.placeholder?.renderedText || '';
+        }
         // const dropdownComponent = buildRecordDropdown(question, el);
         if (question.survey.mode !== 'display' && question.resource) {
           const actionsButtons = document.createElement('div');
