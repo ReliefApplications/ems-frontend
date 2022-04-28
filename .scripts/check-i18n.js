d070fe5b24a0c3687f7925003b17cfc20fe1981c
Antoine Hurard <antoine.reliefapps@gmail.com>
2022-04-28 09:55:42 +0200

    update i18n script

--- .scripts/check-i18n.js
+++ .scripts/check-i18n.js
@@ -1,13 +1,23 @@
 const fs = require('fs');
+/** Translation files are stored there */
 const I18N_FOLDER_PATH = 'projects/safe/src/i18n/';
-const enJson = require('../'+I18N_FOLDER_PATH+'en.json');
 
+const DEFAULT_LANGUAGE = 'en';
+const TEST_LANGUAGE = 'test';
+
+/** Default language */
+const DEFAULT_I18N = require('../' +
+  I18N_FOLDER_PATH +
+  DEFAULT_LANGUAGE +
+  '.json');
 
 /** Default value that will be used as key in test translation file. */
 const DEFAULT_VALUE = ' ****** ';
 
-// Regex matching everything that is not between double brackets
-// Can be improved to match even values without brackets and leave the spaces before and after brackets
+/**
+ * Regex matching everything that is not between double brackets.
+ * Can be improved to match even values without brackets and leave the spaces before and after brackets.
+ */
 const REGEX_VALUE_VARIABLE = new RegExp(/([^{}]+(?={{))|((?<=}})[^{}]+)/, 'g');
 
 /**
@@ -51,53 +61,15 @@ const setDefaultValue = (json, defaultValue) => {
 };
 
 /**
- * Gets all keys (including nested) in JSON Object
+ * Update translation file
  *
- * @param {*} json json to inspect and extract the keys from
- * @param {*} ret_array array in which we will return the keys
- * @returns ret_array, containing all the keys
+ * @param {*} lang language
+ * @param {*} json json value
  */
-function getAllJsonKeys(json, ret_array = []) {
-  for (key in json) {
-      if (typeof(json[key]) === 'object') {
-          ret_array.push(key);
-          getAllJsonKeys(json[key], ret_array);
-      } else {
-          ret_array.push(key);
-      }
-  }
-  return ret_array
-}
-
-let listOfJson = [];
-let listOfFileNames = [];
-
-//Getting the name of all the files in the I18n folder except for 'test.json'
-listOfFileNames = fs.readdirSync(I18N_FOLDER_PATH);
-const indexTestJson = listOfFileNames.indexOf('test.json');
-if (indexTestJson > -1) {
-  listOfFileNames.splice(indexTestJson,1); 
-}
-
-//Putting all the JSONs in a list
-listOfJson = listOfFileNames.map(filename=>require('../'+I18N_FOLDER_PATH+filename));
-
-//Sort all the JSONs
-listOfJson = listOfJson.map(json=>sortJson(json));
-let enJsonSorted = sortJson(enJson);
-
-//Create a list of all the JSONs files except en.json
-let listOfJSONWithoutEn = JSON.parse(JSON.stringify(listOfJson));
-const indexEnJson = listOfFileNames.indexOf('en.json');
-if (indexEnJson > -1) {
-  listOfJSONWithoutEn.splice(indexEnJson,1); 
-}
-
-// Check that translation files are sorted.
-for(let i in listOfJson){
+const updateFile = (lang, json) => {
   fs.writeFile(
-    I18N_FOLDER_PATH + listOfFileNames[i],
-    JSON.stringify(listOfJson[i], null, '\t'),
+    I18N_FOLDER_PATH + lang + '.json',
+    JSON.stringify(json, null, '\t'),
     (err) => {
       if (err) {
         console.error(err);
@@ -106,32 +78,90 @@ for(let i in listOfJson){
       // else success
     }
   );
+};
+
+/**
+ * Checks the translation key in a JSON.
+ *
+ * @param {*} module module name
+ * @param {*} language language to test
+ * @param {*} json language json
+ * @param {*} baseLanguage language reference
+ * @param {*} baseJson language reference json
+ * @returns
+ */
+const checkTranslationKeys = (
+  language,
+  json,
+  baseLanguage,
+  baseJson,
+  prefix
+) => {
+  prefix = prefix || '';
+  for (const key of Object.keys(baseJson)) {
+    if (typeof baseJson[key] === 'string') {
+      if (!json[key] || typeof json[key] !== 'string') {
+        throw(`Missing key: ${prefix + key} in ${language} translation.`);
+        // console.error(
+          
+        // );
+      }
+    } else {
+      if (!json[key] || typeof json[key] === 'string') {
+        throw(`Incorrect key: ${prefix + key} in ${language} translation.`);
+        // console.error(
+          
+        // );
+      } else {
+        checkTranslationKeys(
+          language,
+          json[key],
+          baseLanguage,
+          baseJson[key],
+          prefix + key + '.'
+        );
+      }
+    }
+  }
+};
+
+// Build dictionnary from files
+let filenames = fs.readdirSync(I18N_FOLDER_PATH);
+let languages = filenames
+  .filter((name) => name.endsWith('.json'))
+  .map((name) => name.split('.json')[0]);
+
+let dictionnary = languages.map((lang) => ({
+  lang: lang,
+  value: require('../' + I18N_FOLDER_PATH + lang + '.json'),
+}));
+
+// Sort the files
+dictionnary
+  .filter((x) => x.lang !== TEST_LANGUAGE)
+  .map((x) => {
+    x.value = sortJson(x.value);
+    updateFile(x.lang, x.value)
+  });
+
+// Check the files
+try {
+  dictionnary
+  .filter((x) => ![TEST_LANGUAGE, DEFAULT_LANGUAGE].includes(x.lang))
+  .forEach((x) =>
+    checkTranslationKeys(
+      x.lang,
+      x.value,
+      DEFAULT_LANGUAGE,
+      DEFAULT_I18N
+    )
+  );
+} catch (err) {
+  console.error(err);
+  process.exit();
 }
 
-//Check that the "non-english" files have the same keys than en.json
-let allKeysEn = [];
-getAllJsonKeys(enJsonSorted,allKeysEn);
-listOfJSONWithoutEn.map(element=>{
-  let allKeys = [];
-  getAllJsonKeys(element,allKeys);
-  if(allKeys.toString() !== allKeysEn.toString()){
-    console.error('Error : One of the other languages does not contain exactly the same keys as en.json');
-    return;
-  }
-  // else success
-})
 
 // Update the i18n test file.
-const testJson = setDefaultValue(enJsonSorted, DEFAULT_VALUE);
-fs.writeFile(
-  I18N_FOLDER_PATH + 'test.json',
-  JSON.stringify(testJson, null, '\t'),
-  (err) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    // else success
-  }
-);
-
+const testJson = setDefaultValue(dictionnary.find(x => x.lang === DEFAULT_LANGUAGE).value, DEFAULT_VALUE);
+updateFile(TEST_LANGUAGE, testJson);
