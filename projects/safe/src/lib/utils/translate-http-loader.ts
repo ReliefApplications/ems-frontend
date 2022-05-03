2e9d1580550e2e5171ec5f11ab3f2d2c36eb4997
Alexis Delage <alexisd.reliefapps@gmail.com>
2022-05-03 16:17:16 +0200

    Update translateHttpLaoder

--- projects/safe/src/lib/utils/translate-http-loader.ts
+++ projects/safe/src/lib/utils/translate-http-loader.ts
@@ -1,8 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
+/**
+ * TranslateLoader object to manage missing languages
+ */
 export class SafeTranslateHttpLoader extends TranslateHttpLoader {
   defaultLocale: string;
 
@@ -16,12 +19,25 @@ export class SafeTranslateHttpLoader extends TranslateHttpLoader {
     this.defaultLocale = defaultLocale;
   }
 
+  /**
+   * Get the translation file for a specific language. First try to get the
+   * file for the given language, then try for the "pure" language if it is a
+   * composed-language code (for example try "fr" for "fr-fr"), and finally
+   * get the default translation file if no file has been found.
+   *
+   * @param lang The code of the language file
+   * @returns An observable of the language as a dict
+   */
   getTranslation(lang: string): Observable<any> {
     return super.getTranslation(lang).pipe(
-      catchError(() => {
+      catchError((err) => {
         const langCode = lang.split('-')[0];
-        console.warn(`${lang} not found, use ${langCode} instead`);
-        return super.getTranslation(langCode);
+        if (langCode !== lang) {
+          console.warn(`${lang} not found, use ${langCode} instead`);
+          return super.getTranslation(langCode);
+        } else {
+          throw throwError(err);
+        }
       }),
       catchError(() => {
         console.warn(`${lang} not found, use ${this.defaultLocale} instead`);
