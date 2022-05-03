6af098545a86a9ecbc9351622b8d777be2e23f64
Antoine Hurard <antoine.reliefapps@gmail.com>
2022-05-03 16:20:49 +0200

    add custom control for palette edition

---
+++ projects/safe/src/lib/components/palette-control/palette-control.component.ts
@@ -0,0 +1,69 @@
+import { Component, forwardRef, Input, OnInit, Provider } from '@angular/core';
+import {
+  ControlValueAccessor,
+  FormControl,
+  NG_VALUE_ACCESSOR,
+} from '@angular/forms';
+
+const CONTROL_VALUE_ACCESSOR: Provider = {
+  provide: NG_VALUE_ACCESSOR,
+  useExisting: forwardRef(() => SafePaletteControlComponent),
+  multi: true,
+};
+
+/**
+ * Palette form control.
+ */
+@Component({
+  selector: 'safe-palette-control',
+  templateUrl: './palette-control.component.html',
+  styleUrls: ['./palette-control.component.scss'],
+  providers: [CONTROL_VALUE_ACCESSOR],
+})
+export class SafePaletteControlComponent
+  implements OnInit, ControlValueAccessor
+{
+  @Input() formControl!: FormControl;
+
+  @Input() formControlName!: string;
+
+  private onTouched!: any;
+  private onChanged!: any;
+
+  public colors: string[] = [];
+  public disabled = false;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  public registerOnChange(fn: any): void {
+    this.onChanged = fn;
+  }
+
+  public registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  writeValue(value: string[]): void {
+    this.colors = value;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
+
+  onColorChange(e: any, i: number): void {
+    this.colors[i] = e;
+  }
+
+  onClose(e: any): void {
+    this.formControl.setValue(this.colors);
+  }
+
+  onReorder(e: any): void {
+    this.colors[e.previousContainer.data.index] = e.container.data.item;
+    this.colors[e.container.data.index] = e.previousContainer.data.item;
+    this.formControl.setValue(this.colors);
+  }
+}
---
+++ projects/safe/src/lib/components/palette-control/palette-control.stories.ts
@@ -0,0 +1,54 @@
+import { Meta, moduleMetadata, Story } from '@storybook/angular';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SafePaletteControlComponent } from './palette-control.component';
+import { SafePaletteControlModule } from './palette-control.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+export default {
+  component: SafePaletteControlComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [
+        BrowserAnimationsModule,
+        SafePaletteControlModule,
+        FormsModule,
+        ReactiveFormsModule,
+      ],
+      providers: [],
+    }),
+  ],
+  title: 'UI/Controls/Palette',
+} as Meta;
+
+const TEMPLATE: Story<SafePaletteControlComponent> = (args) => ({
+  // template:
+  //   '<safe-palette-control [formControl]="palette"></safe-palette-control>',
+  props: {
+    ...args,
+    // palette: new FormControl(['red', 'blue']),
+  },
+});
+
+const COLORS = [
+  '#FF8C00',
+  '#B0C4DE',
+  '#ADD8E6',
+  '#BC8F8F',
+  '#DA70D6',
+  '#CD5C5C',
+  '#48D1CC',
+  '#FF00FF',
+  '#6495ED',
+  '#FF0000',
+];
+
+export const DEFAULT = TEMPLATE.bind({});
+DEFAULT.args = {
+  formControl: new FormControl(COLORS),
+  colors: COLORS,
+};
+
+export const DISABLED = TEMPLATE.bind({});
+DISABLED.args = {
+  formControl: new FormControl({ value: COLORS, disabled: true }),
+};
