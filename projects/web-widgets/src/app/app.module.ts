d19a3ac3d0a19f4dff75ae02c2d10343002a00fa
Alexis Delage <alexisd.reliefapps@gmail.com>
2022-05-17 10:36:57 +0200

    Allow authentification in new tabs

--- projects/web-widgets/src/app/app.module.ts
+++ projects/web-widgets/src/app/app.module.ts
@@ -28,7 +28,7 @@ import { environment } from '../environments/environment';
 import { BehaviorSubject } from 'rxjs';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { RouterModule } from '@angular/router';
-import { OAuthModule, OAuthService } from 'angular-oauth2-oidc';
+import { OAuthModule, OAuthService, OAuthStorage } from 'angular-oauth2-oidc';
 import {
   TranslateLoader,
   TranslateModule,
@@ -212,6 +212,7 @@ const provideOverlay = (_platform: Platform): AppOverlayContainer =>
       provide: MessageService,
       useClass: KendoTranslationService,
     },
+    { provide: OAuthStorage, useValue: localStorage },
   ],
 })
 export class AppModule implements DoBootstrap {
