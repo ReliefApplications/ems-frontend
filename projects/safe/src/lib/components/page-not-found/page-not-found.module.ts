a5aefc355eeb22a0b9b9461333dc8daf8901a726
Alexis Delage <alexisd.reliefapps@gmail.com>
2022-05-23 17:43:26 +0200

    Implement design

--- projects/safe/src/lib/components/page-not-found/page-not-found.module.ts
+++ projects/safe/src/lib/components/page-not-found/page-not-found.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SafePageNotFoundComponent } from './page-not-found.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [SafePageNotFoundComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [SafePageNotFoundComponent],
 })
 export class SafePageNotFoundModule {}
