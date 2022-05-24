9e2a7dc0d6be5a8b8741db82bf322bec015c9c9f
Alexis Delage <alexisd.reliefapps@gmail.com>
2022-05-24 10:58:13 +0200

    Add translations and animations

--- projects/safe/src/lib/components/page-not-found/page-not-found.module.ts
+++ projects/safe/src/lib/components/page-not-found/page-not-found.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SafePageNotFoundComponent } from './page-not-found.component';
 import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [SafePageNotFoundComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, TranslateModule],
   exports: [SafePageNotFoundComponent],
 })
 export class SafePageNotFoundModule {}
