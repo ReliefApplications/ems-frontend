f7cb035f96d81ea4c35b9e48394b31c09ee1aabb
Antoine Hurard <antoine.reliefapps@gmail.com>
2022-06-15 10:25:06 +0200

    update apollo version

--- projects/back-office/src/app/app.module.ts
+++ projects/back-office/src/app/app.module.ts
@@ -6,20 +6,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-// Apollo
+// Http
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { Apollo, APOLLO_OPTIONS } from 'apollo-angular';
-import { HttpLink } from 'apollo-angular/http';
-import { InMemoryCache, ApolloLink, split } from '@apollo/client/core';
-import { getMainDefinition } from '@apollo/client/utilities';
-import { WebSocketLink } from '@apollo/client/link/ws';
-import { setContext } from '@apollo/client/link/context';
 
 // Env
 import { environment } from '../environments/environment';
 
 // Config
-import { BehaviorSubject } from 'rxjs';
 import { MatNativeDateModule, MAT_DATE_LOCALE } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatDialogModule } from '@angular/material/dialog';
@@ -46,98 +39,17 @@ import {
 import { PopupService } from '@progress/kendo-angular-popup';
 import { ResizeBatchService } from '@progress/kendo-angular-common';
 import { touchEnabled } from '@progress/kendo-common';
-
-localStorage.setItem('loaded', 'false');
-
-const REFRESH = new BehaviorSubject<boolean>(false);
+// Apollo / GraphQL
+import { GraphQLModule } from './graphql.module';
 
 /**
- * Configuration of the Apollo client.
+ * Initialize authentication in the platform.
+ * Configuration in environment file.
+ * Use oAuth
  *
- * @param httpLink Apollo http link
- * @returns void
+ * @param oauth OAuth Service
+ * @returns oAuth configuration
  */
-export const provideApollo = (httpLink: HttpLink): any => {
-  const basic = setContext((operation, context) => ({
-    headers: {
-      // eslint-disable-next-line @typescript-eslint/naming-convention
-      Accept: 'charset=utf-8',
-    },
-  }));
-
-  const auth = setContext((operation, context) => {
-    // Get the authentication token from local storage if it exists
-    const token = localStorage.getItem('idtoken');
-    return {
-      headers: {
-        // eslint-disable-next-line @typescript-eslint/naming-convention
-        Authorization: `Bearer ${token}`,
-      },
-    };
-  });
-
-  const http = httpLink.create({ uri: `${environment.apiUrl}/graphql` });
-
-  const ws = new WebSocketLink({
-    uri: `${environment.subscriptionApiUrl}/graphql`,
-    options: {
-      reconnect: true,
-      connectionParams: {
-        authToken: localStorage.getItem('idtoken'),
-      },
-      connectionCallback: (error) => {
-        if (localStorage.getItem('loaded') === 'true') {
-          // location.reload();
-          REFRESH.next(true);
-          localStorage.setItem('loaded', 'false');
-        }
-        localStorage.setItem('loaded', 'true');
-      },
-    },
-  });
-
-  interface Definition {
-    kind: string;
-    operation?: string;
-  }
-
-  const link = ApolloLink.from([
-    basic,
-    auth,
-    split(
-      ({ query }) => {
-        const { kind, operation }: Definition = getMainDefinition(query);
-        return kind === 'OperationDefinition' && operation === 'subscription';
-      },
-      ws,
-      http
-    ),
-  ]);
-
-  // Cache is not currently used, due to fetchPolicy values
-  const cache = new InMemoryCache();
-
-  return {
-    link,
-    cache,
-    defaultOptions: {
-      watchQuery: {
-        fetchPolicy: 'network-only',
-        // fetchPolicy: 'cache-and-network',
-        errorPolicy: 'ignore',
-      },
-      query: {
-        fetchPolicy: 'network-only',
-        // fetchPolicy: 'cache-and-network',
-        errorPolicy: 'all',
-      },
-      mutate: {
-        errorPolicy: 'all',
-      },
-    },
-  };
-};
-
 const initializeAuth =
   (oauth: OAuthService): any =>
   () => {
@@ -153,6 +65,9 @@ const initializeAuth =
 export const httpTranslateLoader = (http: HttpClient) =>
   new TranslateHttpLoader(http);
 
+/**
+ * Main module of Back-Office project.
+ */
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -179,17 +94,12 @@ export const httpTranslateLoader = (http: HttpClient) =>
         sendAccessToken: true,
       },
     }),
+    GraphQLModule,
   ],
   providers: [
     {
       provide: 'environment',
       useValue: environment,
-    },
-    {
-      // TODO: added default options to solve cache issues, cache solution can be added at the query / mutation level.
-      provide: APOLLO_OPTIONS,
-      useFactory: provideApollo,
-      deps: [HttpLink],
     },
     {
       provide: APP_INITIALIZER,
@@ -228,6 +138,4 @@ export const httpTranslateLoader = (http: HttpClient) =>
   ],
   bootstrap: [AppComponent],
 })
-export class AppModule {
-  constructor(private apollo: Apollo) {}
-}
+export class AppModule {}
