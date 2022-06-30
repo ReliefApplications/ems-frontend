f02bc9d48d02044778033b415b884e11c8532948
Alexis Delage <alexisd.reliefapps@gmail.com>
2022-06-30 13:22:51 +0200

    Focus on new widgets and open settings

--- projects/back-office/src/app/dashboard/pages/dashboard/dashboard.component.ts
+++ projects/back-office/src/app/dashboard/pages/dashboard/dashboard.component.ts
@@ -17,6 +17,7 @@ import {
   SafeWorkflowService,
   SafeDashboardService,
   SafeAuthService,
+  SafeTileDataComponent,
   Application,
 } from '@safe/builder';
 import { ShareUrlComponent } from './components/share-url/share-url.component';
@@ -152,6 +153,30 @@ export class DashboardComponent implements OnInit, OnDestroy {
     this.generatedTiles += 1;
     this.tiles = [...this.tiles, tile];
     this.autoSaveChanges();
+    setTimeout(() => {
+      const el = document.getElementById(`widget-${tile.id}`);
+      el?.scrollIntoView({ behavior: 'smooth' });
+      setTimeout(() => {
+        const dialogRef = this.dialog.open(SafeTileDataComponent, {
+          disableClose: true,
+          data: {
+            tile,
+            template: this.dashboardService.findSettingsTemplate(tile),
+          },
+          // hasBackdrop: false,
+          position: {
+            bottom: '0',
+            right: '0',
+          },
+          panelClass: 'tile-settings-dialog',
+        });
+        dialogRef.afterClosed().subscribe((res) => {
+          if (res) {
+            this.onEditTile({ type: 'data', id: tile.id, options: res });
+          }
+        });
+      }, 300);
+    });
   }
 
   /**
