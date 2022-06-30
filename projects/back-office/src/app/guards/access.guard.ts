2435485df7713addaa5ea8b85ba6e3cedee5e78a
Alexis Delage <alexisd.reliefapps@gmail.com>
2022-06-30 17:27:46 +0200

    More docs

--- projects/back-office/src/app/guards/access.guard.ts
+++ projects/back-office/src/app/guards/access.guard.ts
@@ -21,12 +21,26 @@ import { map } from 'rxjs/operators';
   providedIn: 'root',
 })
 export class AccessGuard implements CanActivate {
+  /**
+   * Constructor of the accessguard
+   *
+   * @param authService The authentification service
+   * @param snackBar The snack bar service
+   * @param router The router client
+   */
   constructor(
     private authService: SafeAuthService,
     private snackBar: SafeSnackBarService,
     private router: Router
   ) {}
 
+  /**
+   * Check if user can activate the route
+   *
+   * @param route The route to test
+   * @param state The current state
+   * @returns A boolean indicating if he can activate
+   */
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
