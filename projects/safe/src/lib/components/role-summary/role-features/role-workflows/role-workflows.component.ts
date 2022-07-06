b632f4e8a8845e389e3b7ab566da02696d26e1ac
Antoine Hurard <antoine.reliefapps@gmail.com>
2022-07-06 13:08:23 +0200

    add ui for steps of workflow

--- projects/safe/src/lib/components/role-summary/role-features/role-workflows/role-workflows.component.ts
+++ projects/safe/src/lib/components/role-summary/role-features/role-workflows/role-workflows.component.ts
@@ -1,3 +1,10 @@
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { Component, Input, OnInit } from '@angular/core';
 import { Page } from '../../../../models/page.model';
 
@@ -5,12 +12,31 @@ import { Page } from '../../../../models/page.model';
   selector: 'safe-role-workflows',
   templateUrl: './role-workflows.component.html',
   styleUrls: ['./role-workflows.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({ height: '0px', minHeight: '0' })),
+      state('expanded', style({ height: '*' })),
+      transition(
+        'expanded <=> collapsed',
+        animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')
+      ),
+    ]),
+  ],
 })
 export class RoleWorkflowsComponent implements OnInit {
   @Input() pages: Page[] = [];
   public displayedColumns = ['name', 'actions'];
+  public openedWorkflowId = '';
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  toggleWorkflow(id: string): void {
+    if (id === this.openedWorkflowId) {
+      this.openedWorkflowId = '';
+    } else {
+      this.openedWorkflowId = id;
+    }
+  }
 }
