7137dd8a2a7b80d0c2aeac2034f8ac8ecd54c8ee
Matheus Dias <matheus-relief>
2022-07-07 01:10:48 -0300

    added tooltips for toggle button

--- projects/safe/src/lib/components/role-summary/role-features/role-features.module.ts
+++ projects/safe/src/lib/components/role-summary/role-features/role-features.module.ts
@@ -12,6 +12,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 /**
  * Features tab of Role Summary component.
@@ -32,6 +33,7 @@ import { MatIconModule } from '@angular/material/icon';
     FormsModule,
     ReactiveFormsModule,
     TranslateModule,
+    MatTooltipModule,
     MatTableModule,
     SafeButtonModule,
     SafeIconModule,
