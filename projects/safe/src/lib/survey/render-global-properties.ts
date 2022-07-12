2df35526699d3381751ee2d4e475545d07d58c28
Alexis Delage <alexisd.reliefapps@gmail.com>
2022-07-12 11:55:17 +0200

    simplify render global properties #22069

--- projects/safe/src/lib/survey/render-global-properties.ts
+++ projects/safe/src/lib/survey/render-global-properties.ts
@@ -12,16 +12,11 @@ import * as TooltipProperty from './global-properties/tooltip';
  * @returns A function which takes the survey instance and some options as args
  */
 export const renderGlobalProperties =
-  (
-    referenceDataService: SafeReferenceDataService
-  ): ((
-    survey: SurveyModel,
-    options: { question: Question; htmlElement: HTMLElement }
-  ) => void) =>
+  (referenceDataService: SafeReferenceDataService) =>
   (
     survey: SurveyModel,
     options: { question: Question; htmlElement: HTMLElement }
-  ): void => {
+  ) => {
     OthersProperties.render(options.question);
     TooltipProperty.render(options.question, options.htmlElement);
     ReferenceDataProperties.render(options.question, referenceDataService);
