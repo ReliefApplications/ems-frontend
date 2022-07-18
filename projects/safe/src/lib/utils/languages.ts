dbbc3fa1a0895b359b81fec8e5382dec255fc3dc
Alexis Delage <alexisd.reliefapps@gmail.com>
2022-07-18 13:00:34 +0200

    Add a method fot gettting language name

--- projects/safe/src/lib/utils/languages.ts
+++ projects/safe/src/lib/utils/languages.ts
@@ -1,3 +1,4 @@
+import { TranslateService } from '@ngx-translate/core';
 import { startCase } from 'lodash';
 
 // eslint-disable-next-line @typescript-eslint/no-namespace
@@ -25,6 +26,27 @@ export const getLanguageNativeName = (lang: string): string => {
   try {
     // try to get names for the asking language
     const displayName = new Intl.DisplayNames(lang, { type: 'language' });
+    return startCase(displayName.of(lang)) || lang;
+  } catch {
+    // if lang argument is not a language code
+    return lang;
+  }
+};
+
+/**
+ * Get the full name of a language in the current language of the user
+ *
+ * @param lang The code of the language we want the name of
+ * @param translate The translate service
+ * @returns The language name
+ */
+export const getLanguageName = (lang: string, translate: TranslateService) => {
+  try {
+    // try to get names for the asking language
+    const displayLang = translate.currentLang || translate.defaultLang;
+    const displayName = new Intl.DisplayNames(displayLang, {
+      type: 'language',
+    });
     return startCase(displayName.of(lang)) || lang;
   } catch {
     // if lang argument is not a language code
