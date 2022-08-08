c044a31730901178cb8bd191b99fd5760bd134b5
Antoine Hurard <antoine.reliefapps@gmail.com>
2022-08-08 13:14:14 +0200

    fix issue where merge marker was left

--- projects/safe/src/lib/graphql/queries.ts
+++ projects/safe/src/lib/graphql/queries.ts
@@ -1,7 +1,7 @@
 import { gql } from 'apollo-angular';
 import { Form } from '../models/form.model';
 import { Resource } from '../models/resource.model';
-import { User, Group } from '../models/user.model';
+import { User } from '../models/user.model';
 import { Notification } from '../models/notification.model';
 import { Application } from '../models/application.model';
 import { Workflow } from '../models/workflow.model';
@@ -9,7 +9,6 @@ import { Dashboard } from '../models/dashboard.model';
 import { ReferenceData } from '../models/reference-data.model';
 
 // === GET PROFILE ===
-
 /** Graphql request for getting profile of the current user */
 export const GET_PROFILE = gql`
   {
@@ -58,7 +57,6 @@ export interface GetProfileQueryResponse {
 }
 
 // === GET FORM BY ID ===
-
 /** Graphql request for getting the meta fields of a grid by form id */
 export const GET_GRID_FORM_META = gql`
   query GetFormAsTemplate($id: ID!) {
@@ -84,7 +82,6 @@ export interface GetFormByIdQueryResponse {
 }
 
 // === GET RELATED FORMS FROM RESOURCE ===
-
 /** Graphql request for getting resource meta date for a grid */
 export const GET_GRID_RESOURCE_META = gql`
   query GetGridResourceMeta($resource: ID!) {
@@ -113,7 +110,6 @@ export const GET_GRID_RESOURCE_META = gql`
 `;
 
 // === GET RESOURCE BY ID ===
-
 /** Model for GetResourceByIdQueryResponse object */
 export interface GetResourceByIdQueryResponse {
   loading: boolean;
@@ -121,7 +117,6 @@ export interface GetResourceByIdQueryResponse {
 }
 
 // === GET NOTIFICATIONS ===
-
 /** Graphql request for getting notifications */
 export const GET_NOTIFICATIONS = gql`
   query GetNotifications($first: Int, $afterCursor: ID) {
@@ -172,7 +167,6 @@ export interface GetNotificationsQueryResponse {
 }
 
 // === GET APPLICATION BY ID ===
-
 /** Graphql request for getting application data by its id */
 export const GET_APPLICATION_BY_ID = gql`
   query GetApplicationById($id: ID!, $asRole: ID) {
@@ -543,4 +537,3 @@ export interface GetReferenceDataByIdQueryResponse {
   loading: boolean;
   referenceData: ReferenceData;
 }
-<<<<<<< HEAD
