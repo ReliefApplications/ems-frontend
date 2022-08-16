ac0573f3e96c86556fe954074a14ac238afb4def
Antoine Hurard <antoine.reliefapps@gmail.com>
2022-08-16 17:04:25 +0200

    add graphql select in summary cards to select the resource

--- projects/safe/src/lib/components/widgets/summary-card-settings/card-modal/data-source-tab/data-source-tab.component.ts
+++ projects/safe/src/lib/components/widgets/summary-card-settings/card-modal/data-source-tab/data-source-tab.component.ts
@@ -27,19 +27,7 @@ export class SafeDataSourceTabComponent implements OnInit {
 
   // === DATA ===
   public resources: any[] = [];
-  private resourcesQuery!: QueryRef<GetResourcesQueryResponse>;
-  public loading = true;
-  public loadingResource = false;
-  public loadingMore = false;
-  private pageInfo = {
-    endCursor: '',
-    hasNextPage: true,
-  };
-  public loadingGrid = true;
-  public gridData: any = {
-    data: [],
-    total: 0,
-  };
+  public resourcesQuery!: QueryRef<GetResourcesQueryResponse>;
 
   /** @returns the aggregation form */
   public get aggregationForm(): FormGroup {
@@ -63,6 +51,7 @@ export class SafeDataSourceTabComponent implements OnInit {
     // Data source query
     const variables: any = {
       first: ITEMS_PER_PAGE,
+      sortField: 'name',
     };
     if (this.aggregationForm.value.dataSource) {
       variables.filter = {
@@ -80,14 +69,6 @@ export class SafeDataSourceTabComponent implements OnInit {
       query: GET_RESOURCES,
       variables,
     });
-    this.resourcesQuery.valueChanges.subscribe((res) => {
-      this.resources = res.data.resources.edges.map((x) => x.node);
-      this.pageInfo = res.data.resources.pageInfo;
-      this.loadingMore = res.loading;
-      if (this.loading) {
-        this.loading = res.loading;
-      }
-    });
   }
 
   /**
@@ -95,71 +76,12 @@ export class SafeDataSourceTabComponent implements OnInit {
    *
    * @param value string used to filter.
    */
-  public onFilterDataSource(value: string): void {
-    if (!this.loadingMore) {
-      this.loadingMore = true;
-      this.fetchMoreDataSources(false, value);
-    }
-  }
-
-  /**
-   * Fetches next page of data source to add to list.
-   *
-   * @param value string used to filter.
-   */
-  public onScrollDataSource(value: boolean): void {
-    if (!this.loadingMore && this.pageInfo.hasNextPage) {
-      this.loadingMore = true;
-      this.fetchMoreDataSources(value);
-    }
-  }
-
-  /**
-   * Fetches more data sources using filtering and pagination.
-   *
-   * @param nextPage boolean to indicate if we must fetch the next page.
-   * @param filter the data sources fetched must respect this filter
-   */
-  public fetchMoreDataSources(nextPage: boolean = false, filter: string = '') {
-    const variables: any = {
-      first: ITEMS_PER_PAGE,
-    };
-    variables.filter = {
-      logic: 'and',
-      filters: [
-        {
-          field: 'name',
-          operator: 'contains',
-          value: filter,
-        },
-      ],
-    };
-    if (nextPage) {
-      variables.afterCursor = this.pageInfo.endCursor;
-    }
-    this.resourcesQuery.fetchMore({
-      variables,
-      updateQuery: (prev, { fetchMoreResult }) => {
-        if (!fetchMoreResult) {
-          return prev;
-        }
-        return Object.assign({}, prev, {
-          resources: {
-            edges: prev.resources.edges.concat(
-              fetchMoreResult.resources.edges.filter(
-                (x: any) =>
-                  !prev.resources.edges.some(
-                    (y: any) => y.node.id === x.node.id
-                  )
-              )
-            ),
-            pageInfo: fetchMoreResult.resources.pageInfo,
-            totalCount: fetchMoreResult.resources.totalCount,
-          },
-        });
-      },
-    });
-  }
+  // public onFilterDataSource(value: string): void {
+  //   if (!this.loadingMore) {
+  //     this.loadingMore = true;
+  //     this.fetchMoreDataSources(false, value);
+  //   }
+  // }
 
   /**
    * Changes the form value assigned to the radio component.
