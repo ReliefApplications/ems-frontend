8a1629b43a5da4cb54de2f0a194e8a4bbc4802cd
Antoine Hurard <antoine.reliefapps@gmail.com>
2022-08-30 10:35:42 +0200

    add enum for modal size

--- projects/safe/src/lib/components/ui/modal/modal.component.ts
+++ projects/safe/src/lib/components/ui/modal/modal.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
+import { ModalSize } from './modal-size.enum';
 
 /**
  * Modal component, used as a generic wrapper for all modals
@@ -11,24 +12,24 @@ import { MatDialogRef } from '@angular/material/dialog';
 })
 export class SafeModalComponent implements OnInit {
   @Input() closable = false;
-  @Input() size = '';
+  @Input() size: ModalSize | string = ModalSize.MEDIUM;
   @Input() padding = true;
 
   /**
    * Constructor for the modal component
    *
-   * @param dialogRef Used to acces the dialog properties
+   * @param dialogRef Used to access the dialog properties
    */
   constructor(private dialogRef: MatDialogRef<SafeModalComponent>) {}
 
   ngOnInit(): void {
-    if (this.size === 'fullscreen') {
+    if (this.size === ModalSize.FULLSCREEN) {
       this.dialogRef.addPanelClass('fullscreen-dialog');
-    } else if (this.size === 'medium') {
+    } else if (this.size === ModalSize.MEDIUM) {
       this.dialogRef.updateSize('700px');
-    } else if (this.size === 'small') {
+    } else if (this.size === ModalSize.SMALL) {
       this.dialogRef.updateSize('300px');
-    } else if (this.size === 'big') {
+    } else if (this.size === ModalSize.BIG) {
       this.dialogRef.updateSize('100vw', '98%');
     }
 
