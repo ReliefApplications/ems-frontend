fa9b2d94da6001ff71eefdb5d12ec195649012cf
Theo DARONAT <theo.reliefapps@gmail.com>
2022-10-10 08:20:02 +0200

    remove unused code

--- projects/front-office/src/app/dashboard/pages/applications/applications.component.ts
+++ projects/front-office/src/app/dashboard/pages/applications/applications.component.ts
@@ -7,17 +7,10 @@ import {
   SafeSnackBarService,
   SafeApplicationService,
   Permission,
-  Permissions,
-  ContentType,
 } from '@safe/builder';
 import { Subscription } from 'rxjs';
 import { filter } from 'rxjs/operators';
-import {
-  ActivatedRoute,
-  Router,
-  NavigationEnd,
-  RouterEvent,
-} from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 
 /**
@@ -47,12 +40,6 @@ export class ApplicationsComponent implements OnInit, OnDestroy {
   public favorite = '';
   /** User subscription */
   private authSubscription?: Subscription;
-  /** Application service subscription */
-  private applicationSubscription?: Subscription;
-  /** Permissions of the user */
-  private permissions: Permission[] = [];
-  /** Roles of the user */
-  private roles: Role[] = [];
   /** Current user */
   private user: User | null = null;
 
@@ -100,9 +87,6 @@ export class ApplicationsComponent implements OnInit, OnDestroy {
     if (this.authSubscription) {
       this.authSubscription.unsubscribe();
     }
-    if (this.applicationSubscription) {
-      this.applicationSubscription.unsubscribe();
-    }
   }
 
   /**
@@ -112,146 +96,5 @@ export class ApplicationsComponent implements OnInit, OnDestroy {
    */
   onOpenApplication(appID: any): void {
     this.applicationService.loadApplication(appID);
-    this.roles = this.user?.roles || [];
-    this.permissions = this.user?.permissions || [];
-
-    this.applicationSubscription =
-      this.applicationService.application$.subscribe(
-        (application: Application | null) => {
-          if (application) {
-            this.title = application.name || '';
-            this.appID = application.id || '';
-            const adminNavItems: any[] = [];
-            if (
-              this.permissions.some(
-                (x) =>
-                  (x.type === Permissions.canSeeUsers &&
-                    this.roles.some(
-                      (y) =>
-                        y.application?.id === application.id &&
-                        y.permissions?.some((perm) => perm.id === x.id)
-                    )) ||
-                  (x.type === Permissions.canManageApplications && x.global)
-              )
-            ) {
-              adminNavItems.push({
-                name: 'Users',
-                path: `../${this.appID}/settings/users`,
-                icon: 'supervisor_account',
-              });
-            }
-            if (
-              this.permissions.some(
-                (x) =>
-                  (x.type === Permissions.canSeeRoles &&
-                    this.roles.some(
-                      (y) =>
-                        y.application?.id === application.id &&
-                        y.permissions?.some((perm) => perm.id === x.id)
-                    )) ||
-                  (x.type === Permissions.canManageApplications && x.global)
-              )
-            ) {
-              adminNavItems.push({
-                name: 'Roles',
-                path: `../${this.appID}/settings/roles`,
-                icon: 'admin_panel_settings',
-              });
-            }
-            this.navGroups = [
-              {
-                name: 'Pages',
-                navItems: application.pages
-                  ?.filter((x) => x.content)
-                  .map((x) => ({
-                    name: x.name,
-                    path:
-                      x.type === ContentType.form
-                        ? `../${this.appID}/${x.type}/${x.id}`
-                        : `../${this.appID}/${x.type}/${x.content}`,
-                    icon: this.getNavIcon(x.type || ''),
-                  })),
-              },
-              {
-                name: 'Administration',
-                navItems: adminNavItems,
-              },
-            ];
-            if (!this.application || application.id !== this.application.id) {
-              const [firstPage, ..._] = application.pages || [];
-              const find = !this.application
-                ? this.validPage(application)
-                : false;
-              if (
-                !find &&
-                (this.router.url.endsWith('/') ||
-                  application.id !== this.application?.id ||
-                  !firstPage)
-              ) {
-                if (firstPage) {
-                  this.router.navigate(
-                    [
-                      `../${this.appID}/${firstPage.type}/${
-                        firstPage.type === ContentType.form
-                          ? firstPage.id
-                          : firstPage.content
-                      }`,
-                    ],
-                    { relativeTo: this.route }
-                  );
-                } else {
-                  this.router.navigate([`../${this.appID}`], {
-                    relativeTo: this.route,
-                  });
-                }
-              }
-            }
-            this.application = application;
-            this.appID = application.id || '';
-          } else {
-            this.navGroups = [];
-          }
-        }
-      );
-  }
-
-  /**
-   * Gets nav icon from page content type.
-   *
-   * @param type content type of the page
-   * @returns icon
-   */
-  private getNavIcon(type: string): string {
-    switch (type) {
-      case 'workflow':
-        return 'linear_scale';
-      case 'form':
-        return 'description';
-      default:
-        return 'dashboard';
-    }
-  }
-
-  /**
-   * Checks if route page is valid.
-   *
-   * @param app application to check pages of
-   * @returns Is page valid or not
-   */
-  private validPage(app: any): boolean {
-    if (
-      this.appPage &&
-      (this.appPage === 'profile' ||
-        this.appPage === 'settings/users' ||
-        this.appPage === 'settings/roles' ||
-        app.pages?.find(
-          (val: any) =>
-            val.type + '/' + val.content === this.appPage ||
-            val.type + '/' + val.id === this.appPage
-        ))
-    ) {
-      return true;
-    }
-    return false;
   }
 }
