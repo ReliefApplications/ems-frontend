597f5f6f2b9b31c1af0bac42b80f15524fbc0130
Antoine Hurard <antoine.reliefapps@gmail.com>
2022-10-24 09:44:02 +0200

    add summary card to list of available widgets

--- projects/back-office/src/environments/environment.ts
+++ projects/back-office/src/environments/environment.ts
@@ -43,5 +43,5 @@ export const environment = {
   esriApiKey:
     'AAPKf2bae9b3f32943e2a8d58b0b96ffea3fj8Vt8JYDt1omhzN_lONXPRHN8B89umU-pA9t7ze1rfCIiiEVXizYEiFRFiVrl6wg',
   theme,
-  availableWidgets: ['chart', 'grid', 'editor', 'map'],
+  availableWidgets: ['chart', 'grid', 'editor', 'map', 'summaryCard'],
 };
