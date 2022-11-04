e4ab1b0aa506df37dab7f89c98259ebc67e349f4
Antoine Hurard <antoine.reliefapps@gmail.com>
2022-11-04 12:35:09 +0100

    add loading indicator to breadcrumbs when loading

--- projects/safe/src/lib/components/ui/breadcrumb/breadcrumb.module.ts
+++ projects/safe/src/lib/components/ui/breadcrumb/breadcrumb.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { SafeBreadcrumbComponent } from './breadcrumb.component';
 import { RouterModule } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
+import { SkeletonModule } from '@progress/kendo-angular-indicators';
 
 /**
  * Breadcrumb component module.
  */
 @NgModule({
   declarations: [SafeBreadcrumbComponent],
-  imports: [CommonModule, RouterModule, TranslateModule],
+  imports: [CommonModule, RouterModule, TranslateModule, SkeletonModule],
   exports: [SafeBreadcrumbComponent],
 })
 export class SafeBreadcrumbModule {}
