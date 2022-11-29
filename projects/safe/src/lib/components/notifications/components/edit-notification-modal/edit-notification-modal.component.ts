990d4cef926858db31b8f02c010e302d0ab559c6
Antoine Hurard <antoine.reliefapps@gmail.com>
2022-11-29 17:12:25 +0100

    start ui to add / edit / delete custom notifications

--- projects/safe/src/lib/components/notifications/components/edit-notification-modal/edit-notification-modal.component.ts
+++ projects/safe/src/lib/components/notifications/components/edit-notification-modal/edit-notification-modal.component.ts
@@ -1,4 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { cronValidator } from '../../../../utils/validators/cron.validator';
+import get from 'lodash/get';
+import { CustomNotification } from '../../../../models/custom-notification.model';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+/**
+ * Dialog data interface
+ */
+interface DialogData {
+  notification: CustomNotification;
+}
 
 /**
  * Add / Edit custom notification modal component.
@@ -9,10 +21,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-notification-modal.component.scss'],
 })
 export class EditNotificationModalComponent implements OnInit {
+  public formGroup!: FormGroup;
+
   /**
    * Add / Edit custom notification modal component.
+   *
+   * @param formBuilder Angular form builder
+   * @param data Modal injected data
    */
-  constructor() {}
+  constructor(
+    private formBuilder: FormBuilder,
+    @Inject(MAT_DIALOG_DATA)
+    public data: DialogData
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.formGroup = this.formBuilder.group({
+      name: [get(this.data, 'notification.name', ''), Validators.required],
+      description: [get(this.data, 'notification.description', '')],
+      schedule: [
+        get(this.data, 'pullJob.schedule', ''),
+        [Validators.required, cronValidator()],
+      ],
+      notificationType: [{ value: 'email', disabled: true }],
+      resource: [get(this.data, 'notification.resource', '')],
+      layout: [get(this.data, 'notification.layout', '')],
+      template: [get(this.data, 'notification.template', '')],
+    });
+  }
 }
--- projects/safe/src/lib/components/notifications/components/edit-notification-modal/edit-notification-modal.module.ts
+++ projects/safe/src/lib/components/notifications/components/edit-notification-modal/edit-notification-modal.module.ts
@@ -2,13 +2,33 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EditNotificationModalComponent } from './edit-notification-modal.component';
 import { SafeModalModule } from '../../../ui/modal/modal.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { SafeAlertModule } from '../../../ui/alert/alert.module';
+import { SafeReadableCronModule } from '../../../../pipes/readable-cron/readable-cron.module';
+import { SafeDividerModule } from '../../../ui/divider/divider.module';
 
 /**
  * Add / Edit notification modal module.
  */
 @NgModule({
   declarations: [EditNotificationModalComponent],
-  imports: [CommonModule, SafeModalModule],
+  imports: [
+    CommonModule,
+    SafeModalModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatTooltipModule,
+    SafeAlertModule,
+    SafeReadableCronModule,
+    SafeDividerModule,
+  ],
   exports: [EditNotificationModalComponent],
 })
 export class EditNotificationModalModule {}
