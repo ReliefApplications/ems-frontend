7863f0f83a3a60e03f19201bd5c87bb605ddc75d
Antoine Hurard <antoine.reliefapps@gmail.com>
2022-12-05 12:20:09 +0100

    style: replace table shadows + widget shadows

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./projects/**/src/**/*.{html,ts}"
+    "./projects/**/src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'light': '0 2px 5px 0 rgba(134, 134, 134, 0.2)',
+      }
+    },
   },
   plugins: [],
 }
