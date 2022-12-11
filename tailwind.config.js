b9674e5834767c48d511b5888d9eeb34d2029c36
Matheus Dias <matheus-relief>
2022-12-11 20:37:12 -0300

    refactor: cdk-drag-preview class

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,17 @@
 
 module.exports = {
   content: [
-    "./projects/**/src/**/*.{html,ts}",
-    "./dist/safe/**/*.{html,ts,js,mjs}",
-    "./node_modules/safe/**/*.mjs"
+    './projects/**/src/**/*.{html,ts}',
+    './dist/safe/**/*.{html,ts,js,mjs}',
+    './node_modules/safe/**/*.mjs',
   ],
   theme: {
     extend: {
       boxShadow: {
-        'light': '0 2px 5px 0 rgba(134, 134, 134, 0.2)',
-      }
+        light: '0 2px 5px 0 rgba(134, 134, 134, 0.2)',
+        dark: '0 5px 5px -3px rgba(0, 0, 0, 0.2), 0 8px 10px 1px rgba(0, 0, 0, 0.14), 0 3px 14px 2px rgba(0, 0, 0, 0.12)',
+      },
     },
   },
   plugins: [],
-}
+};
