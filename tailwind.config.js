5d0bf04441336e31ba45be0629b11ecb3b3eb6e1
Antoine Hurard <antoine.reliefapps@gmail.com>
2022-12-12 09:15:29 +0100

    style: change cdk-drag-preview box-shadow, and add white background

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,7 @@ module.exports = {
   theme: {
     extend: {
       boxShadow: {
-        light: '0 2px 5px 0 rgba(134, 134, 134, 0.2)',
-        dark: '0 5px 5px -3px rgba(0, 0, 0, 0.2), 0 8px 10px 1px rgba(0, 0, 0, 0.14), 0 3px 14px 2px rgba(0, 0, 0, 0.12)',
+        '2lg': '0 2px 5px 0 rgba(134, 134, 134, 0.2)',
       },
     },
   },
