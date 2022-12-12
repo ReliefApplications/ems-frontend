4f833d5878b8323664514a68489f21d05d1dbb11
Matheus Dias <matheus-relief>
2022-12-11 21:34:45 -0300

    refactor: replace inline styles w tailwind classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
+  important: true,
   content: [
-    "./projects/**/src/**/*.{html,ts}",
-    "./dist/safe/**/*.{html,ts,js,mjs}",
-    "./node_modules/safe/**/*.mjs"
+    './projects/**/src/**/*.{html,ts}',
+    './dist/safe/**/*.{html,ts,js,mjs}',
+    './node_modules/safe/**/*.mjs',
   ],
   theme: {
     extend: {
       boxShadow: {
-        'light': '0 2px 5px 0 rgba(134, 134, 134, 0.2)',
-      }
+        light: '0 2px 5px 0 rgba(134, 134, 134, 0.2)',
+      },
+      flexGrow: {
+        2: 2,
+        3: 3,
+      },
     },
   },
   plugins: [],
-}
+};
