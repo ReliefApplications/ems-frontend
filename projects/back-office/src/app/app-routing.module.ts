fb71c678bef3bb3f23d97476fad36dfa07d0f2a3
Antoine Hurard <antoine.reliefapps@gmail.com>
2023-01-24 09:07:37 +0100

    update angular to v15

--- projects/back-office/src/app/app-routing.module.ts
+++ projects/back-office/src/app/app-routing.module.ts
@@ -62,9 +62,7 @@ const routes: Routes = [
  */
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, {
-      relativeLinkResolution: 'legacy',
-    }),
+    RouterModule.forRoot(routes, {}),
   ],
   exports: [RouterModule],
 })
--- projects/front-office/src/app/app-routing.module.ts
+++ projects/front-office/src/app/app-routing.module.ts
@@ -39,9 +39,7 @@ const routes: Routes = [
  */
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, {
-      relativeLinkResolution: 'legacy',
-    }),
+    RouterModule.forRoot(routes, {}),
   ],
   exports: [RouterModule],
 })
