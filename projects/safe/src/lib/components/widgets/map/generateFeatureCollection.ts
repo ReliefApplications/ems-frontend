3e9cb4b1fb2bf7e64229391c0e928f9ad835db90
Antoine Hurard <antoine.reliefapps@gmail.com>
2023-02-03 11:01:18 +0100

    fix incorrect location + change images in popup

--- projects/safe/src/lib/components/widgets/map/generateFeatureCollection.ts
+++ projects/safe/src/lib/components/widgets/map/generateFeatureCollection.ts
@@ -1,5 +1,10 @@
 import { FeatureCollection } from 'geojson';
 
+/** Bound for the latitude in a map */
+const latBounds = [-90, 90];
+/** Bound for the longitude in a map */
+const lngBounds = [-180, 180];
+
 /** Represents the types of geometries that can be generated */
 type FeatureTypes = 'Point' | 'Polygon' | 'LineString';
 
@@ -25,6 +30,14 @@ const DEFAULT_MIN_DISTANCE = 5;
 /** Location icon svg */
 const LOCATION_ICON_SVG =
   '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M396.6 6.5L235.8 129.1c9.6 1.8 18.9 5.8 27 12l168 128c13.2 10.1 22 24.9 24.5 41.4l6.2 41.5H608c9.3 0 18.2-4.1 24.2-11.1s8.8-16.4 7.4-25.6l-24-160c-1.2-8.2-5.6-15.7-12.3-20.7l-168-128c-11.5-8.7-27.3-8.7-38.8 0zm-153.2 160c-11.5-8.7-27.3-8.7-38.8 0l-168 128c-6.6 5-11 12.5-12.3 20.7l-24 160c-1.4 9.2 1.3 18.6 7.4 25.6S22.7 512 32 512H224V352l96 160h96c9.3 0 18.2-4.1 24.2-11.1s8.8-16.4 7.4-25.6l-24-160c-1.2-8.2-5.6-15.7-12.3-20.7l-168-128z"/></svg>';
+
+/** @returns random latitude */
+const randomLat = () =>
+  Math.random() * (latBounds[1] - latBounds[0] + 1) + latBounds[0];
+
+/** @returns random longitude */
+const randomLng = () =>
+  Math.random() * (lngBounds[1] - lngBounds[0] + 1) + lngBounds[0];
 
 /**
  * Generates random features based on the specified options
@@ -55,7 +68,7 @@ export const generateRandomFeatures = (
     let coordinates;
     switch (type) {
       case 'Point':
-        coordinates = [Math.random() * 180 - 90, Math.random() * 360 - 180];
+        coordinates = [randomLng(), randomLat()];
         break;
       case 'Polygon':
       case 'LineString':
@@ -64,8 +77,8 @@ export const generateRandomFeatures = (
         let previousLat = 0;
         let previousLng = 0;
         for (let j = 0; j < numCoordinates; j++) {
-          let lat = Math.random() * 180 - 90;
-          let lng = Math.random() * 360 - 180;
+          let lat = randomLat();
+          let lng = randomLng();
           if (j !== 0) {
             let latDifference = Math.abs(previousLat - lat);
             let lngDifference = Math.abs(previousLng - lng);
@@ -75,15 +88,15 @@ export const generateRandomFeatures = (
               latDifference < minDistance ||
               lngDifference < minDistance
             ) {
-              lat = Math.random() * 180 - 90;
-              lng = Math.random() * 360 - 180;
+              lat = randomLat();
+              lng = randomLng();
               latDifference = Math.abs(previousLat - lat);
               lngDifference = Math.abs(previousLng - lng);
             }
           }
           previousLat = lat;
           previousLng = lng;
-          coordinates.push([lat, lng]);
+          coordinates.push([lng, lat]);
         }
         if (type === 'Polygon') {
           coordinates = [coordinates];
--- projects/safe/src/lib/components/widgets/map/heatmap-test.ts
+++ projects/safe/src/lib/components/widgets/map/heatmap-test.ts
@@ -62,8 +62,8 @@ export const generateHeatMap = (
           title,
           'marker-symbol': 'harbor',
           imgSrc: random
-            ? 'https://media.npr.org/assets/img/2017/09/12/macaca_nigra_self-portrait-3e0070aa19a7fe36e802253048411a38f14a79f8-s800-c85.webp'
-            : 'https://i.pinimg.com/originals/4b/92/8b/4b928be7d4b2d54c72227350709adebd.jpg',
+            ? 'https://upload.wikimedia.org/wikipedia/commons/e/e1/FullMoon2010.jpg'
+            : 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Saturn_during_Equinox.jpg/560px-Saturn_during_Equinox.jpg',
         };
       },
     },
