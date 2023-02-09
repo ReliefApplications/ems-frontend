bd7b0b20a23428aa2ff53bf7aeb67aebe9ad1db4
MwanPygmay <matt.brunot@laposte.net>
2023-02-09 09:22:00 +0100

    Unfunctionnal commit

--- projects/safe/src/lib/components/widgets/grid-settings/tab-buttons/tab-buttons.component.ts
+++ projects/safe/src/lib/components/widgets/grid-settings/tab-buttons/tab-buttons.component.ts
@@ -1,8 +1,16 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { UntypedFormArray, UntypedFormGroup } from '@angular/forms';
 import { createButtonFormGroup } from '../grid-settings.forms';
 import { Form } from '../../../../models/form.model';
 import { Channel } from '../../../../models/channel.model';
+import {
+  CdkDragDrop,
+  CdkDragEnter,
+  CdkDragExit,
+  moveItemInArray,
+} from '@angular/cdk/drag-drop';
+import { MatTabGroup } from '@angular/material/tabs';
+import { BehaviorSubject } from 'rxjs';
 
 /**
  * Buttons tab of grid widget configuration modal.
@@ -12,13 +20,27 @@ import { Channel } from '../../../../models/channel.model';
   templateUrl: './tab-buttons.component.html',
   styleUrls: ['./tab-buttons.component.scss'],
 })
-export class TabButtonsComponent {
+export class TabButtonsComponent implements OnInit {
   @Input() formGroup!: UntypedFormGroup;
   @Input() fields: any[] = [];
   @Input() relatedForms: Form[] = [];
   @Input() channels: Channel[] = [];
   @Input() templates: any[] = [];
   @Input() distributionLists: any[] = [];
+
+  @ViewChild('tabGroup', { static: false }) childTabGroup!: MatTabGroup;
+
+  CHILD_ID_NAME = 'menu-name';
+  childMenuIds$ = new BehaviorSubject<string[]>([]);
+  menus: number[] = [];
+
+  ngOnInit(): void {
+    const floatingButtons = this.formGroup?.get(
+      'floatingButtons'
+    ) as UntypedFormArray;
+    this.menus = [...Array(floatingButtons.length).keys()];
+    this.recalculateUniqIdsForDragDrop();
+  }
 
   /** @returns List of the floating buttons */
   get buttons(): UntypedFormArray {
@@ -29,12 +51,17 @@ export class TabButtonsComponent {
 
   /**
    * Adds a floating button configuration.
+   *
+   * @param event sf
    */
-  public addButton(): void {
+  public addButton(event: MouseEvent): void {
     const floatingButtons = this.formGroup?.get(
       'floatingButtons'
     ) as UntypedFormArray;
     floatingButtons.push(createButtonFormGroup({ show: true }));
+    event.stopPropagation();
+    this.menus.push(this.menus.length + 1);
+    this.recalculateUniqIdsForDragDrop();
   }
 
   /**
@@ -47,5 +74,102 @@ export class TabButtonsComponent {
       'floatingButtons'
     ) as UntypedFormArray;
     floatingButtons.removeAt(index);
+    this.menus.splice(index, 1);
+    this.recalculateUniqIdsForDragDrop();
+  }
+
+  /**
+   *tgs
+   *
+   * @param index dfg
+   * @returns rst
+   */
+  trackByIndex(index: number): number {
+    return index;
+  }
+
+  /**
+   * d
+   *
+   * @param event d
+   */
+  onDropTab(event: CdkDragDrop<string[]>): void {
+    const previousIndex = parseInt(
+      event.previousContainer.id.replace(this.CHILD_ID_NAME, ''),
+      10
+    );
+    const newIndex = parseInt(
+      event.container.id.replace(this.CHILD_ID_NAME, ''),
+      10
+    );
+    moveItemInArray(this.menus, previousIndex, newIndex);
+    this.showDragWrapper(event);
+  }
+
+  /**
+   * c
+   *
+   * @param event d
+   */
+  onDragEntered(event: CdkDragEnter): void {
+    this.hideDragWrapper(event);
+  }
+
+  /**
+   * d
+   *
+   * @param event d
+   */
+  onDragExited(event: CdkDragExit): void {
+    this.showDragWrapper(event);
+  }
+
+  /**
+   * d
+   *
+   * @param event d
+   */
+  private showDragWrapper(event: CdkDragExit | CdkDragDrop<string[]>): void {
+    const element = this.getDragWrappedElement(event);
+    if (element) {
+      element.classList.remove('d-none');
+    }
+  }
+
+  /**
+   * d
+   *
+   * @param event d
+   */
+  private hideDragWrapper(event: CdkDragEnter): void {
+    const element = this.getDragWrappedElement(event);
+    if (element) {
+      element.classList.add('d-none');
+    }
+  }
+
+  /**
+   * berth
+   *
+   * @param event d
+   * @returns dreg
+   */
+  private getDragWrappedElement(
+    event: CdkDragEnter | CdkDragExit
+  ): HTMLElement | null {
+    return event.container.element.nativeElement.querySelector(`.drag-wrapper`);
+  }
+
+  /**
+   * berth
+   */
+  private recalculateUniqIdsForDragDrop(): void {
+    const uniqIds: string[] = [];
+    this.menus.reduce((accumulator: string[], _, index) => {
+      accumulator.push(`${this.CHILD_ID_NAME}${index}`);
+      return accumulator;
+    }, uniqIds);
+    this.childMenuIds$.next(uniqIds);
+    console.log('reordered ids: ', this.childMenuIds$);
   }
 }
