0271a358e7377a8d1e1df64256265f4bc461d5ef
Antoine Hurard <antoine.reliefapps@gmail.com>
2023-02-15 14:28:04 +0100

    prevent date filter menu to close on click

---
+++ projects/safe/src/lib/components/ui/core-grid/date-filter-menu/date-filter-menu.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SafeDateFilterMenuComponent } from './date-filter-menu.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TranslateModule } from '@ngx-translate/core';
+import { ButtonModule, ButtonsModule } from '@progress/kendo-angular-buttons';
+import { DateInputsModule } from '@progress/kendo-angular-dateinputs';
+import { DropDownsModule } from '@progress/kendo-angular-dropdowns';
+import { InputsModule } from '@progress/kendo-angular-inputs';
+
+@NgModule({
+  declarations: [SafeDateFilterMenuComponent],
+  imports: [
+    CommonModule,
+    ButtonModule,
+    ButtonsModule,
+    TranslateModule,
+    FormsModule,
+    ReactiveFormsModule,
+    DropDownsModule,
+    InputsModule,
+    DateInputsModule,
+  ],
+  exports: [SafeDateFilterMenuComponent],
+})
+export class SafeDateFilterMenuModule {}
