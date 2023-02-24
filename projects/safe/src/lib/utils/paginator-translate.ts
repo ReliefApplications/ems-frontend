1d02d26a253a540757f2025a13c0b52e60457801
unknown <caio.reliefapps@gmail.com>
2023-02-24 09:24:21 -0300

    linting fixes

--- projects/safe/src/lib/utils/paginator-translate.ts
+++ projects/safe/src/lib/utils/paginator-translate.ts
@@ -1,6 +1,6 @@
-import { Injectable } from "@angular/core";
-import { MatPaginatorIntl } from "@angular/material/paginator";
-import { TranslateService } from "@ngx-translate/core";
+import { Injectable } from '@angular/core';
+import { MatPaginatorIntl } from '@angular/material/paginator';
+import { TranslateService } from '@ngx-translate/core';
 
 @Injectable()
 export class MatPaginationIntlService extends MatPaginatorIntl {
@@ -8,22 +8,26 @@ export class MatPaginationIntlService extends MatPaginatorIntl {
     super();
 
     this.translateService.onLangChange.subscribe((_event: Event) => {
-      this.translateLabels();
+      if (_event) {
+        this.translateLabels();
+      }
     });
 
     this.translateLabels();
   }
 
   getRangeLabel = (page: number, pageSize: number, length: number): string => {
-    const of = this.translateService ? this.translateService.instant("common.pagination.of") : "of";
+    const of = this.translateService ?
+      this.translateService.instant('common.pagination.of') : 'of';
     if (length === 0 || pageSize === 0) {
-      return "0 " + of + " " + length;
+      return '0 ' + of + ' ' + length;
     }
     length = Math.max(length, 0);
-    const startIndex = page * pageSize > length ? (Math.ceil(length / pageSize) - 1) * pageSize : page * pageSize;
+    const startIndex = page * pageSize > length ?
+      (Math.ceil(length / pageSize) - 1) * pageSize : page * pageSize;
 
     const endIndex = Math.min(startIndex + pageSize, length);
-    return startIndex + 1 + " - " + endIndex + " " + of + " " + length;
+    return startIndex + 1 + ' - ' + endIndex + ' ' + of + ' ' + length;
   };
 
   injectTranslateService(translate: TranslateService): void {
@@ -37,7 +41,9 @@ export class MatPaginationIntlService extends MatPaginatorIntl {
   }
 
   translateLabels(): void {
-    this.itemsPerPageLabel = this.translateService.instant("common.pagination.itemsPerPage");
+    this.itemsPerPageLabel = this.translateService.instant(
+      'common.pagination.itemsPerPage'
+    );
     this.changes.next();
   }
 }
