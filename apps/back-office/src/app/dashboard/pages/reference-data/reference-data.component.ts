46a2d49b4e597a640b97509bb62741180f312535
Antoine Hurard <antoine.reliefapps@gmail.com>
2023-03-08 12:55:33 +0100

    resolve lint errors

--- apps/back-office/src/app/dashboard/pages/reference-data/reference-data.component.ts
+++ apps/back-office/src/app/dashboard/pages/reference-data/reference-data.component.ts
@@ -4,7 +4,6 @@ import {
   FormControl,
   FormGroup,
   UntypedFormBuilder,
-  UntypedFormGroup,
   Validators,
 } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
--- apps/front-office/src/environments/environment.shared.ts
+++ apps/front-office/src/environments/environment.shared.ts
@@ -3,5 +3,6 @@
  */
 export const sharedEnvironment = {
   module: 'frontoffice',
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
   version: require('../../../../package.json').version,
 };
