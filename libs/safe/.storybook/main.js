3edee6db436e5febdda7a0321b4056b85a1c49a9
Antoine Hurard <antoine.reliefapps@gmail.com>
2023-03-08 17:30:10 +0100

    start to fix storybook

---
+++ libs/safe/.storybook/main.js
@@ -0,0 +1,13 @@
+module.exports = {
+  core: { builder: 'webpack5' },
+  // stories: ['../**/*.stories.mdx', '../**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: [
+    '../**/*.stories.mdx',
+    '../src/lib/components/ui/button/button.stories.@(js|jsx|ts|tsx)',
+  ],
+  addons: ['@storybook/addon-essentials'],
+};
+
+// To customize your webpack configuration you can use the webpackFinal field.
+// Check https://storybook.js.org/docs/react/builders/webpack#extending-storybooks-webpack-config
+// and https://nx.dev/packages/storybook/documents/custom-builder-configs
