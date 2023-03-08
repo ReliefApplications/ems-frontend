dea1a2d2074193b4bb65bf12d3ffd28d3712db12
Antoine Hurard <antoine.reliefapps@gmail.com>
2023-03-08 18:30:27 +0100

    generate story for safe

--- libs/safe/.storybook/main.js
+++ libs/safe/.storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   core: { builder: 'webpack5' },
   // stories: ['../**/*.stories.mdx', '../**/*.stories.@(js|jsx|ts|tsx)'],
@@ -6,6 +8,23 @@ module.exports = {
     '../src/lib/components/ui/button/button.stories.@(js|jsx|ts|tsx)',
   ],
   addons: ['@storybook/addon-essentials'],
+  // webpackFinal: async (config, { configType }) => {
+  //   // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+  //   // You can change the configuration based on that.
+  //   // 'PRODUCTION' is used when building the static version of storybook.
+
+  //   // Make whatever fine-grained changes you need
+  //   config.module.rules.push({
+  //     test: /\.m?js$/,
+  //     resolve: {
+  //       fullySpecified: false,
+  //     },
+  //   });
+
+  //   config.resolve.alias['variables.scss'] = path.resolve(__dirname, '../stories/variables.scss');
+  //   // Return the altered config
+  //   return config;
+  // },
 };
 
 // To customize your webpack configuration you can use the webpackFinal field.
