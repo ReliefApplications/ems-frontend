6327f21663ae04d4e0b20e744497dd05f37917f4
Valentin Lécart <valentin.reliefapps@gmail.com>
2023-03-17 12:47:36 +0100

    feat: add fields component in map layer settings

--- libs/safe/src/lib/components/widgets/map-settings/edit-layer-modal/layer-fields/layer-fields.component.ts
+++ libs/safe/src/lib/components/widgets/map-settings/edit-layer-modal/layer-fields/layer-fields.component.ts
@@ -1,5 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
+/**
+ * Fields interface
+ */
+interface Fields {
+  label: string;
+  name: string;
+  type: string;
+}
 /**
  * Map layer fields settings component.
  */
@@ -8,4 +16,17 @@ import { Component } from '@angular/core';
   templateUrl: './layer-fields.component.html',
   styleUrls: ['./layer-fields.component.scss'],
 })
-export class LayerFieldsComponent {}
+export class LayerFieldsComponent {
+  @Input() fields: Fields[] = [];
+  /**
+   * Save value of the input
+   *
+   * @param event event of the input.
+   * @param index index of the field.
+   */
+  saveLabel(event: string, index: number): void {
+    if (event && this.fields[index]) {
+      this.fields[index].label = event;
+    }
+  }
+}
