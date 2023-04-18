2a2bb22a72539c04be1904d8db8d9854af5727d3
Bricecrx <94597541+Bricecrx@users.noreply.github.com>
2023-04-18 12:29:25 +0200

    fix : add of one different story for each case of breadcrumbs #61944

--- libs/ui/src/lib/breadcrumbs/breadcrumbs.component.stories.ts
+++ libs/ui/src/lib/breadcrumbs/breadcrumbs.component.stories.ts
@@ -17,8 +17,8 @@ const Template: Story<BreadcrumbsComponent> = (args: BreadcrumbsComponent) => ({
   props: args,
 });
 
-export const Primary = Template.bind({});
-Primary.args = {
+export const SimpleChevron = Template.bind({});
+SimpleChevron.args = {
   breadcrumbs: [
     {
       text: 'item 0',
@@ -35,4 +35,64 @@ Primary.args = {
   ],
   separator: BreadcrumbSeparator.CHEVRON,
   display: BreadcrumbDisplay.SIMPLE,
+};
+
+export const SimpleSlash = Template.bind({});
+SimpleSlash.args = {
+  breadcrumbs: [
+    {
+      text: 'item 0',
+      uri: '#',
+    },
+    {
+      text: 'item 1',
+      uri: '#',
+    },
+    {
+      text: 'item 2',
+      uri: '#',
+    },
+  ],
+  separator: BreadcrumbSeparator.SLASH,
+  display: BreadcrumbDisplay.SIMPLE,
+};
+
+export const Contained = Template.bind({});
+Contained.args = {
+  breadcrumbs: [
+    {
+      text: 'item 0',
+      uri: '#',
+    },
+    {
+      text: 'item 1',
+      uri: '#',
+    },
+    {
+      text: 'item 2',
+      uri: '#',
+    },
+  ],
+  separator: BreadcrumbSeparator.CHEVRON,
+  display: BreadcrumbDisplay.CONTAINED,
+};
+
+export const Full = Template.bind({});
+Full.args = {
+  breadcrumbs: [
+    {
+      text: 'item 0',
+      uri: '#',
+    },
+    {
+      text: 'item 1',
+      uri: '#',
+    },
+    {
+      text: 'item 2',
+      uri: '#',
+    },
+  ],
+  separator: BreadcrumbSeparator.CHEVRON,
+  display: BreadcrumbDisplay.FULL,
 };
