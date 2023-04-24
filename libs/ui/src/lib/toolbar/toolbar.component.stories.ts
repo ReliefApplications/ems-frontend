3ace1c472a1499bf35cebc0575a73fa72b5c739e
Bricecrx <94597541+Bricecrx@users.noreply.github.com>
2023-04-24 15:10:20 +0200

    feat/AB#62243 working on component (template, contents, stories) but issue with stories

--- libs/ui/src/lib/toolbar/toolbar.component.stories.ts
+++ libs/ui/src/lib/toolbar/toolbar.component.stories.ts
@@ -1,19 +1,60 @@
 import { moduleMetadata, Story, Meta } from '@storybook/angular';
 import { ToolbarComponent } from './toolbar.component';
+import { Variant } from '../shared/variant.enum';
+import { ToolbarModule } from './toolbar.module';
 
 export default {
-  title: 'ToolbarComponent',
+  title: 'Toolbar',
   component: ToolbarComponent,
+  argTypes: {
+    toolbarVariant: {
+      options: Variant,
+      control: 'select',
+    },
+  },
   decorators: [
     moduleMetadata({
-      imports: [],
+      imports: [ToolbarModule],
     }),
   ],
 } as Meta<ToolbarComponent>;
 
 const Template: Story<ToolbarComponent> = (args: ToolbarComponent) => ({
   props: args,
+  template: `
+  <ui-toolbar [color]="color">
+  <ng-container ngProjectAs="leftWingContent">
+  <button
+    class="bg-gray-200 text-gray-700 shadow-md hover:shadow-xl hover:bg-gray-300 p-2 rounded-md"
+  >
+    Test
+  </button>
+  </ng-container>
+  <div ngProjectAs="headerText">Back-office</div>
+  <div ngProjectAs="userName">Jean-Eudes</div>
+  <div ngProjectAs="userMail">je@reliefapps.org</div>
+  <ng-container ngProjectAs="rightWingContent">
+  <button
+    class="bg-gray-200 text-gray-700 shadow-md hover:shadow-xl hover:bg-gray-300 p-2 rounded-md"
+  >
+    Random content
+  </button>
+  <button
+    class="bg-gray-200 text-gray-700 shadow-md hover:shadow-xl hover:bg-gray-300 p-2 rounded-md"
+  >
+    Random bis
+  </button>
+  </ng-container>
+  </ui-toolbar>`,
 });
 
 export const Primary = Template.bind({});
-Primary.args = {};
+Primary.args = {
+  color: '#6f51ae',
+  toolbarVariant: Variant.LIGHT,
+};
+export const Secondary = Template.bind({});
+Secondary.args = {
+  color: '#ceded8',
+  toolbarVariant: Variant.DARK,
+};
--- libs/ui/src/lib/toolbar/toolbar.component.ts
+++ libs/ui/src/lib/toolbar/toolbar.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Variant } from '../shared/variant.enum';
 
 @Component({
   selector: 'ui-toolbar',
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss'],
 })
-export class ToolbarComponent {}
+export class ToolbarComponent {
+  @Input() color = '';
+  @Input() toolbarVariant!: Variant;
+
+  toolbarVariants = Variant;
+}
