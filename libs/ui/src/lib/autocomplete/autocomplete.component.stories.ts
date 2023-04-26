af7c014db76a0aae136e144c1b3e0af72975e8ed
estelafs <estelafersil@gmail.com>
2023-04-26 14:22:55 -0300

    added optinal control value accessor and filter to the autocomplete component

--- libs/ui/src/lib/autocomplete/autocomplete.component.stories.ts
+++ libs/ui/src/lib/autocomplete/autocomplete.component.stories.ts
@@ -1,31 +1,22 @@
-import { moduleMetadata, Meta, StoryObj } from '@storybook/angular';
+import { moduleMetadata, Meta, StoryFn } from '@storybook/angular';
 import { AutocompleteComponent } from './autocomplete.component';
 import { AutocompleteModule } from './autocomplete.module';
 import { AutocompleteOptions } from './interfaces/autocomplete-options.interface';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 /** Autocomplete options example */
 const options: AutocompleteOptions[] = [
   {
     label: 'A',
-    children: [
-      {
-        label: 'option A 1',
-      },
-    ],
   },
   {
-    label: 'B',
-    children: [
-      {
-        label: 'B 1',
-      },
-      {
-        label: 'bê two',
-      },
-    ],
+    label: 'a B',
   },
   {
     label: 'C',
+  },
+  {
+    label: 'D',
   },
 ];
 
@@ -33,24 +24,126 @@ export default {
   title: 'Autocomplete',
   component: AutocompleteComponent,
   argTypes: {
+    placeholder: {
+      control: 'text',
+    },
     required: {
       control: 'boolean',
     },
     options: {
       control: 'array',
     },
+    disabled: {
+      type: 'boolean',
+    },
   },
   decorators: [
     moduleMetadata({
-      imports: [AutocompleteModule],
+      imports: [AutocompleteModule, ReactiveFormsModule],
     }),
   ],
 } as Meta<AutocompleteComponent>;
 
-/** Primary autocomplete story */
-export const Default: StoryObj<AutocompleteComponent> = {
-  args: {
-    required: true,
-    options,
-  },
+/**
+ * Template autocomplete
+ *
+ * @param {AutocompleteComponent} args args
+ * @returns AutocompleteComponent
+ */
+const Template: StoryFn<AutocompleteComponent> = (
+  args: AutocompleteComponent
+) => ({
+  props: args,
+});
+
+/**
+ * Form control template autocomplete
+ *
+ * @param {AutocompleteComponent} args args
+ * @returns AutocompleteComponent
+ */
+const FormControlTemplate: StoryFn<AutocompleteComponent> = (
+  args: AutocompleteComponent
+) => {
+  const formGroup = new FormGroup({
+    autocomplete: new FormControl(''),
+  });
+  args.options = options;
+  args.required = true;
+  args.placeholder = 'Select a value';
+  return {
+    component: AutocompleteComponent,
+    template: `
+      <form [formGroup]="formGroup">
+        <ui-autocomplete
+          [options]="options"
+          [required]="required"
+          [placeholder]="placeholder"
+          formControlName="autocomplete"
+        ></ui-autocomplete>
+      </form>
+      <br>
+      <p>value: {{formGroup.get('autocomplete').value}}</p>
+      <p>touched: {{formGroup.get('autocomplete').touched}}</p>
+    `,
+    props: {
+      ...args,
+      formGroup,
+    },
+  };
+};
+
+/** Form control autocomplete */
+export const FormAutocomplete = FormControlTemplate.bind({});
+
+/** Autocomplete with grouped options */
+export const GroupedOptions = Template.bind({});
+GroupedOptions.args = {
+  required: true,
+  options: [
+    {
+      label: 'A',
+      children: [
+        {
+          label: 'option A 1',
+        },
+      ],
+    },
+    {
+      label: 'B',
+      children: [
+        {
+          label: 'B 1',
+        },
+        {
+          label: 'b',
+        },
+      ],
+    },
+    {
+      label: 'C',
+    },
+    {
+      label: 'D',
+      children: [
+        {
+          label: 'D 1',
+        },
+        {
+          label: 'D two',
+        },
+        {
+          label: 'D a b',
+        },
+      ],
+    },
+  ],
+};
+
+/** Autocomplete with simple options */
+export const SimpleOptions = Template.bind({});
+SimpleOptions.args = {
+  placeholder: 'Select a value',
+  required: false,
+  options,
 };
--- libs/ui/src/lib/autocomplete/autocomplete.component.ts
+++ libs/ui/src/lib/autocomplete/autocomplete.component.ts
@@ -1,11 +1,28 @@
 import {
-  AfterViewInit,
   Component,
   EventEmitter,
   Input,
+  OnInit,
   Output,
+  ElementRef,
+  HostListener,
+  Provider,
+  forwardRef,
 } from '@angular/core';
 import { AutocompleteOptions } from './interfaces/autocomplete-options.interface';
+import {
+  FormControl,
+  Validators,
+  ControlValueAccessor,
+  NG_VALUE_ACCESSOR,
+} from '@angular/forms';
+
+/** A provider for the ControlValueAccessor interface. */
+const CONTROL_VALUE_ACCESSOR: Provider = {
+  provide: NG_VALUE_ACCESSOR,
+  useExisting: forwardRef(() => AutocompleteComponent),
+  multi: true,
+};
 
 /**
  * UI Autocomplete component
@@ -14,8 +31,9 @@ import { AutocompleteOptions } from './interfaces/autocomplete-options.interface
   selector: 'ui-autocomplete',
   templateUrl: './autocomplete.component.html',
   styleUrls: ['./autocomplete.component.scss'],
+  providers: [CONTROL_VALUE_ACCESSOR],
 })
-export class AutocompleteComponent implements AfterViewInit {
+export class AutocompleteComponent implements ControlValueAccessor, OnInit {
   @Input() placeholder = '';
   @Input() required = false;
   @Input() options!: AutocompleteOptions[];
@@ -24,19 +42,53 @@ export class AutocompleteComponent implements AfterViewInit {
   @Output() closed: EventEmitter<boolean> = new EventEmitter();
   @Output() selectOption: EventEmitter<AutocompleteOptions> =
     new EventEmitter();
+  disabled = false;
 
+  public formControl = new FormControl('');
+  public filteredOptions!: AutocompleteOptions[];
   public open = false;
   public optionSelected: AutocompleteOptions = { label: '' };
 
-  ngAfterViewInit(): void {
-    console.log(this.options);
-    document.addEventListener('click', (event: any) => {
-      console.log('target', event?.target);
-      console.log('event ', event);
-      if (document.getElementById('autocomplete')?.contains(event?.target)) {
-        console.log('--> IF');
+  onChange!: (value: string) => void;
+  onTouch!: () => void;
+
+  /**
+   * Listen to click event on the document to close the panel when clicking outside.
+   *
+   * @param event Mouse event
+   */
+  @HostListener('document:click', ['$event'])
+  clickOut(event: MouseEvent) {
+    if (!this.elementRef.nativeElement.contains(event.target) && this.open) {
+      this.open = false;
+      this.closed.emit(true);
+    }
+  }
+
+  /**
+   * Creates an instance of AutocompleteComponent.
+   *
+   * @param elementRef shared element ref service
+   */
+  constructor(private elementRef: ElementRef) {}
+
+  ngOnInit(): void {
+    this.filteredOptions = [...this.options];
+    if (this.required) this.formControl.addValidators(Validators.required);
+    if (this.disabled) this.formControl.disable();
+    this.formControl.valueChanges.subscribe((searchText) => {
+      if (searchText) {
+        this.filteredOptions = this.filterAutocompleteOptions(
+          [...this.options],
+          searchText
+        );
+        if (this.onTouch && this.onChange) {
+          this.onTouch();
+          this.onChange(searchText);
+        }
       } else {
-        console.log('--> else');
+        if (this.optionSelected) this.optionSelected.selected = false;
+        this.filteredOptions = this.options;
       }
     });
   }
@@ -47,25 +99,97 @@ export class AutocompleteComponent implements AfterViewInit {
    * @param value selected option
    */
   public onSelect(value: AutocompleteOptions): void {
-    console.log('onSelect', value);
     value.selected = true;
-    if (this.optionSelected) this.optionSelected.selected = false;
-    this.optionSelected = value;
+    if (this.optionSelected) {
+      this.optionSelected.selected = false;
+    }
     this.open = false;
+    this.optionSelected = value;
     this.closed.emit(true);
-    // console.log(this.options)
+    if (this.onTouch && this.onChange) {
+      this.onTouch();
+      this.onChange(value.label);
+    }
     this.selectOption.emit(value);
   }
 
-  onClick(e: any): void {
+  /** Handles the panel opening */
+  public onOpen(): void {
     if (!this.open) {
       this.open = true;
       this.opened.emit(true);
     }
-    // else {
-    //   this.open = false;
-    //   this.closed.emit(true);
-    // }
-    // console.log('onKey:', e, this.open)
+  }
+
+  /**
+   * Write value of control.
+   *
+   * @param value new value
+   */
+  public writeValue(value: string): void {
+    this.optionSelected.label = value;
+  }
+
+  /**
+   * Register new method to call when control state change
+   *
+   * @param fn callback function
+   */
+  public registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  /**
+   * Register new method to call when control touch state change
+   *
+   * @param fn callback function
+   */
+  public registerOnTouched(fn: any): void {
+    this.onTouch = fn;
+  }
+
+  /**
+   * Set disabled state of the control
+   *
+   * @param isDisabled is control disabled
+   */
+  public setDisabledState(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
+
+  /**
+   * Recursively filter the original options list based on the label and return the matching options
+   *
+   * @param options autocomplete options original list
+   * @param searchText text typed in the input
+   * @returns filtered options that match the search text
+   */
+  private filterAutocompleteOptions(
+    options: AutocompleteOptions[],
+    searchText: string
+  ): AutocompleteOptions[] {
+    const filteredOptions: AutocompleteOptions[] = [];
+    options.forEach((option: AutocompleteOptions) => {
+      const filteredOption: AutocompleteOptions = {
+        label: option.label,
+        selected: option.selected ?? false,
+      };
+      if (option.children && option.children.length > 0) {
+        const filteredChildren = this.filterAutocompleteOptions(
+          option.children,
+          searchText
+        );
+        if (filteredChildren.length > 0) {
+          filteredOption.children = filteredChildren;
+        }
+      }
+      if (
+        option.label.toLowerCase().includes(searchText.toLowerCase()) ||
+        (filteredOption.children && filteredOption.children.length > 0)
+      ) {
+        filteredOptions.push(filteredOption);
+      }
+    });
+    return filteredOptions;
   }
 }
