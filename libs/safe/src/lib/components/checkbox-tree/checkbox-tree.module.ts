c8b9c9402e186a9c33f4549310cd813cbfa134c3
Unai Zalba <unai@reliefapplications.org>
2023-05-10 10:29:35 +0200

    refactor/63567_ABC-replace-menu-tree-component refactor: replace material tree by cdk tree refactor: remove material tree module

--- libs/safe/src/lib/components/checkbox-tree/checkbox-tree.module.ts
+++ libs/safe/src/lib/components/checkbox-tree/checkbox-tree.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SafeCheckboxTreeComponent } from './checkbox-tree.component';
 import { CdkTreeModule } from '@angular/cdk/tree';
-import { MatTreeModule } from '@angular/material/tree';
 import { MatLegacyCheckboxModule as MatCheckboxModule } from '@angular/material/legacy-checkbox';
 import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
 import { MatLegacyFormFieldModule as MatFormFieldModule } from '@angular/material/legacy-form-field';
@@ -18,7 +17,6 @@ import { MatIconModule } from '@angular/material/icon';
   imports: [
     CommonModule,
     CdkTreeModule,
-    MatTreeModule,
     MatCheckboxModule,
     MatButtonModule,
     MatFormFieldModule,
