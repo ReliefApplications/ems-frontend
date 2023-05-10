a289ef7b70f1ef172cace4cd2759a1b8bd5d5fb6
estelafs <estelafersil@gmail.com>
2023-05-10 15:23:38 -0300

    added kendo loading indicator

--- libs/ui/src/lib/breadcrumbs/breadcrumbs.module.ts
+++ libs/ui/src/lib/breadcrumbs/breadcrumbs.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { BreadcrumbsComponent } from './breadcrumbs.component';
 import { TranslateModule } from '@ngx-translate/core';
 import { RouterModule } from '@angular/router';
+import { SkeletonModule } from '@progress/kendo-angular-indicators';
 
 /**
  * UI Breadcrumbs Module.
  */
 @NgModule({
   declarations: [BreadcrumbsComponent],
-  imports: [CommonModule, TranslateModule, RouterModule],
+  imports: [CommonModule, TranslateModule, RouterModule, SkeletonModule],
   exports: [BreadcrumbsComponent],
 })
 export class BreadcrumbsModule {}
