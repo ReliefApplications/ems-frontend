d962dde07602c22936187764366d2e06aceac3cb
estelafs <estelafersil@gmail.com>
2023-05-23 11:48:55 -0300

    deleted SafeModalComponent

--- libs/safe/src/index.ts
+++ libs/safe/src/index.ts
@@ -89,7 +89,6 @@ export * from './lib/components/skeleton/skeleton-table/public-api';
 export * from './lib/components/mapping/public-api';
 export * from './lib/components/role-summary/public-api';
 export * from './lib/components/graphql-select/public-api';
-export * from './lib/components/ui/modal/public-api';
 export * from './lib/components/ui/empty/public-api';
 export * from './lib/components/edit-calculated-field-modal/public-api';
 export * from './lib/components/utils/unsubscribe/public-api';
