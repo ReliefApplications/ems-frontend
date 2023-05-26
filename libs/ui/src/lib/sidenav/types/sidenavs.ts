ee44a20ecb253b7e62f03dfdbb921d9ddf8dc730
Bricecrx <94597541+Bricecrx@users.noreply.github.com>
2023-05-26 08:52:27 +0200

    refactor/AB#64178_ABC-Replace-Sidenav-Component (#1370)
    * refactor/AB#64178_ABC-Replace-Sidenav-Component fix : deal with some prettier non formatted code that was preventing from pushing
    
    * refactor/AB#64178_ABC-Replace-Sidenav-Component fix : sidenav is now not collapsing when clicking outside of it (in side mode only)
    
    * refactor/AB#64178_ABC-Replace-Sidenav-Component fix : removed styling issues due to navbar and further issue that weren't thought of during creation of the component refactor : removed all mat-sidenav component + imports
    
    * refactor/AB#64178_ABC-Replace-Sidenav-Component refactor : a bit of cleaning
    
    * refactor/AB#64178_ABC-Replace-Sidenav-Component refactor : modified layout component so the app-toolbar is included in sidenav content and is dealt by the sidenav mecanism
    
    * refactor/AB#64178_ABC-Replace-Sidenav-Component refactor: delete not necessary classes plus change some in order to adapt to the new ui sidenav container fix: restore mat sidenavs for web widget components as the new ui sidenav does not have to be used in web widget library refactor: new ui sidenav component to the needs of our back-office application
    
    * refactor/AB#64178_ABC-Replace-Sidenav-Component fix: linting
    
    * refactor/AB#64178_ABC-Replace-Sidenav-Component fix: z-index issue of sidenav with opened modals refactor: deleted dark: class from the sidenav
    
    * refactor/AB#64178_ABC-Replace-Sidenav-Component feat: add second right sidenav option
    
    * change styling of left sidenav to don't have shadow
    
    ---------
    
    Co-authored-by: Unai Zalba <unai@reliefapplications.org>
    Co-authored-by: Antoine Hurard <antoine.reliefapps@gmail.com>

--- libs/ui/src/lib/sidenav/types/sidenavs.ts
+++ libs/ui/src/lib/sidenav/types/sidenavs.ts
@@ -3,3 +3,8 @@
  */
 export const sidenavs = ['side', 'over'] as const;
 export type SidenavTypes = (typeof sidenavs)[number];
+/**
+ * Sidenav position types
+ */
+export const sidenavPositions = ['start', 'end'] as const;
+export type SidenavPositionTypes = (typeof sidenavPositions)[number];
