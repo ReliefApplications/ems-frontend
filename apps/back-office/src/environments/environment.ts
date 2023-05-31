6b8bd3498402f5c29445812c238f66d4fcdb9cce
RenzoPrats <renzoprats@icloud.com>
2023-05-31 12:52:45 -0300

    fixed environment

--- apps/back-office/src/environments/environment.ts
+++ apps/back-office/src/environments/environment.ts
@@ -2,30 +2,18 @@ import { AuthConfig } from 'angular-oauth2-oidc';
 import { theme } from '../themes/default/default.local';
 import { sharedEnvironment } from './environment.shared';
 
-const authConfig: AuthConfig = {
-  issuer:
-    'https://login.microsoftonline.com/fbacd48d-ccf4-480d-baf0-31048368055f/v2.0',
-  redirectUri: 'http://localhost:4200/',
-  postLogoutRedirectUri: 'http://localhost:4200/auth/',
-  clientId: 'd62083d8-fdc0-4a6a-8618-652380eebdb9',
-  scope: 'openid profile email offline_access',
-  responseType: 'code',
-  showDebugInformation: true,
-  strictDiscoveryDocumentValidation: false,
-};
-
 /**
  * Authentication configuration
  */
-// const authConfig: AuthConfig = {
-//   issuer: 'https://id-dev.oortcloud.tech/auth/realms/oort',
-//   redirectUri: 'http://localhost:4200/',
-//   postLogoutRedirectUri: 'http://localhost:4200/auth/',
-//   clientId: 'oort-client',
-//   scope: 'openid profile email offline_access',
-//   responseType: 'code',
-//   showDebugInformation: true,
-// };
+const authConfig: AuthConfig = {
+  issuer: 'https://id-dev.oortcloud.tech/auth/realms/oort',
+  redirectUri: 'http://localhost:4200/',
+  postLogoutRedirectUri: 'http://localhost:4200/auth/',
+  clientId: 'oort-client',
+  scope: 'openid profile email offline_access',
+  responseType: 'code',
+  showDebugInformation: true,
+};
 
 /**
  * Environment file for local development.
@@ -39,8 +27,6 @@ export const environment = {
   backOfficeUri: 'http://localhost:4200/',
   availableLanguages: ['en', 'fr', 'test'],
   authConfig,
-  esriApiKey:
-    'AAPKf2bae9b3f32943e2a8d58b0b96ffea3fj8Vt8JYDt1omhzN_lONXPRHN8B89umU-pA9t7ze1rfCIiiEVXizYEiFRFiVrl6wg',
   theme,
   availableWidgets: [
     'donut-chart',
