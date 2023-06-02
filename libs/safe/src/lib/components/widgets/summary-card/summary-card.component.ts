1a2d6cc7b6f0ee61760c2256722d1d9bb61e3d66
Nathan <65243509+NathanHGit@users.noreply.github.com>
2023-06-02 12:38:56 +0200

    increase amount of searchable fields

--- libs/safe/src/lib/components/widgets/summary-card/summary-card.component.ts
+++ libs/safe/src/lib/components/widgets/summary-card/summary-card.component.ts
@@ -215,22 +215,71 @@ export class SafeSummaryCardComponent implements OnInit, AfterViewInit {
       const filters: {
         field: string;
         operator: string;
-        value: string | number;
+        value: string | number | boolean | Array<any> | Date;
       }[] = [];
       this.fields.forEach((field) => {
         if (skippedFields.includes(field.name)) return;
-        if (field?.type === 'text')
+
+        // string
+        if (
+          [
+            'text',
+            'color',
+            'email',
+            'tel',
+            'url',
+            'comment',
+            'radiogroup',
+            'dropdown',
+          ].includes(field?.type)
+        )
           filters.push({
             field: field.name,
             operator: 'contains',
             value: search,
           });
-        if (field?.type === 'numeric' && !isNaN(parseFloat(search)))
+
+        // number
+        if (
+          (field?.type === 'numeric' || field?.name === 'range') &&
+          !isNaN(parseFloat(search))
+        )
           filters.push({
             field: field.name,
             operator: 'eq',
             value: parseFloat(search),
           });
+
+        // boolean
+        if (field?.type === 'boolean') {
+          try {
+            filters.push({
+              field: field.name,
+              operator: 'eq',
+              value: Boolean(JSON.parse(search)), // allows numerical and textual search (1 / true)
+            });
+          } catch {
+            // cannot be parsed to JSON
+          }
+        }
+
+        // array
+        if (['checkbox', 'tagbox'].includes(field?.type)) {
+          filters.push({
+            field: field.name,
+            operator: 'contains',
+            value: [search],
+          });
+        }
+
+        // date
+        if (['date', 'datetime', 'datetime-local'].includes(field?.type)) {
+          filters.push({
+            field: field.name,
+            operator: 'eq',
+            value: new Date(search),
+          });
+        }
       });
       this.pageInfo.skip = 0;
       this.dataQuery?.refetch({
