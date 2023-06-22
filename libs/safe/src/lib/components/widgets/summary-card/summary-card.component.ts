d4f1ee336885b4c0fa8d511cfae9c5ccbf2b6cef
Antoine Hurard <antoine.reliefapps@gmail.com>
2023-06-22 18:59:54 +0400

    fix: scroll indicator in summary card could appear infinitively

--- libs/safe/src/lib/components/widgets/summary-card/summary-card.component.ts
+++ libs/safe/src/lib/components/widgets/summary-card/summary-card.component.ts
@@ -428,7 +428,7 @@ export class SafeSummaryCardComponent
       e.target.scrollHeight - (e.target.clientHeight + e.target.scrollTop) <
       50
     ) {
-      if (!this.loading && this.pageInfo.length > this.cards.length) {
+      if (!this.scrolling && this.pageInfo.length > this.cards.length) {
         this.dataQuery
           ?.fetchMore({
             variables: {
