464fb65c6c6e361ca16c1a94779993458ab28bdd
Antoine Hurard <antoine.reliefapps@gmail.com>
2023-07-06 22:34:51 +0400

    fix: chart would not display nicely in too small

--- libs/safe/src/lib/components/widget/widget.component.ts
+++ libs/safe/src/lib/components/widget/widget.component.ts
@@ -4,12 +4,16 @@ import {
   Output,
   EventEmitter,
   ViewChild,
+  HostBinding,
+  OnInit,
+  OnDestroy,
 } from '@angular/core';
 import { SafeChartComponent } from '../widgets/chart/chart.component';
 import { SafeEditorComponent } from '../widgets/editor/editor.component';
 import { SafeGridWidgetComponent } from '../widgets/grid/grid.component';
 import { SafeMapWidgetComponent } from '../widgets/map/map.component';
 import { SafeSummaryCardComponent } from '../widgets/summary-card/summary-card.component';
+import { v4 as uuidv4 } from 'uuid';
 
 /** Component for the widgets */
 @Component({
@@ -17,7 +21,7 @@ import { SafeSummaryCardComponent } from '../widgets/summary-card/summary-card.c
   templateUrl: './widget.component.html',
   styleUrls: ['./widget.component.scss'],
 })
-export class SafeWidgetComponent {
+export class SafeWidgetComponent implements OnInit, OnDestroy {
   @Input() widget: any;
   @Input() header = true;
   @Input() canUpdate = false;
@@ -30,6 +34,11 @@ export class SafeWidgetComponent {
       return true;
     }
   }
+
+  private customStyle?: HTMLStyleElement;
+
+  @HostBinding()
+  id = `widget-${uuidv4()}`;
 
   @ViewChild('widgetContent')
   widgetContentComponent!:
@@ -44,4 +53,24 @@ export class SafeWidgetComponent {
 
   // === STEP CHANGE FOR WORKFLOW ===
   @Output() changeStep: EventEmitter<number> = new EventEmitter();
+
+  ngOnInit(): void {
+    console.log('init widget');
+    // Deactivate that to use custom styling
+    // const css = `#${this.id} {
+    //   background-color: red;
+    // }`;
+    // const head = document.getElementsByTagName('head')[0];
+    // this.customStyle = document.createElement('style');
+    // this.customStyle.appendChild(document.createTextNode(css));
+    // head.appendChild(this.customStyle);
+  }
+
+  ngOnDestroy(): void {
+    console.log('destroy widget');
+    // Deactivate that to use custom stying
+    // if (this.customStyle) {
+    //   document.getElementsByTagName('head')[0].removeChild(this.customStyle);
+    // }
+  }
 }
