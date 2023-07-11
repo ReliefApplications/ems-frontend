80fd034892c447f59cbefe08362fca953487ac0e
Nathan <65243509+NathanHGit@users.noreply.github.com>
2023-07-11 11:21:51 +0200

    remove style editor from widget settings

--- libs/safe/src/lib/components/widgets/common/display-settings/display-settings.component.ts
+++ libs/safe/src/lib/components/widgets/common/display-settings/display-settings.component.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
 import { ToggleModule } from '@oort-front/ui';
-import { MonacoEditorModule } from 'ngx-monaco-editor-v2';
 
 /** Component for selecting the widget display options */
 @Component({
@@ -15,16 +14,10 @@ import { MonacoEditorModule } from 'ngx-monaco-editor-v2';
     ReactiveFormsModule,
     ToggleModule,
     TranslateModule,
-    MonacoEditorModule,
   ],
   templateUrl: './display-settings.component.html',
   styleUrls: ['./display-settings.component.scss'],
 })
 export class DisplaySettingsComponent {
   @Input() formGroup!: FormGroup;
-  public editorOptions = {
-    theme: 'vs-dark',
-    language: 'scss',
-    fixedOverflowWidgets: true,
-  };
 }
