4477f99cc13e76ba221c0989ad7457cdc56cec01
MwanPygmay <matt.brunot@laposte.net>
2023-08-08 16:48:17 +0200

    changed styling for dropdowns, and fixed problem when option selected

--- libs/safe/src/lib/components/ui/aggregation-builder/pipeline/field-dropdown/field-dropdown.component.ts
+++ libs/safe/src/lib/components/ui/aggregation-builder/pipeline/field-dropdown/field-dropdown.component.ts
@@ -2,7 +2,9 @@ import {
   AfterViewInit,
   Component,
   Input,
+  OnChanges,
   QueryList,
+  SimpleChanges,
   ViewChild,
   ViewChildren,
 } from '@angular/core';
@@ -22,14 +24,27 @@ interface ExpansionTree {
   templateUrl: './field-dropdown.component.html',
   styleUrls: ['./field-dropdown.component.scss'],
 })
-export class SafeFieldDropdownComponent implements AfterViewInit {
+export class SafeFieldDropdownComponent implements AfterViewInit, OnChanges {
   @Input() fieldControl!: UntypedFormControl;
   @Input() fields: any[] = [];
   @Input() label = '';
   @Input() nullable = false;
   @ViewChildren('field') fieldComponents!: QueryList<any>;
   @ViewChild('menu') selectMenu!: any;
+  private currentValueUnnested = false;
   public expansionTree: ExpansionTree = {};
+
+  ngAfterViewInit(): void {
+    this.selectMenu.forceOptionList(this.fieldComponents);
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.fields.currentValue != changes.fields.previousValue) {
+      this.expansionTree = {};
+      this.buildExpansionTree(this.fields, this.expansionTree);
+      this.unnestCurrentValue();
+    }
+  }
 
   /**
    * Builds the expansion tree recursively
@@ -52,27 +67,39 @@ export class SafeFieldDropdownComponent implements AfterViewInit {
     }
   }
 
-  ngAfterViewInit(): void {
-    this.selectMenu.forceOptionList(this.fieldComponents);
-    this.buildExpansionTree(this.fields, this.expansionTree);
+  /** Unnests the value that is displayed inside of the select menu */
+  unnestCurrentValue() {
+    if (!this.fieldControl.value || !this.fieldControl.value.includes('.')) {
+      this.currentValueUnnested = true;
+    }
+    if (!this.currentValueUnnested) {
+      const array: string[] = this.fieldControl.value.split('.');
+      for (let i = 1; i < array.length; i++) {
+        this.toggleNodeExpansion(array.slice(0, i).join('.'));
+      }
+      this.currentValueUnnested = this.toggleNodeExpansion(array.join('.'));
+    }
   }
 
   /**
    * expands target node
    *
    * @param path path to be expanded
+   * @returns whether the operation was a success
    */
-  toggleNodeExpansion(path: string): void {
+  toggleNodeExpansion(path: string): boolean {
     const pathSegments = path.split('.');
     let node = this.expansionTree;
 
     if (!node) {
-      return;
+      return false;
     }
 
     for (const segment of pathSegments) {
+      console.log(node, segment);
       node = node[segment] as ExpansionTree;
     }
     node.expanded = !node.expanded;
+    return true;
   }
 }
