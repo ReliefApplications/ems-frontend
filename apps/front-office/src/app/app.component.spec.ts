5f34a4f04ddf0123cce241a2ec11b007aff5b4ef
Renzo Prats <56398308+RenzoPrats@users.noreply.github.com>
2023-08-10 05:29:58 -0300

    refactor: add more front-office tests
    * fixed tests in front-office
    
    * lint fixed
    
    * fix some tests
    
    * partial fix of app component test for front office
    
    ---------
    
    Co-authored-by: Antoine Hurard <antoine.reliefapps@gmail.com>

--- apps/front-office/src/app/app.component.spec.ts
+++ apps/front-office/src/app/app.component.spec.ts
@@ -1,4 +1,4 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { DialogModule as DialogCdkModule } from '@angular/cdk/dialog';
@@ -7,20 +7,24 @@ import { TranslateModule } from '@ngx-translate/core';
 import { OAuthModule } from 'angular-oauth2-oidc';
 import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
+import { ApolloTestingModule } from 'apollo-angular/testing';
+import { Ability } from '@casl/ability';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
+  beforeAll(async () => {
     await TestBed.configureTestingModule({
       imports: [
         RouterTestingModule,
-        HttpClientModule,
+        HttpClientTestingModule,
         OAuthModule.forRoot(),
         DialogCdkModule,
         ReactiveFormsModule,
         TranslateModule.forRoot(),
+        ApolloTestingModule,
       ],
       declarations: [AppComponent],
       providers: [
+        Ability,
         {
           provide: 'environment',
           useValue: environment,
@@ -39,14 +43,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('front-office');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain(
-      'front-office app is running!'
-    );
   });
 });
