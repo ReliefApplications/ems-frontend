c5188e6a9222fcb0b9080f67b82417912e2a1e6b
RenzoPrats <renzoprats@icloud.com>
2023-08-10 08:03:13 -0300

    fixed some tests

--- apps/back-office/src/app/app-preview/pages/dashboard/dashboard.component.spec.ts
+++ apps/back-office/src/app/app-preview/pages/dashboard/dashboard.component.spec.ts
@@ -1,6 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ApolloTestingModule } from 'apollo-angular/testing';
 import { DashboardComponent } from './dashboard.component';
+import { of } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import {
+  TranslateFakeLoader,
+  TranslateLoader,
+  TranslateModule,
+  TranslateService,
+} from '@ngx-translate/core';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
@@ -9,6 +17,28 @@ describe('DashboardComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [DashboardComponent],
+      imports: [
+        ApolloTestingModule,
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useClass: TranslateFakeLoader,
+          },
+        }),
+      ],
+      providers: [
+        TranslateService,
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            params: of({}),
+          },
+        },
+        {
+          provide: 'environment',
+          useValue: {},
+        },
+      ]
     }).compileComponents();
   });
 
