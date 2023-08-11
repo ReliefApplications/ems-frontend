2b7ab8cd6a91d488f5a840770f298aa1c5a6c849
RenzoPrats <renzoprats@icloud.com>
2023-08-10 21:01:52 -0300

    Finished fixing components tests

--- apps/back-office/src/app/app-preview/pages/workflow/workflow.component.spec.ts
+++ apps/back-office/src/app/app-preview/pages/workflow/workflow.component.spec.ts
@@ -1,6 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ApolloTestingModule } from 'apollo-angular/testing';
 import { WorkflowComponent } from './workflow.component';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
+import {
+  TranslateFakeLoader,
+  TranslateLoader,
+  TranslateModule,
+  TranslateService,
+} from '@ngx-translate/core';
+import { SafeWorkflowStepperModule } from '@oort-front/safe';
 
 describe('WorkflowComponent', () => {
   let component: WorkflowComponent;
@@ -9,6 +18,25 @@ describe('WorkflowComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [WorkflowComponent],
+      imports: [
+        ApolloTestingModule,
+        SafeWorkflowStepperModule,
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useClass: TranslateFakeLoader,
+          },
+        }),
+      ],
+      providers: [
+        TranslateService,
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            params: of({}),
+          },
+        },
+      ]
     }).compileComponents();
   });
 
--- apps/back-office/src/app/application/pages/home/home.component.spec.ts
+++ apps/back-office/src/app/application/pages/home/home.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HomeComponent } from './home.component';
 
 describe('HomeComponent', () => {
