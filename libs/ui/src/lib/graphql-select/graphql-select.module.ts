6312ebd93dd81ae7694f90fc6501b18f8f5952c3
Antoine Hurard <antoine.reliefapps@gmail.com>
2023-08-14 15:13:18 +0200

    simplify empty list display for graphql select

--- libs/ui/src/lib/graphql-select/graphql-select.module.ts
+++ libs/ui/src/lib/graphql-select/graphql-select.module.ts
@@ -6,7 +6,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SpinnerModule } from '../spinner/spinner.module';
 import { ButtonModule } from '../button/button.module';
 import { SelectMenuModule } from '../select-menu/select-menu.module';
-import { IconModule } from '../icon/icon.module';
 
 /**
  * Select module for GraphQL queries.
@@ -21,7 +20,6 @@ import { IconModule } from '../icon/icon.module';
     SpinnerModule,
     ButtonModule,
     SelectMenuModule,
-    IconModule,
   ],
   exports: [GraphQLSelectComponent],
 })
