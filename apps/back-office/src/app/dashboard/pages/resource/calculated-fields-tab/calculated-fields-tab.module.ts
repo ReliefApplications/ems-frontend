bace37558a9400db1183830fc93fb5f05148d66d
Nathan <humbertclaude.nathan@gmail.com>
2023-08-30 12:54:33 +0200

    add ui divider in menus

--- apps/back-office/src/app/dashboard/pages/resource/calculated-fields-tab/calculated-fields-tab.module.ts
+++ apps/back-office/src/app/dashboard/pages/resource/calculated-fields-tab/calculated-fields-tab.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CalculatedFieldsTabRoutingModule } from './calculated-fields-tab-routing.module';
 import { OverlayModule } from '@angular/cdk/overlay';
-import { IconModule } from '@oort-front/ui';
+import { DividerModule, IconModule } from '@oort-front/ui';
 import { TranslateModule } from '@ngx-translate/core';
 import { SafeEmptyModule } from '@oort-front/safe';
 import { CalculatedFieldsTabComponent } from './calculated-fields-tab.component';
@@ -23,6 +23,7 @@ import { MenuModule, ButtonModule, TableModule } from '@oort-front/ui';
     SafeEmptyModule,
     ButtonModule,
     TableModule,
+    DividerModule,
   ],
 })
 export class CalculatedFieldsTabModule {}
