613ed067afc1325e73346ebacb4dcb6ed98f52be
matheus-relief <matheus.reliefapps@gmail.com>
2023-09-02 02:16:22 -0300

    make function run on each key down

--- libs/safe/src/lib/survey/functions/searchEnterpriseName.ts
+++ libs/safe/src/lib/survey/functions/searchEnterpriseName.ts
@@ -18,7 +18,7 @@ const EXAMPLES_TO_FETCH = 5;
  */
 export default (options: GlobalOptions) => {
   const { apollo, form } = options;
-
+  let listenerAdded = false;
   /**
    * Custom function that searches for the current enterprise name being entered.
    *
@@ -35,69 +35,92 @@ export default (options: GlobalOptions) => {
     },
     params: any[]
   ) {
-    if (!form?.queryName) {
-      return this.returnResult(null);
+    if (listenerAdded) {
+      return;
     }
-    // Tries to get the question name from the params, or defaults to 'name'
-    const question = this.survey.getQuestionByName(params[0] ?? 'name');
+    listenerAdded = true;
+    let timeout: NodeJS.Timeout | null = null;
 
-    if (!question || !question.value) {
-      return this.returnResult(null);
-    }
-    const query = gql<QueryResponse, QueryVariables>`
-        query GetCustomQuery($first: Int, $filter: JSON) {
-          ${form.queryName}(first: $first, filter: $filter) {
-            edges {
-              node {
-                ${question.name}
+    this.survey.onValueChanged.add((__: any, opt: any) => {
+      // Tries to get the question name from the params, or defaults to 'name'
+      const question = this.survey.getQuestionByName(params[0] ?? 'name');
+      const queryName = form?.queryName;
+
+      if (!queryName || !question || !question.value) {
+        return this.returnResult(null);
+      }
+
+      if (opt.question.name === question.name) {
+        if (timeout) {
+          clearTimeout(timeout);
+        }
+        timeout = setTimeout(async () => {
+          const query = gql<QueryResponse, QueryVariables>`
+            query GetCustomQuery($first: Int, $filter: JSON) {
+              ${queryName}(first: $first, filter: $filter) {
+                edges {
+                  node {
+                    ${question.name}
+                  }
+                }
               }
             }
-          }
-        }
-      `;
+          `;
 
-    const response = await firstValueFrom(
-      apollo.query<any>({
-        query,
-        variables: {
-          first: EXAMPLES_TO_FETCH,
-          filter: {
-            logic: 'and',
-            filters: [
-              {
-                field: question.name,
-                operator: 'contains',
-                value: question.value,
+          this.survey.setVariable(
+            'enterpriseSearchStatus',
+            'Searching matching names...'
+          );
+          const response = await firstValueFrom(
+            apollo.query<any>({
+              query,
+              variables: {
+                first: EXAMPLES_TO_FETCH,
+                filter: {
+                  logic: 'and',
+                  filters: [
+                    {
+                      field: question.name,
+                      operator: 'contains',
+                      value: question.value,
+                    },
+                  ],
+                },
               },
-            ],
-          },
-        },
-      })
-    );
+            })
+          );
 
-    const names: string[] = response.data[form.queryName].edges.map(
-      (edge: any) => edge.node[question.name]
-    );
-    if (names.length === 0) {
-      return this.returnResult(null);
-    }
+          const names: string[] = response.data[queryName].edges.map(
+            (edge: any) => edge.node[question.name]
+          );
+          if (names.length === 0) {
+            this.survey.setVariable(
+              'enterpriseSearchStatus',
+              'No matches found'
+            );
+            return this.returnResult(null);
+          }
 
-    return this.returnResult(`
-    <div class="flex flex-col ml-[14px]">
-      ${names
-        .map((n) => {
-          const name = `<p>${n}</p>`;
-          // match the corresponding value in the name and put it in bold
-          const match = n.match(new RegExp(question.value, 'gi'));
-          return match
-            ? name.replace(
-                new RegExp(question.value, 'gi'),
-                `<b>${match[0]}</b>`
-              )
-            : name;
-        })
-        .join('')}
-    </div>
-    `);
+          this.survey.setVariable('enterpriseSearchStatus', 'Search complete');
+          return this.returnResult(`
+            <div class="flex flex-col ml-[14px]">
+              ${names
+                .map((n) => {
+                  const name = `<p>${n}</p>`;
+                  // match the corresponding value in the name and put it in bold
+                  const match = n.match(new RegExp(question.value, 'gi'));
+                  return match
+                    ? name.replace(
+                        new RegExp(question.value, 'gi'),
+                        `<b>${match[0]}</b>`
+                      )
+                    : name;
+                })
+                .join('')}
+            </div>
+          `);
+        }, 500);
+      }
+    });
   };
 };
