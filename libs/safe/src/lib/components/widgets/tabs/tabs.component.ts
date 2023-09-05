3d9b7880edd286efe8fa8c153be6669a57fd0cc9
Antoine Hurard <antoine.reliefapps@gmail.com>
2023-09-05 10:06:35 +0200

    add possibility in tabs widget to open settings from template if empty

--- libs/safe/src/lib/components/widgets/tabs/tabs.component.ts
+++ libs/safe/src/lib/components/widgets/tabs/tabs.component.ts
@@ -1,4 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Dialog } from '@angular/cdk/dialog';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { SafeDashboardService } from '../../../services/dashboard/dashboard.service';
+import { SafeUnsubscribeComponent } from '../../utils/unsubscribe/unsubscribe.component';
+import { takeUntil } from 'rxjs';
 
 /**
  * Tabs widget component.
@@ -8,9 +12,49 @@ import { Component, Input } from '@angular/core';
   templateUrl: './tabs.component.html',
   styleUrls: ['./tabs.component.scss'],
 })
-export class TabsComponent {
+export class TabsComponent extends SafeUnsubscribeComponent {
   /** Should display header */
   @Input() header = true;
   /** Widget settings */
   @Input() settings: any;
+  /** Widget definition */
+  @Input() widget: any;
+  /** Editable widget */
+  @Input() canUpdate = false;
+  /** Widget edit event */
+  @Output() edit: EventEmitter<any> = new EventEmitter();
+
+  /**
+   * Tabs widget component.
+   *
+   * @param dialog CDK Dialog service
+   * @param dashboardService Shared dashboard service
+   */
+  constructor(
+    private dialog: Dialog,
+    private dashboardService: SafeDashboardService
+  ) {
+    super();
+  }
+
+  /**
+   * Open settings
+   */
+  async openSettings(): Promise<void> {
+    const { SafeTileDataComponent } = await import(
+      '../../widget-grid/floating-options/menu/tile-data/tile-data.component'
+    );
+    const dialogRef = this.dialog.open(SafeTileDataComponent, {
+      disableClose: true,
+      data: {
+        tile: this.widget,
+        template: this.dashboardService.findSettingsTemplate(this.widget),
+      },
+    });
+    dialogRef.closed.pipe(takeUntil(this.destroy$)).subscribe((res: any) => {
+      if (res) {
+        this.edit.emit({ type: 'data', id: this.widget.id, options: res });
+      }
+    });
+  }
 }
