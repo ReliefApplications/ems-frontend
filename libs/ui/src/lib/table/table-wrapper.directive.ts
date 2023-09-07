cbcec22ac298bda13333f97266674501d63ac938
RenzoPrats <renzoprats@icloud.com>
2023-09-07 15:45:39 -0300

    unified safe-skeleton-table

--- libs/ui/src/lib/table/table-wrapper.directive.ts
+++ libs/ui/src/lib/table/table-wrapper.directive.ts
@@ -9,13 +9,17 @@ import {
   Output,
   QueryList,
   Renderer2,
+  Input,
+  OnChanges,
+  SimpleChanges,
   ViewContainerRef,
-  TemplateRef
+  createComponent,
+  EnvironmentInjector
 } from '@angular/core';
 import { TableSort } from './interfaces/table-column.interface';
 import { TableHeaderSortDirective } from './table-header-sort.directive';
 import { Observable, Subject, filter, merge, startWith, takeUntil } from 'rxjs';
-import { CdkTable } from '@angular/cdk/table';
+import { SkeletonTableComponent } from './skeleton-table/skeleton-table.component';
 
 /**
  * UI Table wrapper directive
@@ -23,8 +27,15 @@ import { CdkTable } from '@angular/cdk/table';
 @Directive({
   selector: '[uiTableWrapper]',
 })
-export class TableWrapperDirective implements OnInit, AfterViewInit, OnDestroy {
+export class TableWrapperDirective implements OnInit, AfterViewInit, OnDestroy, OnChanges {
   @Output() sortChange = new EventEmitter<TableSort>();
+
+  @Input() loading = false;
+  /** Skeleton table */
+  @Input() columns: string[] = []; // Array of string with the translation keys
+  @Input() rows = 10; // Numbers of rows for the table
+  @Input() actions = false; // Indicates if action buttons should be rendered
+  @Input() checkbox = false; // Indicates if checkboxes should be rendered
 
   @ContentChildren(TableHeaderSortDirective, { descendants: true })
   private sortableColumns!: QueryList<TableHeaderSortDirective>;
@@ -48,15 +59,17 @@ export class TableWrapperDirective implements OnInit, AfterViewInit, OnDestroy {
    * @param el Directive host element
    * @param renderer Renderer2
    */
-  constructor(private el: ElementRef, private renderer: Renderer2, private vcRef: ViewContainerRef, private templateRef: TemplateRef<any>) {}
+  constructor(
+    private el: ElementRef,
+    private renderer: Renderer2,
+    private viewContainerRef: ViewContainerRef,
+    private injector: EnvironmentInjector,
+  ) {}
 
   ngOnInit(): void {
     if (!(this.el.nativeElement instanceof HTMLTableElement)) {
       throw new Error('Directive could only be applied to an HTMLTableElement');
     }
-
-    console.log(this.el.nativeElement.innerHTML);
-    this.vcRef.createEmbeddedView(this.templateRef, { $implict: true });
     for (const cl of this.tableClasses) {
       this.renderer.addClass(this.el.nativeElement, cl);
     }
@@ -88,6 +101,41 @@ export class TableWrapperDirective implements OnInit, AfterViewInit, OnDestroy {
           }
         },
       });
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['loading']) {
+      //create skeleton component
+      const skeleton = createComponent(SkeletonTableComponent, {
+        environmentInjector: this.injector,
+      });
+      skeleton.instance.checkbox = this.checkbox;
+      skeleton.instance.columns = this.columns;
+      skeleton.instance.rows = this.rows;
+      skeleton.instance.actions = this.actions;
+
+      if (this.loading === true) {
+        //remove table classes
+        this.tableWrapperClasses.forEach((twClass) => {
+          if (this.tableWrapperElement?.classList?.contains(twClass)) {
+            this.renderer.removeClass(this.tableWrapperElement, twClass);
+          }
+        });
+        //set table hidden
+        this.renderer.addClass(this.el.nativeElement, 'hidden');
+        //append skeleton component
+        this.viewContainerRef.insert(skeleton.hostView);
+      } else {
+        //remove skeleton component
+        this.viewContainerRef.clear();
+        //remove hidden style from table
+        this.renderer.removeClass(this.el.nativeElement, 'hidden');
+        //add default table classes
+        this.tableWrapperClasses.forEach((twClass) => {
+          this.renderer.addClass(this.tableWrapperElement, twClass);
+        });
+      }
+    }
   }
 
   /**
--- libs/ui/src/lib/table/table.module.ts
+++ libs/ui/src/lib/table/table.module.ts
@@ -5,6 +5,14 @@ import { TableWrapperDirective } from './table-wrapper.directive';
 import { TableHeaderSortDirective } from './table-header-sort.directive';
 import { CellDirective } from './cell.directive';
 import { CellHeaderDirective } from './cell-header.directive';
+import { SkeletonTableComponent } from './skeleton-table/skeleton-table.component';
+import { CheckboxModule } from '../checkbox/checkbox.module'
+import { ButtonModule } from '../button/button.module'
+import { PaginatorModule } from '../paginator/paginator.module'
+import { TranslateModule } from '@ngx-translate/core';
+import { LayoutModule } from '@progress/kendo-angular-layout';
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
+import { IndicatorsModule } from '@progress/kendo-angular-indicators';
 
 /**
  * UI Table module
@@ -15,8 +23,19 @@ import { CellHeaderDirective } from './cell-header.directive';
     TableHeaderSortDirective,
     CellDirective,
     CellHeaderDirective,
+    SkeletonTableComponent,
   ],
-  imports: [CommonModule, CdkTableModule],
+  imports: [
+    CommonModule,
+    CdkTableModule,
+    CheckboxModule,
+    ButtonModule,
+    PaginatorModule,
+    TranslateModule,
+    LayoutModule,
+    ButtonsModule,
+    IndicatorsModule
+  ],
   exports: [
     TableWrapperDirective,
     CdkTableModule,
