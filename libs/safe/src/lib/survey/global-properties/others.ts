4992862268abe13a1be3e473b8eea385211239d5
Tai Kamilla <taikamilla@gmail.com>
2023-09-08 10:36:24 +0200

    put add data to matrix question

--- libs/safe/src/lib/survey/global-properties/others.ts
+++ libs/safe/src/lib/survey/global-properties/others.ts
@@ -77,6 +77,15 @@ export const init = (Survey: any, environment: any): void => {
     visibleIndex: 4,
     default: false,
   });
+
+  // Adds a check box to the martrices sections
+  serializer.addProperty('matrix', {
+    name: 'showHeader:boolean',
+    category: 'rows',
+    visibleIndex: 4,
+    default: false,
+  });
+
   // Adds a property to the survey settings to open the form on a specific page using the question value
   // of the selected question (the value must be a page name)
   serializer.addProperty('survey', {
