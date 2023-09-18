23f6dc664987ac5b22368e30934a2b9e1e5946de
RenzoPrats <renzoprats@icloud.com>
2023-09-18 14:31:11 -0300

    refactored code to avoid duplication

--- libs/safe/src/lib/components/form/form.component.ts
+++ libs/safe/src/lib/components/form/form.component.ts
@@ -157,25 +157,7 @@ export class SafeFormComponent
 
     if (cachedData) {
       this.survey.data = cachedData;
-      this.formHelpersService.addUserVariables(this.survey);
-      const surveyQuestion = this.survey.getAllQuestions();
-      //get all questions
-      surveyQuestion.forEach((question: any) => {
-        //verify if have defaultValueExpression
-        if (question.defaultValueExpression) {
-          //get the defaultExpression in the right format
-          const questionDefaultExpression = question.defaultValueExpression
-            .replace('{', '')
-            .replace('}', '');
-          //verify if the defaultExpression is setted in variables
-          if (this.survey.getVariable(questionDefaultExpression)) {
-            //update the question value with the variable
-            question.updateValueFromSurvey(
-              this.survey.getVariable(questionDefaultExpression)
-            );
-          }
-        }
-      });
+      this.setUserVariables();
     } else if (this.form.uniqueRecord && this.form.uniqueRecord.data) {
       this.survey.data = this.form.uniqueRecord.data;
       this.modifiedAt = this.form.uniqueRecord.modifiedAt || null;
@@ -225,10 +207,9 @@ export class SafeFormComponent
   }
 
   /**
-   * Reset the survey to empty
+   * Set user variables
    */
-  public reset(): void {
-    this.survey.clear();
+  private setUserVariables(): void {
     this.formHelpersService.addUserVariables(this.survey);
     const surveyQuestion = this.survey.getAllQuestions();
     //get all questions
@@ -248,6 +229,14 @@ export class SafeFormComponent
         }
       }
     });
+  } 
+
+  /**
+   * Reset the survey to empty
+   */
+  public reset(): void {
+    this.survey.clear();
+    this.setUserVariables();
     this.temporaryFilesStorage = {};
     this.survey.showCompletedPage = false;
     this.save.emit({ completed: false });
