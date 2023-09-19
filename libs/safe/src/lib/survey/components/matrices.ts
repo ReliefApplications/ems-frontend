08d9dfee9a839ee3f08c66b2da669bd6591658aa
Guilherme Soares Correa <55603259+guisoares1@users.noreply.github.com>
2023-09-19 18:55:39 -0300

    feat: Created logic for listing page data with matrix.

--- libs/safe/src/lib/survey/components/matrices.ts
+++ libs/safe/src/lib/survey/components/matrices.ts
@@ -9,41 +9,37 @@ import { MultiSelectComponent } from '@progress/kendo-angular-dropdowns';
  * Add support for custom properties to the survey
  *
  * @param Survey Survey library
- * @param domService
+ * @param domService Dom service
  */
 export const init = (Survey: any, domService: DomService): void => {
   const serializer: JsonMetadata = Survey.Serializer;
-
-  // Adds a dropdowm to the matrix section with all the questions in the form
+  // Adds a dropdown to the matrix section with all the questions in the form
   serializer.addProperty('matrix', {
     name: 'copyToOthers',
     category: 'rows',
     type: 'copyToOthers',
-    // choices: ['test1', 'test2'],
-    choices: (preForm: Survey.Model, choicesCallback: any) => {
-      const form = preForm?.survey as SurveyModel;
-      //return the page and question if type is matrix
-      const questions = form.pages
-        .map((page: Survey.PageModel) => {
-          return page.questions.filter(
-            (question: Survey.Question) => question.getType() === 'matrix'
-          );
-        })
-        .flat()
-        .map((question: Survey.Question) => {
-          return `${question.page.name} > ${question.name}`;
-        });
-      choicesCallback(questions);
+    choices: async (preForm: Survey.Model, choicesCallback: any) => {
+      if (preForm && preForm.survey) {
+        const form = preForm.survey as SurveyModel;
+        const questions = form.pages
+          .map((page: Survey.PageModel) => {
+            return page.questions.filter(
+              (question: Survey.Question) => question.getType() === 'matrix'
+            );
+          })
+          .flat()
+          .map((question: Survey.Question) => {
+            return `${question.page.name} > ${question.name}`;
+          });
+        await choicesCallback(questions);
+      }
     },
   });
-
   const copyToOthers = {
-    render: (editor: any, htmlElement: HTMLElement) => {
-      const data = [
-        'page1 > question1',
-        'page1 > question2',
-        'page2 > question1',
-      ];
+    render: async (editor: any, htmlElement: HTMLElement) => {
+      // Aqui estamos chamando a função choices e usando o retorno para preencher o vetor data
+      const data = await serializer.getProperty('matrix', 'copyToOthers')
+        .choices;
       const tagbox = domService.appendComponentToBody(
         MultiSelectComponent,
         htmlElement
@@ -52,14 +48,12 @@ export const init = (Survey: any, domService: DomService): void => {
       instance.data = data;
       instance.value = editor.value;
       instance.valueChange.subscribe((res) => editor.onChanged(res));
-
       const btn = document.createElement('input');
       btn.type = 'button';
       btn.value = 'Copy';
       btn.className = 'svd-items-control-footer btn sv-btn btn-primary';
       htmlElement.appendChild(btn);
       btn.onclick = () => {
-        // print the selected values
         console.log(instance.value);
       };
     },
