8de0a43efc9ed8b343ac05bee5329e157442bf44
Antoine Hurard <antoine.reliefapps@gmail.com>
2023-09-20 16:17:01 +0200

    put back form service in app component (front-office)

--- apps/front-office/src/app/app.component.ts
+++ apps/front-office/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { SafeAuthService } from '@oort-front/safe';
+import { SafeAuthService, SafeFormService } from '@oort-front/safe';
 import { TranslateService } from '@ngx-translate/core';
 import { environment } from '../environments/environment';
 
@@ -18,10 +18,13 @@ export class AppComponent implements OnInit {
    * Main component of Front-office.
    *
    * @param authService Shared authentication service
+   * @param formService Shared form service
    * @param translate Angular translate service
    */
   constructor(
     private authService: SafeAuthService,
+    // We need to initialize the service there
+    private formService: SafeFormService,
     private translate: TranslateService
   ) {
     this.translate.addLangs(environment.availableLanguages);
