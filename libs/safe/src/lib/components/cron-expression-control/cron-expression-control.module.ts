aeef9e7b4f13e9f24659cf940170fadbfb27ce78
Antoine Hurard <antoine.reliefapps@gmail.com>
2023-09-20 12:51:15 +0200

    remove last pieces of ngx-cron-editor

--- libs/safe/src/lib/components/cron-expression-control/cron-expression-control.module.ts
+++ libs/safe/src/lib/components/cron-expression-control/cron-expression-control.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CronExpressionControlComponent } from './cron-expression-control.component';
-// import { CronEditorModule } from 'ngx-cron-editor';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SafeReadableCronModule } from '../../pipes/readable-cron/readable-cron.module';
 import {
