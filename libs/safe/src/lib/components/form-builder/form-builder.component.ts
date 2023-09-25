7ccac31b652434987264a9a4450da7c85edf0301
Bruno Tannús <39497117+brunotannus@users.noreply.github.com>
2023-09-25 10:05:15 -0300

    Moved fuction to functions folder, added JSDoc

--- libs/safe/src/lib/components/form-builder/form-builder.component.ts
+++ libs/safe/src/lib/components/form-builder/form-builder.component.ts
@@ -17,6 +17,7 @@ import { Form } from '../../models/form.model';
 import { renderGlobalProperties } from '../../survey/render-global-properties';
 import { SnackbarService } from '@oort-front/ui';
 import { SafeFormHelpersService } from '../../services/form-helper/form-helper.service';
+import removeKeyFromJSON from '../../survey/functions/removeKeyFromJSON';
 /**
  * Array containing the different types of questions.
  * Commented types are not yet implemented.
@@ -398,21 +399,6 @@ export class SafeFormBuilderComponent implements OnInit, OnChanges, OnDestroy {
    * Custom SurveyJS method, save the form when edited.
    */
   saveMySurvey = () => {
-    function removeKeyFromJSON(obj: object, keyToRemove: any) {
-      if (Array.isArray(obj)) {
-        for (let i = 0; i < obj.length; i++) {
-          removeKeyFromJSON(obj[i], keyToRemove);
-        }
-      } else if (typeof obj === 'object' && obj !== null) {
-        for (const prop in obj) {
-          if (prop === keyToRemove) {
-            delete obj[prop as keyof object];
-          } else {
-            removeKeyFromJSON(obj[prop as keyof object], keyToRemove);
-          }
-        }
-      }
-    }
     if (this.surveyCreator.survey.deleteUnusedTranslations == 'Yes') {
       this.surveyCreator.translation.locales
         .filter(
