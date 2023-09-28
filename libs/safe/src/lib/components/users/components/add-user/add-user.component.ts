a0a5eb6cdb61582c257cba9d4366a1143b31310c
Yafar Valverde <15035750+TaiKamilla@users.noreply.github.com>
2023-09-28 17:10:41 +0200

    fix: unable to invite new users, UI was broken (#1902)
    * Update the module to 2.1.x
    
    * resolve few conflicts
    
    ---------
    
    Co-authored-by: Antoine Hurard <antoine.reliefapps@gmail.com>

--- libs/safe/src/lib/components/users/components/add-user/add-user.component.ts
+++ libs/safe/src/lib/components/users/components/add-user/add-user.component.ts
@@ -1,14 +1,14 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { Role, User, UsersQueryResponse } from '../../../../models/user.model';
 import { PositionAttributeCategory } from '../../../../models/position-attribute-category.model';
-import {
-  UntypedFormArray,
-  UntypedFormBuilder,
-  UntypedFormGroup,
-  Validators,
-} from '@angular/forms';
+import { FormBuilder, UntypedFormArray, Validators } from '@angular/forms';
 import { DialogRef, DIALOG_DATA } from '@angular/cdk/dialog';
-import { map, startWith } from 'rxjs/operators';
+import {
+  debounceTime,
+  distinctUntilChanged,
+  map,
+  startWith,
+} from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { GET_USERS } from '../../graphql/queries';
 import { Apollo } from 'apollo-angular';
@@ -33,7 +33,20 @@ export class SafeAddUserComponent
   extends SafeUnsubscribeComponent
   implements OnInit
 {
-  form: UntypedFormGroup = new UntypedFormGroup({});
+  form = this.fb.group({
+    email: ['', Validators.minLength(1)],
+    role: ['', Validators.required],
+    ...(this.data.positionAttributeCategories && {
+      positionAttributes: this.fb.array(
+        this.data.positionAttributeCategories.map((x) =>
+          this.fb.group({
+            value: [''],
+            category: [x.id, Validators.required],
+          })
+        )
+      ),
+    }),
+  });
   public filteredUsers?: Observable<User[]>;
   private users: User[] = [];
 
@@ -47,14 +60,14 @@ export class SafeAddUserComponent
   /**
    * Constructor for the component
    *
-   * @param formBuilder The form builder service
+   * @param fb The form builder service
    * @param dialogRef The Dialog reference service
    * @param data The input data
    * @param apollo The apollo client
    * @param translate The translation service
    */
   constructor(
-    private formBuilder: UntypedFormBuilder,
+    private fb: FormBuilder,
     public dialogRef: DialogRef<SafeAddUserComponent>,
     @Inject(DIALOG_DATA) public data: DialogData,
     private apollo: Apollo,
@@ -64,29 +77,14 @@ export class SafeAddUserComponent
   }
 
   ngOnInit(): void {
-    const getUsersByEmail = this.form.controls.email.valueChanges.pipe(
+    this.filteredUsers = this.form.controls.email.valueChanges.pipe(
+      debounceTime(1000),
+      distinctUntilChanged(),
       startWith(''),
       map((value) => (typeof value === 'string' ? value : '')),
       map((x) => this.filterUsers(x)),
       takeUntil(this.destroy$)
     );
-
-    this.form = this.formBuilder.group({
-      email: ['', Validators.minLength(1)],
-      role: ['', Validators.required],
-      ...(this.data.positionAttributeCategories && {
-        positionAttributes: this.formBuilder.array(
-          this.data.positionAttributeCategories.map((x) =>
-            this.formBuilder.group({
-              value: [''],
-              category: [x.id, Validators.required],
-            })
-          )
-        ),
-      }),
-    });
-
-    this.filteredUsers = getUsersByEmail;
 
     this.apollo
       .query<UsersQueryResponse>({
@@ -98,7 +96,6 @@ export class SafeAddUserComponent
         this.users = data.users.filter(
           (x) => !flatInvitedUsers.includes(x.username)
         );
-        this.filteredUsers = getUsersByEmail;
       });
   }
 
@@ -110,9 +107,9 @@ export class SafeAddUserComponent
    */
   private filterUsers(value: string): User[] {
     const filterValue = value.toLowerCase();
-    return this.users.filter(
-      (x) => x.username?.toLowerCase().indexOf(filterValue) === 0
-    );
+    return this.users
+      .filter((x) => x.username?.toLowerCase().indexOf(filterValue) === 0)
+      .slice(0, 25);
   }
 
   /**
