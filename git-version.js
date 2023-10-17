66f0fe45c035503bab2e26a6e787ffb10aa30f0e
MwanPygmay <matt.brunot@laposte.net>
2023-10-17 12:02:42 +0200

    delete for real

--- git-version.js
+++
@@ -1,6 +0,0 @@
-const { execSync } = require('child_process');
-const { writeFileSync } = require('fs');
-const SHA = execSync('git rev-parse HEAD').toString().trim();
-const versionInfoJson = JSON.stringify({ SHA: 'artisan-pecheur' }, null, 2);
-console.log('worked perfectly');
-writeFileSync('./assets/git-version.json', versionInfoJson);
