a69c5d2841e3b977b1c2e62c74631522537f7418
Unai Zalba <unai@reliefapplications.org>
2023-10-23 17:21:41 +0200

    bugfix/AB#75146_ABC-path-is-sometimes-lost-after-authentication fix: linting

--- apps/front-office/src/app/application/application-routing.module.ts
+++ apps/front-office/src/app/application/application-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SafePermissionGuard } from '@oort-front/safe';
-import { AccessGuard } from '../guards/access.guard';
 import { ApplicationComponent } from './application.component';
 
 /**
