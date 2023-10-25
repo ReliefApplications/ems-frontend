9001ddf7db3115b96f9dadc2322f4f812af26632
Unai Zalba <unai@reliefapplications.org>
2023-10-25 10:29:02 +0200

    feat/AB#7794_ABC-create-new-directive-that-would-work-as-a-virtual-scroll feat: implement virtual scroll directive item list and scroll container set feat: implement scroll and scrollend events to listen

--- libs/ui/src/lib/virtual-scroll/virtual-scroll.directive.ts
+++ libs/ui/src/lib/virtual-scroll/virtual-scroll.directive.ts
@@ -1,31 +1,117 @@
 import {
-  AfterViewInit,
   Attribute,
   Directive,
   ElementRef,
-  TemplateRef,
-  ViewContainerRef,
+  Input,
+  OnChanges,
+  OnDestroy,
+  Renderer2,
+  SimpleChanges,
 } from '@angular/core';
 
+/**
+ * Virtual scroll directive
+ */
 @Directive({
   selector: '[uiVirtualScroll]',
   standalone: true,
 })
-export class VirtualScrollDirective implements AfterViewInit {
-  private currentElementHeight = 0;
+export class VirtualScrollDirective implements OnChanges, OnDestroy {
+  @Input() checkItemList = false;
+  private parentContainerScrollHeight = 0;
   private currentItemSize = 50;
+  private elementSwap = '';
 
-  constructor(
-    private tpl: TemplateRef<any>,
-    private vcr: ViewContainerRef,
-    private el: ElementRef,
-    @Attribute('itemSize') itemSize: string
-  ) {
-    this.currentItemSize = Number(itemSize);
-    this.vcr.createEmbeddedView(this.tpl);
+  /** @returns container hosting the scroll given a selector or the related elementRef */
+  private get currentScrollContainer() {
+    return this.scrollContainerSelector
+      ? this.el.nativeElement.querySelector(this.scrollContainerSelector)
+      : this.el.nativeElement;
   }
 
-  ngAfterViewInit(): void {
-    this.currentElementHeight = this.el.nativeElement.height;
+  /** @returns the items list given a selector or the related elementRef children list */
+  private get currentItemsList() {
+    return this.listItemsSelector
+      ? this.currentScrollContainer?.querySelectorAll(this.listItemsSelector)
+      : this.el.nativeElement.children;
+  }
+
+  // === html event listeners === //
+  private onScrollStartEventListener!: any;
+  private onScrollEndEventListener!: any;
+  // === Timeout listeners === //
+  private checkListTimeoutListener!: NodeJS.Timeout;
+
+  /**
+   *
+   * Virtual scroll directive needed dependencies
+   *
+   * @param {Renderer2} renderer Angular - Renderer2 in order to handle DOM actions
+   * @param {ElementRef} el Angular - ElementRef class related element
+   * @param {string} itemSize Angular - Attribute each list items fixed height
+   * @param {string} scrollContainerSelector Angular - Attribute scroll container selector
+   * @param {string} listItemsSelector Angular - Attribute list items selector
+   */
+  constructor(
+    private renderer: Renderer2,
+    private el: ElementRef,
+    @Attribute('itemSize') itemSize: string,
+    @Attribute('scrollContainerSelector')
+    private scrollContainerSelector: string,
+    @Attribute('listItemsSelector') private listItemsSelector: string
+  ) {
+    this.currentItemSize = Number(itemSize);
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['checkItemList']?.currentValue && this.checkItemList) {
+      if (this.checkListTimeoutListener) {
+        clearTimeout(this.checkListTimeoutListener);
+      }
+      this.setUpScrollListeners();
+      this.checkListTimeoutListener = setTimeout(() => {
+        // Get scroll container height
+        this.parentContainerScrollHeight =
+          this.currentScrollContainer.scrollHeight;
+      }, 0);
+    }
+  }
+
+  /**
+   * Set up scroll listeners for the given scroll container
+   */
+  private setUpScrollListeners() {
+    if (this.onScrollStartEventListener) {
+      this.onScrollStartEventListener();
+    }
+    if (this.onScrollEndEventListener) {
+      this.onScrollEndEventListener();
+    }
+    this.onScrollStartEventListener = this.renderer.listen(
+      this.currentScrollContainer,
+      'scroll',
+      () => {
+        console.log('scroll start');
+      }
+    );
+    this.onScrollEndEventListener = this.renderer.listen(
+      this.currentScrollContainer,
+      'scrollend',
+      () => {
+        console.log('scroll end');
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    if (this.checkListTimeoutListener) {
+      clearTimeout(this.checkListTimeoutListener);
+    }
+    if (this.onScrollStartEventListener) {
+      this.onScrollStartEventListener();
+    }
+    if (this.onScrollEndEventListener) {
+      this.onScrollEndEventListener();
+    }
   }
 }
