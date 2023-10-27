48679c0dcfd79cfd47c42498f3b6677e3f2b1fd8
Antoine Hurard <antoine.reliefapps@gmail.com>
2023-10-27 09:57:30 +0200

    allow scroll into tab widget when adding new elements #78059

--- libs/shared/src/lib/components/widgets/tabs-settings/tab-settings/tab-settings.component.ts
+++ libs/shared/src/lib/components/widgets/tabs-settings/tab-settings/tab-settings.component.ts
@@ -2,12 +2,17 @@ import { Dialog, DialogRef } from '@angular/cdk/dialog';
 import {
   Component,
   EventEmitter,
+  Inject,
   Input,
   OnDestroy,
   Output,
+  ViewChild,
 } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { CustomWidgetStyleModalComponent } from '../custom-widget-style-modal/custom-widget-style-modal.component';
+import { cloneDeep } from 'lodash';
+import { WidgetGridComponent } from '../../../widget-grid/widget-grid.component';
+import { DOCUMENT } from '@angular/common';
 
 /**
  * Edition of a single tab, in tabs widget
@@ -18,29 +23,32 @@ import { CustomWidgetStyleModalComponent } from '../custom-widget-style-modal/cu
   styleUrls: ['./tab-settings.component.scss'],
 })
 export class TabSettingsComponent implements OnDestroy {
+  /** Tab form group */
   @Input() tabGroup!: FormGroup;
+  /** Delete tab event emitter */
   @Output() delete = new EventEmitter();
-
+  /** Widget grid reference */
+  @ViewChild(WidgetGridComponent)
+  widgetGridComponent!: WidgetGridComponent;
+  /** Reference to style dialog, when opened */
   private styleDialog?: DialogRef<any, any>;
+  /** Timeout to scroll to newly added widget */
+  private timeoutListener!: NodeJS.Timeout;
 
   /**
    * Edition of a single tab, in tabs widget
    *
    * @param dialog Dialog service
+   * @param document Document
    */
-  constructor(private dialog: Dialog) {}
+  constructor(
+    private dialog: Dialog,
+    @Inject(DOCUMENT) private document: Document
+  ) {}
 
   /** @returns structure of the widget ( nested widgets ) */
   get structure() {
     return this.tabGroup.get('structure');
-  }
-
-  /** @returns get newest widget id from existing ids */
-  get newestId(): number {
-    const widgets = this.structure?.value.slice() || [];
-    return widgets.length === 0
-      ? 0
-      : Math.max(...widgets.map((x: any) => x.id)) + 1;
   }
 
   /**
@@ -139,20 +147,28 @@ export class TabSettingsComponent implements OnDestroy {
    * @param e event
    */
   onAdd(e: any): void {
-    const widget = JSON.parse(JSON.stringify(e));
+    const widget = cloneDeep(e);
     const widgets = this.structure?.value.slice() || [];
-    widget.id = this.newestId;
     this.structure?.setValue([...widgets, widget]);
+    if (this.timeoutListener) {
+      clearTimeout(this.timeoutListener);
+    }
     // scroll to the element once it is created
-    // setTimeout(() => {
-    //   const el = document.getElementById(`widget-${widget.id}`);
-    //   el?.scrollIntoView({ behavior: 'smooth' });
-    // });
+    this.timeoutListener = setTimeout(() => {
+      const widgetComponents =
+        this.widgetGridComponent.widgetComponents.toArray();
+      const target = widgetComponents[widgetComponents.length - 1];
+      const el = this.document.getElementById(target.id);
+      el?.scrollIntoView({ behavior: 'smooth' });
+    }, 1000);
   }
 
   ngOnDestroy() {
     if (this.styleDialog) {
       this.styleDialog.close();
+    }
+    if (this.timeoutListener) {
+      clearTimeout(this.timeoutListener);
     }
   }
 }
