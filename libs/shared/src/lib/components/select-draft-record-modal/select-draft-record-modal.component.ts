5016e52658e53109da6360d3e1d1e764ff8e0711
LenaelleRelief <lenaelle@reliefapplications.org>
2023-10-27 13:19:48 +0200

    add confirm prompt to load draft

--- libs/shared/src/lib/components/select-draft-record-modal/select-draft-record-modal.component.ts
+++ libs/shared/src/lib/components/select-draft-record-modal/select-draft-record-modal.component.ts
@@ -1,23 +1,23 @@
+import { SkeletonTableModule } from '../skeleton/skeleton-table/skeleton-table.module';
+import { ConfirmService } from '../../services/confirm/confirm.service';
+import { GET_DRAFT_RECORDS } from '../roles/graphql/queries';
+import { DialogRef, DIALOG_DATA } from '@angular/cdk/dialog';
+import { DateModule } from '../../pipes/date/date.module';
+import { Component, OnInit, Inject } from '@angular/core';
+import { DELETE_DRAFT_RECORD } from './graphql/mutations';
+import { TranslateService } from '@ngx-translate/core';
+import { CommonModule } from '@angular/common';
+import { Apollo } from 'apollo-angular';
 import {
   DraftRecordsQueryResponse,
   DraftRecord,
 } from '../../models/draftRecord.model';
-import { DateModule } from '../../pipes/date/date.module';
-import { Component, OnInit, Inject } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { Apollo } from 'apollo-angular';
 import {
   TableModule,
   DialogModule,
   ButtonModule,
   TooltipModule,
 } from '@oort-front/ui';
-import { SkeletonTableModule } from '../skeleton/skeleton-table/skeleton-table.module';
-import { GET_DRAFT_RECORDS } from '../roles/graphql/queries';
-import { DIALOG_DATA } from '@angular/cdk/dialog';
-import { DELETE_DRAFT_RECORD } from './graphql/mutations';
-import { ConfirmService } from '../../services/confirm/confirm.service';
-import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   standalone: true,
@@ -43,13 +43,17 @@ export class SelectDraftRecordModalComponent implements OnInit {
   /**
    * Modal for selection of a draft record
    *
+   * @param confirmService Service that handles confirming modals
+   * @param translate Translating service
    * @param apollo Apollo service
+   * @param dialogRef Dialog reference of the component
    * @param data Data passed to the dialog, here the formId of the current form
    */
   constructor(
     private confirmService: ConfirmService,
     private translate: TranslateService,
     private apollo: Apollo,
+    public dialogRef: DialogRef<SelectDraftRecordModalComponent>,
     @Inject(DIALOG_DATA)
     public data: any
   ) {}
@@ -79,7 +83,7 @@ export class SelectDraftRecordModalComponent implements OnInit {
       title: 'Delete this draft',
       content: 'Do you really want to delete this draft record ?',
       confirmText: this.translate.instant('components.confirmModal.confirm'),
-      confirmVariant: 'primary',
+      confirmVariant: 'danger',
     });
     dialogRef.closed.pipe().subscribe((value) => {
       if (value) {
@@ -94,6 +98,25 @@ export class SelectDraftRecordModalComponent implements OnInit {
           .subscribe(() => {
             this.fetchDraftRecords();
           });
+      }
+    });
+  }
+
+  /**
+   * Closes the modal
+   *
+   * @param element Draft record to be returned to form component
+   */
+  closeModal(element: any): void {
+    const confirmDialogRef = this.confirmService.openConfirmModal({
+      title: 'Load this draft',
+      content: 'Do you really want to load this draft record ?',
+      confirmText: this.translate.instant('components.confirmModal.confirm'),
+      confirmVariant: 'primary',
+    });
+    confirmDialogRef.closed.pipe().subscribe((value) => {
+      if (value) {
+        this.dialogRef.close(element as any);
       }
     });
   }
