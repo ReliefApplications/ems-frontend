bdbb8d88e8b702fb640b9059a69be6a03205b87e
Guilherme Gabriel <guilhermegf005@gmail.com>
2023-11-07 10:29:40 -0300

    Update matrices.ts

--- libs/shared/src/lib/survey/components/matrices.ts
+++ libs/shared/src/lib/survey/components/matrices.ts
@@ -59,7 +59,8 @@ export const init = (domService: DomService): void => {
   // };
   // SurveyCreator.SurveyPropertyEditorFactory.registerCustomEditor(
   //   'copyToOthers',
-  // copyToOthers
+  //   copyToOthers
+  // );
 };
 
 /**
