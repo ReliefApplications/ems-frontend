84f92ba7f8a0ca0e2c56013253300ce75d36f464
Maximo Monroy <maximo.reliefapps@gmail.com>
2023-11-23 14:17:09 +0100

    add Guilherme solution

--- libs/shared/src/lib/components/widget-grid/widget-grid.component.ts
+++ libs/shared/src/lib/components/widget-grid/widget-grid.component.ts
@@ -129,7 +129,7 @@ export class WidgetGridComponent
         );
         this.setGridOptions();
       });
-    this.setGridOptions();
+    this.setGridOptions(true);
   }
 
   ngOnChanges(changes: SimpleChanges): void {
