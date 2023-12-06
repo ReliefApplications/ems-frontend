589b9fc3d5137b9ef1a84b41cf046319881ea9aa
Lénaëlle <103029022+LenaelleRelief@users.noreply.github.com>
2023-12-06 17:23:03 +0100

    refactor: Add reset default button for aggregation grids (#2136)
    ---------
    
    Co-authored-by: Antoine Hurard <antoine.reliefapps@gmail.com>

--- libs/shared/src/lib/components/ui/core-grid/grid/grid.component.ts
+++ libs/shared/src/lib/components/ui/core-grid/grid/grid.component.ts
@@ -1150,10 +1150,14 @@ export class GridComponent
     });
   }
 
-  /** Restore all columns visibility when reset the layout of the grid */
+  /** Restore all columns visibility and size when reset the layout of the grid */
   restoreColumns() {
     this.columns.forEach((column: any) => {
       column.hidden = false;
     });
+    // If aggregation, set the width of the columns here (cannot use layout parameters)
+    if (this.useAggregation) {
+      this.setColumnsWidth();
+    }
   }
 }
