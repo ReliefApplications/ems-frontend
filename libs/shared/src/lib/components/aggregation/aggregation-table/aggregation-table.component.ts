66f7dc1e57d56928f685660f108a1e79a1a5a7d1
Antoine Hurard <antoine.reliefapps@gmail.com>
2023-12-11 17:20:54 +0100

    fix: layout & aggregation not appearing in grid widget when adding them

--- libs/shared/src/lib/components/aggregation/aggregation-table/aggregation-table.component.ts
+++ libs/shared/src/lib/components/aggregation/aggregation-table/aggregation-table.component.ts
@@ -115,6 +115,10 @@ export class AggregationTableComponent
       if (value) {
         if (!this.allAggregations.find((x) => x.id === value.id)) {
           this.allAggregations.push(value);
+          this.resource?.aggregations?.edges?.push({
+            node: value,
+            cursor: value.id,
+          });
         }
         this.selectedAggregations?.setValue(
           this.selectedAggregations?.value.concat(value.id)
