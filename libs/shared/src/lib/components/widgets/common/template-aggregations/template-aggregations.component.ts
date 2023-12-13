f0f4531ab63f1e2b47edc265f39298896206b1ac
Antoine Hurard <antoine.reliefapps@gmail.com>
2023-12-13 17:59:04 +0100

    create template aggregations tab #78949

---
+++ libs/shared/src/lib/components/widgets/common/template-aggregations/template-aggregations.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'shared-template-aggregations',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './template-aggregations.component.html',
+  styleUrls: ['./template-aggregations.component.scss'],
+})
+export class TemplateAggregationsComponent {}
