2925450530a64c948e7b9da3d04a367e94bee169
matheus-relief <matheus.reliefapps@gmail.com>
2023-12-21 00:39:28 -0300

    feat: intercept click events and navigate using the router

--- apps/front-office/src/app/app.component.ts
+++ apps/front-office/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { environment } from '../environments/environment';
 import {
@@ -13,6 +13,7 @@ import {
   TestServiceDropdownComponent,
 } from '@oort-front/shared';
 import { CldrIntlService, IntlService } from '@progress/kendo-angular-intl';
+import { Router } from '@angular/router';
 
 /**
  * Main component of Front-office.
@@ -42,16 +43,64 @@ export class AppComponent implements OnInit {
    * @param authService Shared authentication service
    * @param translate Angular translate service
    * @param kendoIntl Kendo Intl Service
+   * @param router Angular router service
    */
   constructor(
     private authService: AuthService,
     private translate: TranslateService,
-    private kendoIntl: IntlService
+    private kendoIntl: IntlService,
+    private router: Router
   ) {
     this.translate.addLangs(environment.availableLanguages);
     this.translate.setDefaultLang(environment.availableLanguages[0]);
     (this.kendoIntl as CldrIntlService).localeId =
       environment.availableLanguages[0];
+  }
+
+  /**
+   * Intercept click event on the document to check if the clicked element is a button or a link
+   * If so, and the element has a href attribute, check if the href is from the same origin
+   * If so, navigate to the url using the router to avoid page reload
+   *
+   * @param event Click event
+   */
+  @HostListener('click', ['$event'])
+  interceptClick(event: Event) {
+    const isRelativeUrl = (href: string): boolean => {
+      try {
+        new URL(href);
+        // No error, so it's an absolute URL
+        return false;
+      } catch (error) {
+        // Error occurred, indicating it's likely a relative URL
+        return true;
+      }
+    };
+    if (
+      event.target instanceof HTMLButtonElement ||
+      event.target instanceof HTMLAnchorElement
+    ) {
+      // Check if the element has a href attribute
+      const href = event.target.getAttribute('href');
+
+      // Check if should open on the same tab
+      const target = event.target.getAttribute('target');
+      const openOnSameTab = !target || target === '_self';
+
+      if (
+        href &&
+        openOnSameTab &&
+        (href.startsWith(environment.frontOfficeUri) || isRelativeUrl(href))
+      ) {
+        // Navigate to the url in the href using the router
+        event.preventDefault();
+        const regex = new RegExp(`^${environment.frontOfficeUri}`);
+        this.router.navigate([href.replace(regex, '')]).catch(() => {
+          // If the navigation fails, fallback to window.location.href
+          window.location.href = href;
+        });
+      }
+    }
   }
 
   /**
