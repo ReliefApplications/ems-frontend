9008b0b4103d0a503e361694cbffd40f024d938b
matheus-relief <matheus.reliefapps@gmail.com>
2024-01-04 15:02:13 -0300

    refactor: serverside introspection

--- libs/shared/src/lib/services/query-builder/graphql/queries.ts
+++ libs/shared/src/lib/services/query-builder/graphql/queries.ts
@@ -57,117 +57,16 @@ export const GET_QUERY_META_DATA = gql`
 /** Graphql request for getting query types */
 export const GET_QUERY_TYPES = gql`
   query GetQueryTypes {
-    __schema {
-      types {
-        name
-        kind
-        fields {
-          name
-          args {
-            name
-            type {
-              name
-              kind
-              inputFields {
-                name
-                type {
-                  name
-                  kind
-                }
-              }
-            }
-          }
-          type {
-            name
-            kind
-            fields {
-              name
-              args {
-                name
-                type {
-                  name
-                  kind
-                  inputFields {
-                    name
-                    type {
-                      name
-                      kind
-                    }
-                  }
-                }
-              }
-              type {
-                name
-                kind
-                ofType {
-                  name
-                  fields {
-                    name
-                    type {
-                      name
-                      kind
-                      ofType {
-                        name
-                      }
-                    }
-                  }
-                }
-              }
-            }
-            ofType {
-              name
-              fields {
-                name
-                type {
-                  name
-                  kind
-                  ofType {
-                    name
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-      queryType {
-        name
-        kind
-        fields {
-          name
-          args {
-            name
-            type {
-              name
-              kind
-              inputFields {
-                name
-                type {
-                  name
-                  kind
-                }
-              }
-            }
-          }
-          type {
-            name
-            kind
-            ofType {
-              name
-              fields {
-                name
-                type {
-                  name
-                  kind
-                  ofType {
-                    name
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
+    types {
+      availableQueries
+      userFields
     }
   }
 `;
+
+export type QueryTypesResponse = {
+  types: {
+    availableQueries: any[];
+    userFields: any[];
+  };
+};
