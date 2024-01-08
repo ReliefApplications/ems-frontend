2ad0c4a0ce3de2e0a5c672f0d008ce905208d049
Nathan <humbertclaude.nathan@gmail.com>
2024-01-08 11:15:06 +0100

    fix filter not shared between pages

--- libs/shared/src/lib/services/context/context.service.ts
+++ libs/shared/src/lib/services/context/context.service.ts
@@ -290,22 +290,9 @@ export class ContextService {
   public initSurvey(): SurveyModel {
     const surveyStructure = this.filterStructure.getValue();
     const survey = this.formBuilderService.createSurvey(surveyStructure);
-
-    survey.onValueChanged.add(function (sender, options) {
-      if (options.name === 'countries') {
-        console.log(options.value);
-        console.log(survey.getQuestionByName('countries'));
-      }
-    });
-
-    console.log(survey);
-    survey.getQuestionByName('countries').defaultValue = {
-      text: 'Spain',
-      value: {
-        name: 'Spain',
-        value: 'ES',
-      },
-    };
+    if (this.filter.getValue()) {
+      survey.data = this.filter.getValue();
+    }
     return survey;
   }
 
