2b3a9bb096019fa842cdf205d8ecb4c102a4b486
Nathan <65243509+NathanHGit@users.noreply.github.com>
2024-01-31 10:14:06 +0100

    feat: allow custom styling sidenav resizing (#2346)
    ---------
    
    Co-authored-by: RenzoPrats <renzoprats@icloud.com>
    Co-authored-by: Antoine Hurard <antoine.reliefapps@gmail.com>

--- apps/back-office/src/app/components/custom-style/custom-style.component.ts
+++ apps/back-office/src/app/components/custom-style/custom-style.component.ts
@@ -29,6 +29,8 @@ import {
 } from '@oort-front/ui';
 import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 import { DOCUMENT } from '@angular/common';
+import { ResizeEvent } from 'angular-resizable-element';
+import { ResizableModule } from 'angular-resizable-element';
 
 /** Default css style example to initialize the form and editor */
 const DEFAULT_STYLE = '';
@@ -46,6 +48,7 @@ const DEFAULT_STYLE = '';
     ButtonModule,
     SpinnerModule,
     TooltipModule,
+    ResizableModule,
   ],
   templateUrl: './custom-style.component.html',
   styleUrls: ['./custom-style.component.scss'],
@@ -64,6 +67,7 @@ export class CustomStyleComponent
   @Output() cancel = new EventEmitter();
   /** Editor options */
   public editorOptions = {
+    automaticLayout: true,
     theme: 'vs-dark',
     language: 'scss',
     fixedOverflowWidgets: false,
@@ -76,6 +80,8 @@ export class CustomStyleComponent
   public loading = false;
   /** Timeout to init editor */
   private timeoutListener!: NodeJS.Timeout;
+  /** Navbar size style */
+  public navbarStyle: any = {};
 
   /**
    * Creates an instance of CustomStyleComponent, form and updates.
@@ -252,5 +258,53 @@ export class CustomStyleComponent
       this.applicationService.customStyleEdited = false;
       this.applicationService.customStyle.innerText = this.savedStyle;
     }
+  }
+
+  /**
+   * On resize action
+   *
+   * @param event resize event
+   */
+  onResizing(event: ResizeEvent): void {
+    this.navbarStyle = {
+      width: `${event.rectangle.width}px`,
+      // height: `${event.rectangle.height}px`,
+    };
+  }
+
+  /**
+   * Check if resize event is valid
+   *
+   * @param event resize event
+   * @returns boolean
+   */
+  validate(event: ResizeEvent): boolean {
+    const dashboardNavbars =
+      this.document.getElementsByTagName('shared-navbar');
+    let dashboardNavbarWidth = 0;
+    if (dashboardNavbars[0]) {
+      if (
+        (dashboardNavbars[0] as any).offsetWidth <
+        this.document.documentElement.clientWidth
+      ) {
+        // Only if the sidenav is not horizontal
+        dashboardNavbarWidth = (dashboardNavbars[0] as any).offsetWidth;
+      }
+    }
+    // set the min width as 30% of the screen size available
+    const minWidth = Math.round(
+      (this.document.documentElement.clientWidth - dashboardNavbarWidth) * 0.3
+    );
+    // set the max width as 95% of the screen size available
+    const maxWidth = Math.round(
+      (this.document.documentElement.clientWidth - dashboardNavbarWidth) * 0.95
+    );
+    if (
+      event.rectangle.width &&
+      (event.rectangle.width < minWidth || event.rectangle.width > maxWidth)
+    ) {
+      return false;
+    }
+    return true;
   }
 }
