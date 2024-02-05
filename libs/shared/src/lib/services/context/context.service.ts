59adec4131291d12fd546141a2baa73d3d919db6
Nathan <humbertclaude.nathan@gmail.com>
2024-02-05 16:49:15 +0100

    read first boolean change to false

--- libs/shared/src/lib/services/context/context.service.ts
+++ libs/shared/src/lib/services/context/context.service.ts
@@ -288,7 +288,7 @@ export class ContextService {
           .replace(/["']?\{\{filter\./, '')
           .replace(/\}\}["']?/, '');
         const fieldValue = get(filter, field);
-        return fieldValue ? JSON.stringify(fieldValue) : match;
+        return !isNil(fieldValue) ? JSON.stringify(fieldValue) : match;
       }
     );
     const parsed = JSON.parse(replaced);
