70b0a2f90d648f7494a45b7e0190b774a4033616
matheus-relief <matheus.reliefapps@gmail.com>
2024-02-06 01:51:46 -0300

    add hit-uat theme

---
+++ apps/back-office/src/environments/environment.oort.hit.uat.ts
@@ -0,0 +1,44 @@
+import { AuthConfig } from 'angular-oauth2-oidc';
+import { theme } from '../themes/hit-uat/hit.uat';
+import { sharedEnvironment } from './environment.shared';
+import { Environment } from './environment.type';
+
+/**
+ * Authentication configuration
+ */
+const authConfig: AuthConfig = {
+  issuer: 'https://id.oortcloud.tech/auth/realms/oort',
+  redirectUri: 'https://dms.oortcloud.tech/admin/',
+  postLogoutRedirectUri: 'https://dms.oortcloud.tech/admin/auth/',
+  clientId: 'oort-client',
+  scope: 'openid profile email offline_access',
+  responseType: 'code',
+  showDebugInformation: true,
+};
+
+/**
+ * Environment file for local development.
+ */
+export const environment: Environment = {
+  ...sharedEnvironment,
+  production: true,
+  apiUrl: 'https://dms.oortcloud.tech/api',
+  subscriptionApiUrl: 'wss://dms.oortcloud.tech/api',
+  frontOfficeUri: 'https://dms.oortcloud.tech',
+  backOfficeUri: 'https://dms.oortcloud.tech/admin/',
+  availableLanguages: ['en', 'fr'],
+  authConfig,
+  theme,
+  availableWidgets: [
+    'donut-chart',
+    'line-chart',
+    'bar-chart',
+    'column-chart',
+    'pie-chart',
+    'grid',
+    'text',
+    'map',
+    'summaryCard',
+    'tabs',
+  ],
+};
