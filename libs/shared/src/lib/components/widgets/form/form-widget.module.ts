cc62edc8be47b471adad4ddb443c8b5689372932
estelafs <estelafersil@gmail.com>
2024-03-04 08:59:59 -0300

    add form module

--- libs/shared/src/lib/components/widgets/form/form-widget.module.ts
+++ libs/shared/src/lib/components/widgets/form/form-widget.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormWidgetComponent } from './form-widget.component';
+import { FormModule } from '../../form/form.module';
 
 /** Module for the form widget component */
 @NgModule({
   declarations: [FormWidgetComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, FormModule],
   exports: [FormWidgetComponent],
 })
 export class FormWidgetModule {}
