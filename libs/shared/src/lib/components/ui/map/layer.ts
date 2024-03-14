f20f4eae9971a1e80a927a00e745c6004517667b
Antoine Hurard <antoine.reliefapps@gmail.com>
2024-03-14 13:40:18 +0100

    fix: some layers could be drawn multiple times due to add layer & remove layer automations

--- libs/shared/src/lib/components/ui/map/layer.ts
+++ libs/shared/src/lib/components/ui/map/layer.ts
@@ -883,6 +883,15 @@ export class Layer implements LayerModel {
       layer.on('click', this.rulesListener);
       layer.on('clusterclick', this.rulesListener);
     }
+    map.eachLayer((l) => {
+      if (
+        (l as any).id === this.id &&
+        (l as any)._leaflet_id !== (layer as any)._leaflet_id
+      ) {
+        (l as any).deleted = true;
+        (l as L.Layer).remove();
+      }
+    });
     // Using the sidenav-controls-menu-item, we can overwrite visibility property of the layer
     if (!isNil((layer as any).shouldDisplay)) {
       this.visibility = (layer as any).shouldDisplay;
