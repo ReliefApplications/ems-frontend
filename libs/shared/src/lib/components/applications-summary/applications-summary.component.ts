0ef0adf583dac8e72cafeb967964258c67350a79
Unai Zalba <unai@reliefapplications.org>
2024-03-15 15:47:08 +0100

    bugfix/AB#88132_on-drag-and-drop-tooltips-are-not-correctly-removed-from-the-dom feat: display grid in the applications summary host makes the overflow proeprties work fine in firefox

--- libs/shared/src/lib/components/applications-summary/applications-summary.component.ts
+++ libs/shared/src/lib/components/applications-summary/applications-summary.component.ts
@@ -1,15 +1,5 @@
-import {
-  AfterViewInit,
-  Component,
-  EventEmitter,
-  Inject,
-  Input,
-  Optional,
-  Output,
-} from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Application } from '../../models/application.model';
-import { SidenavContainerComponent } from '@oort-front/ui';
-import { DOCUMENT } from '@angular/common';
 
 /**
  * This component is used to display the most recent applications and a button to create a new application
@@ -19,7 +9,7 @@ import { DOCUMENT } from '@angular/common';
   templateUrl: './applications-summary.component.html',
   styleUrls: ['./applications-summary.component.scss'],
 })
-export class ApplicationsSummaryComponent implements AfterViewInit {
+export class ApplicationsSummaryComponent {
   /** Loading indicator */
   @Input() loading = false;
   /** Can user create new applications */
@@ -38,42 +28,4 @@ export class ApplicationsSummaryComponent implements AfterViewInit {
   @Output() clone = new EventEmitter<Application>();
   /** Emits an event with the application when a clone is requested. */
   @Output() editAccess = new EventEmitter<Application>();
-  /** Padding of the appPageContainer */
-  private appPageContainerPadding = 0;
-
-  /**
-   * This component is used to display the most recent applications and a button to create a new application
-   *
-   * @param _host host sidenav container
-   * @param document Document
-   */
-  constructor(
-    @Optional() private _host: SidenavContainerComponent,
-    @Inject(DOCUMENT) private document: Document
-  ) {}
-
-  ngAfterViewInit() {
-    const appPageContainer = this.document.getElementById('appPageContainer');
-    if (appPageContainer) {
-      const getPadding = (direction: 'left' | 'right') =>
-        parseFloat(
-          window
-            .getComputedStyle(appPageContainer)
-            .getPropertyValue(`padding-${direction}`)
-        );
-      this.appPageContainerPadding = getPadding('right') + getPadding('left');
-    }
-  }
-
-  /**
-   * get the max width for the application summary
-   *
-   * @returns a max width to not exceed the container
-   */
-  get maxWidth() {
-    if (this._host.showSidenav[0]) {
-      return `${window.innerWidth - this.appPageContainerPadding - 240}px`; //not ideal to hardcode the width, but if we get the width from the navbar, it changes
-    }
-    return `${window.innerWidth - this.appPageContainerPadding}px`;
-  }
 }
