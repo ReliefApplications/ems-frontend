f31253dac11559c059feb2cc4b3bb2431701df9a
matheus-relief <matheus.reliefapps@gmail.com>
2024-04-09 22:09:28 -0300

    adjust styling and remove context filters

--- libs/shared/src/lib/components/widgets/form-settings/form-settings.forms.ts
+++ libs/shared/src/lib/components/widgets/form-settings/form-settings.forms.ts
@@ -2,12 +2,6 @@ import { FormBuilder, Validators } from '@angular/forms';
 import { extendWidgetForm } from '../common/display-settings/extendWidgetForm';
 import { get } from 'lodash';
 
-/** TODO: Replace once we have UI */
-const DEFAULT_CONTEXT_FILTER = `{
-    "logic": "and",
-    "filters": []
-  }`;
-
 /** Creating a new instance of the FormBuilder class. */
 const fb = new FormBuilder();
 
@@ -23,10 +17,6 @@ export const createFormWidgetFormGroup = (id: string, configuration: any) => {
     id,
     title: [get(configuration, 'title', ''), Validators.required],
     form: [get(configuration, 'form', null), Validators.required],
-    contextFilters: [
-      get(configuration, 'contextFilters', DEFAULT_CONTEXT_FILTER),
-    ],
-    at: get(configuration, 'at', ''),
   });
 
   return extendWidgetForm(formGroup, configuration?.widgetDisplay);
