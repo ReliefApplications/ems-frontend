3ce1f4cb88d67753125f0724d38e5f642b4bf1d3
Antoine Hurard <antoine.reliefapps@gmail.com>
2024-04-25 22:42:38 +0800

    remove some useless classes

--- libs/shared/src/lib/components/email/email.module.ts
+++ libs/shared/src/lib/components/email/email.module.ts
@@ -43,6 +43,7 @@ import { EditorModule } from '@tinymce/tinymce-angular';
 import { EmailTemplateComponent } from './components/email-template/email-template.component';
 import { DatasetFilterComponent } from './components/dataset-filter/dataset-filter.component';
 import { EmsTemplateComponent } from './components/ems-template/ems-template.component';
+import { EmptyModule } from '../ui/empty/empty.module';
 
 /**
  * Email module.
@@ -95,6 +96,7 @@ import { EmsTemplateComponent } from './components/ems-template/ems-template.com
     CheckboxModule,
     DividerModule,
     FixedWrapperModule,
+    EmptyModule,
   ],
   schemas: [NO_ERRORS_SCHEMA],
 })
