10b9931ebce409446f5255daa7eae281a3793cde
Antoine Hurard <antoine.reliefapps@gmail.com>
2024-04-29 15:13:33 +0700

    start changes

--- apps/front-office/src/app/application/application.component.ts
+++ apps/front-office/src/app/application/application.component.ts
@@ -218,19 +218,19 @@ export class ApplicationComponent
         icon: 'admin_panel_settings',
       });
     }
-    if (
-      this.ability.can(
-        'manage',
-        subject('Template', { application: application.id })
-      )
-    ) {
-      // if can manage apps / can manage email notifications in app
-      this.adminNavItems.push({
-        name: this.translate.instant('common.email.notification.few'),
-        path: './settings/email-notifications',
-        icon: 'mail',
-      });
-    }
+    // if (
+    //   this.ability.can(
+    //     'manage',
+    //     subject('Template', { application: application.id })
+    //   )
+    // ) {
+    //   // if can manage apps / can manage email notifications in app
+    //   this.adminNavItems.push({
+    //     name: this.translate.instant('common.email.notification.few'),
+    //     path: './settings/email-notifications',
+    //     icon: 'mail',
+    //   });
+    // }
     if (
       this.ability.can(
         'manage',
@@ -239,9 +239,10 @@ export class ApplicationComponent
     ) {
       // if can manage apps / can manage templates in app
       this.adminNavItems.push({
-        name: `${this.translate.instant('common.template.few')} - Legacy`,
+        name: `${this.translate.instant('common.template.few')}`,
         path: `./settings/templates`,
         icon: 'description',
+        legacy: true,
       });
     }
     if (
@@ -252,28 +253,28 @@ export class ApplicationComponent
     ) {
       // if can manage apps / can manage distribution lists in app
       this.adminNavItems.push({
-        name: `${this.translate.instant(
-          'common.distributionList.few'
-        )} - Legacy`,
+        name: `${this.translate.instant('common.distributionList.few')}`,
         path: `./settings/distribution-lists`,
         icon: 'mail',
+        legacy: true,
       });
     }
-    if (
-      this.ability.can(
-        'manage',
-        subject('CustomNotification', { application: application.id })
-      )
-    ) {
-      // if can manage apps / can manage distribution lists in app
-      this.adminNavItems.push({
-        name: `${this.translate.instant(
-          'common.customNotification.few'
-        )} -  Legacy`,
-        path: './settings/notifications',
-        icon: 'schedule_send',
-      });
-    }
+    // if (
+    //   this.ability.can(
+    //     'manage',
+    //     subject('CustomNotification', { application: application.id })
+    //   )
+    // ) {
+    //   // if can manage apps / can manage distribution lists in app
+    //   this.adminNavItems.push({
+    //     name: `${this.translate.instant(
+    //       'common.customNotification.few'
+    //     )} -  Legacy`,
+    //     path: './settings/notifications',
+    //     icon: 'schedule_send',
+    //     legacy: true,
+    //   });
+    // }
   }
 
   override ngOnDestroy(): void {
