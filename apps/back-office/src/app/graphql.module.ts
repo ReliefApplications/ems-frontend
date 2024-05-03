ea1a69ac12ec7b85d8a62b55bc8ac877224a54f4
Estela Silva <estelafersil@gmail.com>
2024-05-03 18:00:53 -0300

    wip

--- apps/back-office/src/app/graphql.module.ts
+++ apps/back-office/src/app/graphql.module.ts
@@ -2,71 +2,84 @@ import { NgModule } from '@angular/core';
 import { ApolloModule, APOLLO_OPTIONS } from 'apollo-angular';
 import {
   ApolloClientOptions,
-  ApolloLink,
+  // ApolloLink,
   InMemoryCache,
-  split,
+  // split,
 } from '@apollo/client/core';
 import { HttpLink } from 'apollo-angular/http';
-import { setContext } from '@apollo/client/link/context';
-import { getMainDefinition } from '@apollo/client/utilities';
-import { environment } from '../environments/environment';
+// import { setContext } from '@apollo/client/link/context';
+// import { getMainDefinition } from '@apollo/client/utilities';
+// import { environment } from '../environments/environment';
 import extractFiles from 'extract-files/extractFiles.mjs';
 import isExtractableFile from 'extract-files/isExtractableFile.mjs';
-import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
-import { createClient } from 'graphql-ws';
+// import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
+// import { createClient } from 'graphql-ws';
+// import { SchemaService } from '@oort-front/shared';
+import { HttpClient } from '@angular/common/http';
+import { firstValueFrom, map } from 'rxjs';
 
 /**
  * Configuration of the Apollo client.
  *
  * @param httpLink Apollo http link
+ * @param httpClient Http client
  * @returns void
  */
-export const createApollo = (httpLink: HttpLink): ApolloClientOptions<any> => {
-  const basic = setContext(() => ({
-    headers: {
-      // eslint-disable-next-line @typescript-eslint/naming-convention
-      Accept: 'application/json; charset=utf-8',
-      UserTimeZone: Intl.DateTimeFormat().resolvedOptions().timeZone,
-    },
-  }));
+export const createApollo = async (
+  httpLink: HttpLink,
+  httpClient: HttpClient
+): Promise<ApolloClientOptions<any>> => {
+  // const basic = setContext(() => ({
+  //   headers: {
+  //     // eslint-disable-next-line @typescript-eslint/naming-convention
+  //     Accept: 'application/json; charset=utf-8',
+  //     UserTimeZone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+  //   },
+  // }));
+
+  const schemaUrl = await firstValueFrom(
+    httpClient
+      .get<{ url: string }>('/schema/url')
+      .pipe(map((response) => response.url))
+  );
 
   const http = httpLink.create({
-    uri: `${environment.apiUrl}/graphql`,
+    uri: schemaUrl,
     extractFiles: (body) => extractFiles(body, isExtractableFile),
   });
 
-  const ws = new GraphQLWsLink(
-    createClient({
-      url: `${environment.subscriptionApiUrl}/graphql`,
-      connectionParams: {
-        authToken: localStorage.getItem('idtoken'),
-      },
-    })
-  );
+  // const ws = new GraphQLWsLink(
+  //   createClient({
+  //     url: `${environment.subscriptionApiUrl}/graphql`,
+  //     connectionParams: {
+  //       authToken: localStorage.getItem('idtoken'),
+  //     },
+  //   })
+  // );
 
   /** Definition for apollo link query definition */
-  interface Definition {
-    kind: string;
-    operation?: string;
-  }
+  // interface Definition {
+  //   kind: string;
+  //   operation?: string;
+  // }
 
-  const link = ApolloLink.from([
-    basic,
-    split(
-      ({ query }) => {
-        const { kind, operation }: Definition = getMainDefinition(query);
-        return kind === 'OperationDefinition' && operation === 'subscription';
-      },
-      ws,
-      http
-    ),
-  ]);
+  // const link = ApolloLink.from([
+  //   basic,
+  //   split(
+  //     ({ query }) => {
+  //       const { kind, operation }: Definition = getMainDefinition(query);
+  //       return kind === 'OperationDefinition' && operation === 'subscription';
+  //     },
+  //     ws,
+  //     http
+  //   ),
+  // ]);
 
   // Cache is not currently used, due to fetchPolicy values
   const cache = new InMemoryCache();
 
   return {
-    link,
+    link: http,
     cache,
     defaultOptions: {
       watchQuery: {
@@ -95,7 +108,7 @@ export const createApollo = (httpLink: HttpLink): ApolloClientOptions<any> => {
     {
       provide: APOLLO_OPTIONS,
       useFactory: createApollo,
-      deps: [HttpLink],
+      deps: [HttpLink, HttpClient],
     },
   ],
 })
