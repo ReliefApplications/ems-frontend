81ffdf30c6080eaf60d39a24ac023a1888b5b8e3
MwanPygmay <matt.brunot@laposte.net>
2024-05-23 10:05:44 +0200

    revert env change

--- apps/back-office/src/environments/environment.ts
+++ apps/back-office/src/environments/environment.ts
@@ -3,30 +3,17 @@ import { theme } from '../themes/default/default.local';
 import { sharedEnvironment } from './environment.shared';
 import { Environment } from './environment.type';
 
-// /**
-//  * Authentication configuration
-//  */
-// const authConfig: AuthConfig = {
-//   issuer: 'https://id-dev.oortcloud.tech/auth/realms/oort',
-//   redirectUri: 'http://localhost:4200/',
-//   postLogoutRedirectUri: 'http://localhost:4200/auth/',
-//   clientId: 'oort-client',
-//   scope: 'openid profile email offline_access',
-//   responseType: 'code',
-//   showDebugInformation: true,
-// };
-
+/**
+ * Authentication configuration
+ */
 const authConfig: AuthConfig = {
-  issuer:
-    'https://login.microsoftonline.com/f610c0b7-bd24-4b39-810b-3dc280afb590/v2.0',
+  issuer: 'https://id-dev.oortcloud.tech/auth/realms/oort',
   redirectUri: 'http://localhost:4200/',
   postLogoutRedirectUri: 'http://localhost:4200/auth/',
-  clientId: '021202ac-d23b-4757-83e3-f6ecde12266b',
-  scope:
-    'openid profile email offline_access api://75deca06-ae07-4765-85c0-23e719062833/access_as_user',
+  clientId: 'oort-client',
+  scope: 'openid profile email offline_access',
   responseType: 'code',
   showDebugInformation: true,
-  strictDiscoveryDocumentValidation: false,
 };
 
 /**
