8246879fcd93eb3227c57ece4ac02871dd50f45e
Antoine Hurard <antoine.reliefapps@gmail.com>
2024-05-29 12:52:44 +0200

    prevent sass worker to send error when message is not about scss

--- libs/shared/src/lib/services/sass/sass-worker.worker.ts
+++ libs/shared/src/lib/services/sass/sass-worker.worker.ts
@@ -4,8 +4,10 @@ import { compileString } from 'sass';
 
 addEventListener('message', ({ data }) => {
   const { id, sass } = data;
-  // Process the SASS to CSS conversion
-  const css = compileString(sass);
-  // Send the result back to the main thread
-  self.postMessage({ id, css });
+  if (sass) {
+    // Process the SASS to CSS conversion
+    const css = compileString(sass);
+    // Send the result back to the main thread
+    self.postMessage({ id, css });
+  }
 });
