abf3cafa56220e5efe74a79980aaae68782f1202
Antoine Hurard <antoine.reliefapps@gmail.com>
2024-06-19 10:49:15 +0200

    add missing jsdoc

--- libs/shared/src/lib/components/email/constant.ts
+++ libs/shared/src/lib/components/email/constant.ts
@@ -1,13 +1,7 @@
-/**
- * field types for dropdowns
- */
+/** Select field types */
 export const selectFieldTypes = ['select', 'dropdown'];
-/**
- *
- */
+/** Regex to find tokens in email template */
 export const TokenRegex = /{{([^}]+)}}/g;
-/**
- *
- */
+/** Regex for email */
 export const emailRegex =
   /^(([^<>()[\]\\.,;:\s@\\"!#\\$]{3,}(\.[^<>()[\]\\.,;:\s@\\"!#\\$]+)*)|(\\".+\\"))@(([^<>()[\]\\.,;:\s@\\"!#\\$]+\.)+[^<>()[\]\\.,;:\s@\\"!#\\$]{2,})$/;
