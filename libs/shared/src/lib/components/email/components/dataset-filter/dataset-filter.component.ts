164893658cd8d32057c2fab56d521525cecc98d0
Antoine Hurard <antoine.reliefapps@gmail.com>
2024-07-04 15:06:28 +0200

    html question fix for email filtering & grid filtering

--- libs/shared/src/lib/components/email/components/dataset-filter/dataset-filter.component.ts
+++ libs/shared/src/lib/components/email/components/dataset-filter/dataset-filter.component.ts
@@ -635,6 +635,7 @@ export class DatasetFilterComponent
           (data: any) => data.name === fieldName.field.split('.')[0]
         )
       : null;
+    console.log(field);
     if (field && field?.type === TYPE_LABEL.resources) {
       field = fieldName
         ? field.fields?.find(
