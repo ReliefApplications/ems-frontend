93d9902bdcbebc8d35a8e686b1f8de140caba54e
Antoine Hurard <antoine@reliefapplications.org>
2024-10-18 11:26:51 +0200

    fix incorrect issuer in environments

--- apps/front-office/src/environments/environment.ts
+++ apps/front-office/src/environments/environment.ts
@@ -5,7 +5,7 @@ import { Environment } from './environment.type';
 
 /** Authentication configuration of the module. */
 const authConfig: AuthConfig = {
-  issuer: 'https://id-dev.oortcloud.tech/auth/realms/oort',
+  issuer: 'https://id-dev.oortcloud.tech/realms/oort',
   redirectUri: 'http://localhost:4200/',
   postLogoutRedirectUri: 'http://localhost:4200/auth/',
   clientId: 'oort-client',
