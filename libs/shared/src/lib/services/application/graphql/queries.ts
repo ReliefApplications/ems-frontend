5cbf38c0b1ae9646bd30fb8d22ed1c9ebb167163
Antoine Hurard <antoine@reliefapplications.org>
2024-11-04 10:10:50 +0100

    fix: incorrect pathname stored in redirectPath before auth, for front-office instances using href

--- libs/shared/src/lib/services/application/graphql/queries.ts
+++ libs/shared/src/lib/services/application/graphql/queries.ts
@@ -1,9 +1,12 @@
 import { gql } from 'apollo-angular';
 
-// === GET APPLICATION BY ID ===
 /** Graphql request for getting application data by its id */
 export const GET_APPLICATION_BY_ID = gql`
-  query GetApplicationById($id: ID!, $shortcut: String!, $asRole: ID) {
+  query ApplicationService_GetApplicationById(
+    $id: ID!
+    $shortcut: String!
+    $asRole: ID
+  ) {
     application(id: $id, shortcut: $shortcut, asRole: $asRole) {
       id
       name
