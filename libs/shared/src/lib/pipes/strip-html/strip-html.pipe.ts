678031b05583f5446b6b9766ed4e1319f5b4abad
Unai Zalba <unai@reliefapplications.org>
2024-11-13 15:16:11 +0100

    feat/AB#105610_EIS-allow-some-style-attributes-of-html-questions-to-appear-in-grids feat: finish the text formatter cleaner pipe including text format tags and base styles

--- libs/shared/src/lib/pipes/strip-html/strip-html.pipe.ts
+++ libs/shared/src/lib/pipes/strip-html/strip-html.pipe.ts
@@ -1,4 +1,51 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { isNil } from 'lodash';
+
+/**
+ * Text format tags that should be kept
+ * source: https://www.w3docs.com/learn-html/html-tags-for-text-formatting.html
+ */
+const TEXT_FORMAT_TAGS = [
+  'h1',
+  'h2',
+  'h3',
+  'h4',
+  'h5',
+  'h6',
+  'b',
+  'strong',
+  'i',
+  'em',
+  'mark',
+  'small',
+  'del',
+  'ins',
+  'u',
+  'sub',
+  'sup',
+  'pre',
+  's',
+  'dfn',
+  'br',
+  'hr',
+];
+
+/**
+ * Basic text format styles to take in account for the given content
+ * source: https://developer.mozilla.org/en-US/docs/Learn/CSS/Styling_text/Fundamentals
+ */
+const TEXT_FORMAT_STYLE_PROPERTIES = [
+  'color',
+  'background-color',
+  'font-size',
+  'text-decoration',
+  'text-transform',
+  'text-align',
+  'font-family',
+  'font-style',
+  'font-weight',
+  'line-height',
+];
 
 /**
  * Clean up given html from all elements that are not a paragraph, an span or a text element by getting just their text content.
@@ -24,6 +71,28 @@ export class StripHtmlPipe implements PipeTransform {
   }
 
   /**
+   * Extract base text format styles from the given node
+   *
+   * @param node Node from where to fetch styles
+   * @returns Styles for basic text formatting
+   */
+  private getTextFormatStyling(node: HTMLElement): string {
+    const styles: string[] = [];
+    const style = node.getAttribute('style');
+    if (style) {
+      TEXT_FORMAT_STYLE_PROPERTIES.forEach((property) => {
+        const regex = new RegExp(`${property}:s*[^;]+;`, 'gi');
+        const styleAux = style.match(regex);
+        if (!isNil(styleAux)) {
+          styles.push(...styleAux);
+        }
+      });
+      return styles.join('');
+    }
+    return '';
+  }
+
+  /**
    * Remove any element fetching the text content of elements that are not paragraphs, spans or text elements
    *
    * @param nodes Nodes to search and clean
@@ -40,21 +109,27 @@ export class StripHtmlPipe implements PipeTransform {
           )
         ) {
           (node as HTMLElement).innerHTML = this.cleanUpNodes(node.childNodes);
-          const parentForInnerHTML = document.createElement('div');
-          const span = document.createElement('span');
-          if ((node as HTMLElement).style?.cssText) {
-            span.setAttribute('style', (node as HTMLElement).style.cssText);
-            (node as HTMLElement).setAttribute('style', '');
-          }
           /** Could happen that current node is a text format tag, like strong tag */
-          /** We keep it using the outerHTML and moving styles back to the span wrapper (step above) */
-          span.innerHTML = (node as HTMLElement).outerHTML.trim();
-          if (span.innerHTML) {
-            parentForInnerHTML.appendChild(span);
-            return parentForInnerHTML.innerHTML;
-          } else {
-            return '';
+          /** We keep it using the outerHTML and moving styles back to the span wrapper */
+          const textFormatStyling = this.getTextFormatStyling(
+            node as HTMLElement
+          );
+          if (
+            TEXT_FORMAT_TAGS.includes(
+              (node as HTMLElement).tagName?.toLowerCase()
+            ) ||
+            textFormatStyling
+          ) {
+            if (textFormatStyling) {
+              (node as HTMLElement).setAttribute('style', textFormatStyling);
+            }
+            if ((node as HTMLElement).outerHTML) {
+              return (node as HTMLElement).outerHTML.trim();
+            } else {
+              return '';
+            }
           }
+          return (node as HTMLElement).innerHTML;
         } else {
           const parentForInnerHTML = document.createElement('div');
           const span = document.createElement('span');
