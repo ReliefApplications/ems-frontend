d883ec54c99073cb129ec463e900404a538b81e9
Tai Kamilla <taikamilla@gmail.com>
2024-11-18 18:55:01 +0100

    Refactor activity log download functionality - Replaced button with a custom UI button component. - Updated the download method to use HttpClient for fetching data. - Added a new URL for downloading activities. - Included ButtonModule in the module imports.

--- libs/shared/src/lib/components/activity-log/activity-log.component.ts
+++ libs/shared/src/lib/components/activity-log/activity-log.component.ts
@@ -3,6 +3,11 @@ import { LIST_ACTIVITIES } from './graphql/queries';
 import { OnInit } from '@angular/core';
 import { Apollo } from 'apollo-angular';
 import { ActivityLog } from '../../models/activity-log.model';
+import { HttpClient } from '@angular/common/http';
+
+/**
+ * URL to download activities.
+ */
 
 /**
  * Shared activity log component.
@@ -17,14 +22,24 @@ export class ActivityLogComponent implements OnInit {
    * List of activities to display.
    */
   activitiesLogs: ActivityLog[] = [];
+
+  /**
+   * Columns to display in the table.
+   */
   displayedColumns: string[] = ['userId', 'eventType', 'url'];
+
+  /**
+   * URL to download activities.
+   */
+  downloadUrl = 'http://localhost:3000/activity/download-activities';
 
   /**
    * Constructor that injects the Apollo service.
    *
    * @param apollo The Apollo service for interacting with GraphQL API.
+   * @param http The HttpClient service for making HTTP requests.
    */
-  constructor(private apollo: Apollo) {}
+  constructor(private apollo: Apollo, private http: HttpClient) {}
 
   /**
    * OnInit lifecycle hook to fetch activities when the component initializes.
@@ -41,7 +56,19 @@ export class ActivityLogComponent implements OnInit {
       });
   }
 
-  download() {
-    console.log('file downloaded');
+  /**
+   * Method to download activities when the link is clicked.
+   */
+  downloadActivities(): void {
+    this.http
+      .get(this.downloadUrl, { responseType: 'blob' })
+      .subscribe((blob: any) => {
+        const downloadUrl = window.URL.createObjectURL(blob);
+        const link = document.createElement('a');
+        link.href = downloadUrl;
+        link.download = 'activities.xlsx';
+        link.click();
+        window.URL.revokeObjectURL(downloadUrl);
+      });
   }
 }
--- libs/shared/src/lib/components/activity-log/activity-log.module.ts
+++ libs/shared/src/lib/components/activity-log/activity-log.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivityLogComponent } from './activity-log.component';
-import { TableModule, IconModule } from '@oort-front/ui';
+import { TableModule, IconModule, ButtonModule } from '@oort-front/ui';
 import { TranslateModule } from '@ngx-translate/core';
 
 /** Module for components related to templates */
 @NgModule({
   declarations: [ActivityLogComponent],
-  imports: [CommonModule, TableModule, TranslateModule, IconModule],
+  imports: [
+    CommonModule,
+    TableModule,
+    TranslateModule,
+    IconModule,
+    ButtonModule,
+  ],
   exports: [ActivityLogComponent],
 })
 export class ActivityLogModule {}
