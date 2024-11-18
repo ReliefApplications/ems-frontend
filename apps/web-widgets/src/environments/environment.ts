4ee1f68d208638ce231bd654c9002517cccb01db
Antoine Hurard <antoine@reliefapplications.org>
2024-11-18 11:47:45 +0100

    undo changes on env files

--- apps/web-widgets/src/environments/environment.ts
+++ apps/web-widgets/src/environments/environment.ts
@@ -3,28 +3,15 @@ import { theme } from '../themes/default';
 import { sharedEnvironment } from './environment.shared';
 import { Environment } from './environment.type';
 
-// /** Authentication configuration */
-// const authConfig: AuthConfig = {
-//   issuer: 'https://id-dev.oortcloud.tech/realms/oort',
-//   redirectUri: 'http://localhost:4200/',
-//   postLogoutRedirectUri: 'http://localhost:4200/auth/',
-//   clientId: 'oort-client',
-//   scope: 'openid profile email offline_access',
-//   responseType: 'code',
-//   showDebugInformation: true,
-// };
-
 /** Authentication configuration */
 const authConfig: AuthConfig = {
-  issuer:
-    'https://login.microsoftonline.com/f610c0b7-bd24-4b39-810b-3dc280afb590/v2.0',
+  issuer: 'https://id-dev.oortcloud.tech/realms/oort',
   redirectUri: 'http://localhost:4200/',
   postLogoutRedirectUri: 'http://localhost:4200/auth/',
-  clientId: '021202ac-d23b-4757-83e3-f6ecde12266b',
+  clientId: 'oort-client',
   scope: 'openid profile email offline_access',
   responseType: 'code',
   showDebugInformation: true,
-  strictDiscoveryDocumentValidation: false,
 };
 
 /**
