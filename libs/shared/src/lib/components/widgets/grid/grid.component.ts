03b83508e1b56fb829622ab8a0c03f10861e7133
Antoine Hurard <antoine@reliefapplications.org>
2024-12-05 18:26:08 +0100

    fix: quick email button on grids would only work if button is first one

--- libs/shared/src/lib/components/widgets/grid/grid.component.ts
+++ libs/shared/src/lib/components/widgets/grid/grid.component.ts
@@ -506,7 +506,10 @@ export class GridWidgetComponent extends BaseWidgetComponent implements OnInit {
                     (x: any) => x.id === selectedId
                   )[0];
                   if (template) {
-                    const emailQuery = this.buildEmailQuery(selectedIds);
+                    const emailQuery = this.buildEmailQuery(
+                      selectedIds,
+                      options.bodyFields
+                    );
                     if (emailQuery) {
                       emailQuery.pipe(takeUntil(this.destroy$)).subscribe({
                         next: ({ data }) => {
@@ -743,12 +746,12 @@ export class GridWidgetComponent extends BaseWidgetComponent implements OnInit {
    * Build email query for quick action button.
    *
    * @param selectedIds Ids selected in the grid for email sending
+   * @param fields List of fields to pass to email
    * @returns Records graphql query.
    */
-  private buildEmailQuery(selectedIds: string[]) {
+  private buildEmailQuery(selectedIds: string[], fields: any[]) {
     const settingsData: any = clone(this.layout);
-    settingsData.query.fields =
-      this.settings?.floatingButtons?.[0].bodyFields || [];
+    settingsData.query.fields = fields;
     const builtQuery = this.queryBuilder.buildQuery(settingsData);
     if (!builtQuery) {
       this.status = {
