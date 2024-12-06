4111693c2ec560d7d96bd187108c0c1456d5fc9e
Hasnat Lamina <136315346+HasnatL-Adapptlimited@users.noreply.github.com>
2024-12-06 14:15:43 +0000

    fix: Deselecting a resource in email dataset builder would not hide query builder (#2730)
    ---------
    
    Co-authored-by: NehaSakariya-Adappt <119050920+NehaSakariya-Adappt@users.noreply.github.com>
    Co-authored-by: Antoine Hurard <antoine@reliefapplications.org>

--- libs/shared/src/lib/components/email/components/email-template/email-template.component.ts
+++ libs/shared/src/lib/components/email/components/email-template/email-template.component.ts
@@ -259,6 +259,11 @@ export class EmailTemplateComponent
         } else if (value === null) {
           this.selectedResourceId = value;
           this.clearUnusedValues(this.segmentList[this.activeSegmentIndex]);
+          this.availableFields = [];
+          this.selectedFields = [];
+          if (this.resource?.fields) {
+            this.resource.fields = [];
+          }
         }
       });
     this.selectedEmails = this.distributionList.get('inputEmails') as FormArray;
@@ -614,13 +619,6 @@ export class EmailTemplateComponent
     return this.selectedEmails.controls.map(
       (control: AbstractControl) => control.value
     );
-  }
-
-  /**
-   * Resets the state `showDatasetLimitWarning` when the close button is clicked.
-   */
-  closeWarningMessage(): void {
-    this.showDatasetLimitWarning = false;
   }
 
   /**
