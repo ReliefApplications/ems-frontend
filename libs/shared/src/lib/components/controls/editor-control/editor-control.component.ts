cc11ddb413a5c9f39a63d299255b1b3d05d7514c
Antoine Hurard <antoine@reliefapplications.org>
2025-01-24 14:52:40 +0100

    can now use html question in readonly mode & display mode

--- libs/shared/src/lib/components/controls/editor-control/editor-control.component.ts
+++ libs/shared/src/lib/components/controls/editor-control/editor-control.component.ts
@@ -83,6 +83,9 @@ export class EditorControlComponent
   @HostBinding()
   id = `shared-editor-control-${EditorControlComponent.nextId++}`;
 
+  /** Is control readonly */
+  @Input() readonly = false;
+
   /**
    * Gets the placeholder for the select
    *
@@ -214,6 +217,14 @@ export class EditorControlComponent
     // Set the editor language
     this.editorConfig.language = this.editorService.language;
     this.editorContent = this.value || '';
+
+    if (this.editor) {
+      if (this.readonly) {
+        this.editor.editor.setMode('readonly');
+      } else {
+        this.editor.editor.setMode('design');
+      }
+    }
   }
 
   ngAfterViewInit(): void {
@@ -248,6 +259,9 @@ export class EditorControlComponent
     });
     this.editor.disabled = true;
     // this.editor.onInit.subscribe(() => {});
+    if (this.readonly) {
+      this.editor.editor.setMode('readonly');
+    }
   }
 
   /** Emit and change editor loading */
