6860e14e21b530e2b5091acaa718fb2bc5166f39
MwanPygmay <matt.brunot@laposte.net>
2025-01-29 10:56:21 +0100

    adding errors management

--- libs/shared/src/lib/components/form-pages-layout/form-pages-layout.component.ts
+++ libs/shared/src/lib/components/form-pages-layout/form-pages-layout.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { IconModule } from '@oort-front/ui';
 
 /** New layout on the left of the form */
 @Component({
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, IconModule],
   selector: 'shared-form-pages-layout',
   templateUrl: './form-pages-layout.component.html',
   styleUrls: ['./form-pages-layout.component.scss'],
@@ -13,7 +14,8 @@ export class FormPagesLayoutComponent {
   /** Current page */
   @Input() selectedPageIndex = 0;
   /** Number of pages in the form */
-  @Input() pages: { name: string; title: string }[] = [];
+  @Input() pages: { name: string; title: string; containsErrors: boolean }[] =
+    [];
   /** show page when clicking on page name */
   @Output() showPage = new EventEmitter<number>();
 }
