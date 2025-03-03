41b7ec983a66a25908ecd5a371ba1a2eb06ead31
Antoine Hurard <50317271+AntoineRelief@users.noreply.github.com>
2025-03-03 11:57:07 +0100

    fix: prevent tab widget to do incorrect scrolling when switching tab (#2772)
    AB#110913

--- libs/ui/src/lib/tabs/directives/tab-body-host.directive.ts
+++ libs/ui/src/lib/tabs/directives/tab-body-host.directive.ts
@@ -55,11 +55,19 @@ export class TabBodyHostDirective
     this.openedTab
       .pipe(takeUntil(this.destroy$))
       .subscribe((tab: TabComponent) => {
+        const pageContainer = document.getElementById('appPageContainer');
+        const scrollLeft = pageContainer?.scrollLeft || 0;
+        const scrollTop = pageContainer?.scrollTop || 0;
         if (tab !== this._openedTab && this.hasAttached()) {
           this.detach();
         }
         if (!this.hasAttached()) {
           this.attach(tab.content);
+          if (pageContainer) {
+            // Prevent app page container to incorrectly scroll when loading a new tab in tabs widget.
+            pageContainer.scrollTop = scrollTop;
+            pageContainer.scrollLeft = scrollLeft;
+          }
         }
       });
   }
