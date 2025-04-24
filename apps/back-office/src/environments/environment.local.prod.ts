8f855bce8f4b96dd982b5d38671c0baecca14fa0
Antoine Hurard <antoine@reliefapplications.org>
2025-04-24 16:37:39 +0200

    create new environments

---
+++ apps/back-office/src/environments/environment.local.prod.ts
@@ -0,0 +1,64 @@
+import { AuthConfig } from 'angular-oauth2-oidc';
+import { theme } from '../themes/default/default.dev';
+import { sharedEnvironment } from './environment.shared';
+import { Environment } from './environment.type';
+
+/**
+ * Authentication configuration
+ */
+const authConfig: AuthConfig = {
+  /* cSpell:disable */
+  issuer:
+    'https://login.microsoftonline.com/f610c0b7-bd24-4b39-810b-3dc280afb590/v2.0',
+  redirectUri: 'http://localhost:4200/',
+  postLogoutRedirectUri: 'http://localhost:4200/auth/',
+  clientId: '021202ac-d23b-4757-83e3-f6ecde12266b',
+  scope:
+    'openid profile email offline_access offline_access api://75deca06-ae07-4765-85c0-23e719062833/access_as_user',
+  // Last scope is used to authenticate against Common Services
+  responseType: 'code',
+  showDebugInformation: true,
+  strictDiscoveryDocumentValidation: false,
+  /* cSpell:enable */
+};
+
+/**
+ * Environment file for local development.
+ */
+export const environment: Environment = {
+  /* cSpell:disable */
+  ...sharedEnvironment,
+  production: false,
+  href: 'http://localhost:4200',
+  apiUrl: 'http://localhost:3000',
+  subscriptionApiUrl: 'ws://localhost:3000',
+  frontOfficeUri: 'https://hems.who.int/apps/',
+  backOfficeUri: 'http://localhost:4200/',
+  module: 'backoffice',
+  availableLanguages: ['en'],
+  authConfig,
+  esriApiKey:
+    'AAPKf2bae9b3f32943e2a8d58b0b96ffea3fj8Vt8JYDt1omhzN_lONXPRHN8B89umU-pA9t7ze1rfCIiiEVXizYEiFRFiVrl6wg',
+  theme,
+  availableWidgets: [
+    'donut-chart',
+    'line-chart',
+    'bar-chart',
+    'column-chart',
+    'pie-chart',
+    'polar-chart',
+    'radar-chart',
+    'grid',
+    'text',
+    'map',
+    'summaryCard',
+    'tabs',
+  ],
+  user: {
+    attributes: ['country', 'region', 'location', 'department'],
+  },
+  admin0Url: 'https://hems.who.int/app-builder/admin0.json',
+  csApiUrl: 'https://hems.who.int/csapi/api',
+  csDocUrl: 'https://hems.who.int/csdocui',
+  /* cSpell:enable */
+};
