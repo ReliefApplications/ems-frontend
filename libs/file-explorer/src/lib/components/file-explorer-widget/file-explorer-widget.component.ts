ba610a029c295d5b757637ad940172597b07af75
Antoine Hurard <antoine@reliefapplications.org>
2025-06-12 13:42:36 +0600

    integrate table in splitter

--- libs/file-explorer/src/lib/components/file-explorer-widget/file-explorer-widget.component.ts
+++ libs/file-explorer/src/lib/components/file-explorer-widget/file-explorer-widget.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutModule } from '@progress/kendo-angular-layout';
+import { FileExplorerTableComponent } from '../file-explorer-table/file-explorer-table.component';
 
 /**
  * File explorer widget component.
@@ -8,7 +9,7 @@ import { LayoutModule } from '@progress/kendo-angular-layout';
 @Component({
   selector: 'oort-front-file-explorer-widget',
   standalone: true,
-  imports: [CommonModule, LayoutModule],
+  imports: [CommonModule, LayoutModule, FileExplorerTableComponent],
   templateUrl: './file-explorer-widget.component.html',
   styleUrls: ['./file-explorer-widget.component.scss'],
 })
