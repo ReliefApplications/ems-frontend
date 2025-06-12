f08d24cca21d5349312388ce11563e2aabb1f973
Antoine Hurard <antoine@reliefapplications.org>
2025-06-12 14:06:31 +0600

    possibility to switch view using toolbar

---
+++ libs/file-explorer/src/lib/components/file-explorer-toolbar/file-explorer-toolbar.component.ts
@@ -0,0 +1,29 @@
+import { Component, inject, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ButtonModule, FormWrapperModule, IconModule } from '@oort-front/ui';
+import { fileExplorerView } from '../../types/fie-explorer-view.type';
+import { FileExplorerWidgetComponent } from '../file-explorer-widget/file-explorer-widget.component';
+
+@Component({
+  selector: 'oort-front-file-explorer-toolbar',
+  standalone: true,
+  imports: [CommonModule, IconModule, FormWrapperModule, ButtonModule],
+  templateUrl: './file-explorer-toolbar.component.html',
+  styleUrls: ['./file-explorer-toolbar.component.scss'],
+})
+export class FileExplorerToolbarComponent {
+  @Input() view: fileExplorerView = 'list';
+
+  private parent: FileExplorerWidgetComponent | null = inject(
+    FileExplorerWidgetComponent,
+    { optional: true }
+  );
+
+  public onChangeView(view: fileExplorerView) {
+    console.log(view);
+    console.log(this.parent);
+    if (this.parent) {
+      this.parent.view = view;
+    }
+  }
+}
--- libs/file-explorer/src/lib/components/file-explorer-widget/file-explorer-widget.component.ts
+++ libs/file-explorer/src/lib/components/file-explorer-widget/file-explorer-widget.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutModule } from '@progress/kendo-angular-layout';
+import { FileExplorerToolbarComponent } from '../file-explorer-toolbar/file-explorer-toolbar.component';
 import { FileExplorerTableComponent } from '../file-explorer-table/file-explorer-table.component';
+import { fileExplorerView } from '../../types/fie-explorer-view.type';
 
 /**
  * File explorer widget component.
@@ -9,8 +11,15 @@ import { FileExplorerTableComponent } from '../file-explorer-table/file-explorer
 @Component({
   selector: 'oort-front-file-explorer-widget',
   standalone: true,
-  imports: [CommonModule, LayoutModule, FileExplorerTableComponent],
+  imports: [
+    CommonModule,
+    LayoutModule,
+    FileExplorerToolbarComponent,
+    FileExplorerTableComponent,
+  ],
   templateUrl: './file-explorer-widget.component.html',
   styleUrls: ['./file-explorer-widget.component.scss'],
 })
-export class FileExplorerWidgetComponent {}
+export class FileExplorerWidgetComponent {
+  public view: fileExplorerView = 'list';
+}
