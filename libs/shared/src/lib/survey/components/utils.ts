64baf7e6c6d7a947cf01a891c2f1a2bad6e3a4de
Jose Garcia <joselgc1@gmail.com>
2025-08-29 17:25:01 +0200

    AB#118869 - ABC Add record inline in resources questions

--- libs/shared/src/lib/survey/components/utils.ts
+++ libs/shared/src/lib/survey/components/utils.ts
@@ -319,8 +319,7 @@ export const buildAddInlineButton = (
   );
   question.registerFunctionOnPropertyValueChanged('addRecord', () =>
     updateAddInlineVisibility()
-  );
-  
+  );  
   return addInlineButton;
 };
 
