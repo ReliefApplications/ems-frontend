e9db8777357eb8c6b2c11326ad6c1ca67d2dcad0
Antoine Hurard <antoine@reliefapplications.org>
2025-09-10 17:24:03 +0200

    fix: update ESRI API key

--- apps/back-office/src/environments/environment.shared.ts
+++ apps/back-office/src/environments/environment.shared.ts
@@ -8,6 +8,6 @@ export const sharedEnvironment = {
   // eslint-disable-next-line @typescript-eslint/no-var-requires
   version: require('../../../../package.json').version,
   esriApiKey:
-    'AAPK6020068836884707b511570bfb55c042Y7JsUDJU7Dg19M1paHAURrcaX7rPUEnxZj1a_-rDCRSrzSSluutrv3vNaDRnpb9N',
+    'AAPTxy8BH1VEsoebNVZXo8HurCnxSTQxj2ceN8UJa_c_5YzkGC0L_Vyzn6WfiyxiViz6ZGd5U2JoH5ZErKa6RPtA4wdRJOcyAD1nHO2GuT8nsrB6hSxtn4TwxUfgloSkmsbFrbWg-AVB2VRZeSfW8OVDvzm-_MkG9SXpOd6Ctv0Y3jfu1k0XNzbpIN3QeSNaNIYZxYf99-U76DPsz9rdLPUZPaEPRHZvJi4uoMFTNUEgVUc.AT1_d4AzFDjk',
   /* cSpell:enable */
 };
