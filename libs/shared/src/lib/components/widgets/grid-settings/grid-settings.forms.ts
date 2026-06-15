e3829c15c0fd20ea940aed5621fd547412ca5c26
Antoine Hurard <antoine@reliefapplications.org>
2026-06-15 13:49:11 +0200

    Translate more options from grid & summary card widgets

--- libs/shared/src/lib/components/widgets/grid-settings/grid-settings.forms.ts
+++ libs/shared/src/lib/components/widgets/grid-settings/grid-settings.forms.ts
@@ -19,6 +19,7 @@ import { extendWidgetForm } from '../common/display-settings/extendWidgetForm';
 import { ActionButton } from '../grid/action-button.type';
 import { Role } from '../../../models/user.model';
 import { Injector } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 import { ApplicationService } from '../../../services/application/application.service';
 import { Subject, takeUntil } from 'rxjs';
 
@@ -41,6 +42,8 @@ export class GridSettingsFormFactory {
   private fb!: FormBuilder;
   /** Application Service */
   private applicationService!: ApplicationService;
+  /** Translate Service */
+  private translate!: TranslateService;
 
   /**
    * Grid settings form factory
@@ -51,6 +54,7 @@ export class GridSettingsFormFactory {
   constructor(private injector: Injector, public destroy$: Subject<boolean>) {
     this.fb = this.injector.get(FormBuilder);
     this.applicationService = this.injector.get(ApplicationService);
+    this.translate = this.injector.get(TranslateService);
     this.roles = this.applicationService.application.value?.roles || [];
   }
 
@@ -266,7 +270,13 @@ export class GridSettingsFormFactory {
         pageUrl: [get(configuration, 'actions.navigateSettings.pageUrl', '')],
         field: [get(configuration, 'actions.navigateSettings.field', '')],
         title: [
-          get(configuration, 'actions.navigateSettings.title', 'Details view'),
+          get(
+            configuration,
+            'actions.navigateSettings.title',
+            this.translate.instant(
+              'components.widget.settings.grid.actions.goTo.column.defaultTitle'
+            )
+          ) as LocalizedString,
         ],
       }),
     });
@@ -275,7 +285,7 @@ export class GridSettingsFormFactory {
       if (value) {
         formGroup
           .get('navigateSettings.title')
-          ?.setValidators(Validators.required);
+          ?.setValidators(localizedRequired);
       } else {
         formGroup.get('navigateSettings.title')?.clearValidators();
       }
