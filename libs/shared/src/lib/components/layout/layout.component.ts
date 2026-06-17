13db5438b514bca40b8305cd711a81b6d01c1c16
Maximo-reliefapps <maximo.reliefapps@gmail.com>
2026-06-17 16:49:59 +0200

    Set the page language as the browser lang if available and no local lang selected

--- libs/shared/src/lib/components/layout/layout.component.ts
+++ libs/shared/src/lib/components/layout/layout.component.ts
@@ -447,7 +447,11 @@ export class LayoutComponent
     // select the language saved (or default if not)
     let language = localStorage.getItem('lang');
     if (!language || !this.languages.includes(language)) {
-      language = this.translate.defaultLang;
+      // fall back to browser language if supported, otherwise app default
+      const browserLang = this.translate.getBrowserLang() ?? '';
+      language = this.languages.includes(browserLang)
+        ? browserLang
+        : this.translate.defaultLang;
     }
     // if not default language, change language of the interface
     if (language !== this.translate.defaultLang) {
