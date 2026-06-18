4424f9c61d6f4284b3989bd029c13b7ce96152bb
Antoine Hurard <antoine@reliefapplications.org>
2026-06-18 10:28:27 +0200

    revert changes on html parser

--- libs/shared/src/lib/services/html-parser/html-parser.service.ts
+++ libs/shared/src/lib/services/html-parser/html-parser.service.ts
@@ -989,7 +989,8 @@ export class HtmlParserService {
             convertedValue += `<button type="file"
               field="${field.name}"
               index="${i++}"
-              style="border: none; padding: 4px 6px; cursor: pointer; ${style}"
+              style="border: none; padding: 4px 6px; cursor: pointer; ${style}" title=
+              ${file.name}
               >
               <span class="k-icon ${fileIcon}" style="margin-right: 4px"></span>
               ${fileName}
